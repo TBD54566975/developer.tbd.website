63ba4b0c304e71f049f39d33882afcc6bd499909
Antonio Luis <antonio.silva@arctouch.com>
2022-05-25 17:38:58 -0300

    fix: Fix storybook to work with tailwind Also create story for the button component

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../static'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
