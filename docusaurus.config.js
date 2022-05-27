25c94cca901d94be49c9939e3f85583ba958c997
Sergio Baptista <sergio.baptista@akqa.com>
2022-05-27 10:31:10 -0400

    fix: add links ot all component details

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -129,9 +129,27 @@ ${content}
             return file.file === filename;
           });
 
+          let links = '';
+          if (fileData.links) {
+            fileData.links.forEach((ele, idx) => {
+              if (idx === 0) {
+                links += '<div class="flex gap-12 mb-20">';
+              }
+
+              links += `${linkcode
+                .replace('#url#', ele.url)
+                .replace('#text#', linkTypes[ele.type].text)}`;
+
+              if (idx === fileData.links.length - 1) {
+                links += '</div>';
+              }
+            });
+          }
+
           if (fileData) {
             return {
               content: `<div  class="prose prose-pink">
+              ${links}
 
 ${content}
 
@@ -158,9 +176,27 @@ ${content}
             return file.file === filename;
           });
 
+          let links = '';
+          if (fileData.links) {
+            fileData.links.forEach((ele, idx) => {
+              if (idx === 0) {
+                links += '<div class="flex gap-12 mb-20">';
+              }
+
+              links += `${linkcode
+                .replace('#url#', ele.url)
+                .replace('#text#', linkTypes[ele.type].text)}`;
+
+              if (idx === fileData.links.length - 1) {
+                links += '</div>';
+              }
+            });
+          }
+
           if (fileData) {
             return {
               content: `<div  class="prose prose-pink">
+              ${links}
 
 ${content}
 
@@ -187,9 +223,27 @@ ${content}
             return file.file === filename;
           });
 
+          let links = '';
+          if (fileData.links) {
+            fileData.links.forEach((ele, idx) => {
+              if (idx === 0) {
+                links += '<div class="flex gap-12 mb-20">';
+              }
+
+              links += `${linkcode
+                .replace('#url#', ele.url)
+                .replace('#text#', linkTypes[ele.type].text)}`;
+
+              if (idx === fileData.links.length - 1) {
+                links += '</div>';
+              }
+            });
+          }
+
           if (fileData) {
             return {
               content: `<div  class="prose prose-pink">
+              ${links}
 
 ${content}
 
@@ -284,13 +338,13 @@ ${content}
         },
         items: [
           {
-            to: '/open-source',
+            to: '/opensource',
             label: 'Open Source',
             position: 'left',
           },
 
           {
-            to: '/projects',
+            to: '/projects-index',
             label: 'Projects',
             position: 'left',
           },
