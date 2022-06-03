c0cdc0ffb6f36212e9c5f7648362aee932d2826b
jsquash8 <105517872+jsquash8@users.noreply.github.com>
2022-06-03 10:34:31 -0400

    TBDSITES-304
    Replaced links with button component on the following pages
    
    /src/pages/opensource.mdx
    
    Read More About Governance
    View Our Code of Conduct
    /src/pages/projects/web5.mdx
    
    View Project

--- src/content/open-source.js
+++ src/content/open-source.js
@@ -20,4 +20,11 @@ export const content = {
         'Projects need well-defined scope to succeed. They also need accessible, comprehensive documentation, a stellar Getting Started experience, and well-defined expectations. TBD fosters an environment built for contribution without personal intervention. Newcomers should be able to follow the project status on their own without additional explanation.',
     },
   ],
+
+  buttons: [
+    {
+      textButton: 'View Project',
+      url: '/projects/web5',
+    },
+  ],
 };
