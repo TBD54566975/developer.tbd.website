ad15f3cf722812c6e91fca58c546636469abb3fe
Antonio Luis <antonio.silva@arctouch.com>
2022-06-06 17:19:26 -0300

    feat: Add web5 illustration

--- src/content/project-content-web5.js
+++ src/content/project-content-web5.js
@@ -63,6 +63,12 @@ export const content = {
       },
     ],
   },
+  web5Illustrations: {
+    imgMobile: '/img/new-web5-mobile.svg',
+    imgTablet: '/img/new-web5-tablet.svg',
+    imgDesktop: '/img/new-web5-desktop.svg',
+    altText: 'Web2 and Web3 to Web5',
+  },
   useCases: {
     title: 'Use Cases',
     pillars: [
