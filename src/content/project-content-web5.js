452327301e0742c7a4caf04770908504cc183059
Antonio Luis <antonio.silva@arctouch.com>
2022-06-06 10:53:41 -0300

    feat: Update use case icon on web5 page

--- src/content/project-content-web5.js
+++ src/content/project-content-web5.js
@@ -63,13 +63,13 @@ export const content = {
     title: 'Use Cases',
     pillars: [
       {
-        img: '/img/identity-icon.svg',
+        img: '/img/devices-icon.svg',
         title: 'Control Your Identity',
         description:
           'Alice holds a digital wallet that securely manages her identity, data, and authorizations for external apps and connections. Alice uses her wallet to sign in to a new decentralized social media app. Because Alice has connected to the app with her decentralized identity, she does not need to create a profile, and all the connections, relationships, and posts she creates through the app are stored with her, in her decentralized web node. Now Alice can switch apps whenever she wants, taking her social persona with her.',
       },
       {
-        img: '/img/wallet-icon.svg',
+        img: '/img/data-icon.svg',
         title: 'Own Your Data',
         description:
           "Bob is a music lover and hates having his personal data locked to a single vendor. It forces him to regurgitate his playlists and songs over and over again across different music apps. Thankfully there's a way out of this maze of vendor-locked silos: Bob can keep this data in his decentralized web node. This way Bob is able to grant any music app access to his settings and preferences, enabling him to take his personalized music experience wherever he chooses.",
