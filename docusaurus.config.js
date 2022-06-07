f5b4f2d249960d3895d020b62f771b04b0aa0986
Antonio Luis <antonio.silva@arctouch.com>
2022-06-07 14:00:47 -0300

    feat: style blog list to match the design.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -117,7 +117,7 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
-          showReadingTime: true,
+          showReadingTime: false,
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
