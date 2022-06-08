4888440bde08d7128df1784c3a30a2a61ad2a87a
Antonio Luis <antonio.silva@arctouch.com>
2022-06-08 09:55:22 -0300

    fix: Try to fix build error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -117,7 +117,7 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
-          showReadingTime: false,
+          showReadingTime: true,
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
