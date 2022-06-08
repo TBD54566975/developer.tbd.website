5f4f1746bcf48961f650e58e9a86c3f543488f53
Antonio Luis <antonio.silva@arctouch.com>
2022-06-08 15:03:26 -0300

    feat: create learn card component

---
+++ src/components/LearnCard/LearnCard.jsx
@@ -0,0 +1,78 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-no-target-blank */
+import React from 'react';
+
+export default function LearnCard({
+  title,
+  datePosted,
+  description,
+  author,
+  image,
+  url,
+  type,
+  time,
+  isExternalLink,
+}) {
+  let icon = null;
+  if (type === 'video') {
+    icon = '/img/video-media-icon.svg';
+  } else if (type === 'audio') {
+    icon = '/img/audio-media-icon.svg';
+  } else if (type === 'article') {
+    icon = '/img/article-media-icon.svg';
+  }
+  const [seconds, minutes, hours] = `${time}`
+    .split(':')
+    .reverse()
+    .map((el) => parseInt(el));
+  let DurationText = '';
+  if (hours || minutes || seconds) DurationText += ' Duration:';
+  if (hours) DurationText += ` ${hours}h`;
+  if (minutes) DurationText += ` ${minutes}min`;
+  if (seconds) DurationText += ` ${seconds}sec`;
+
+  const linkLabel = `${
+    isExternalLink ? 'Opens in a new tab: ' : ''
+  } ${title}.${DurationText}`;
+
+  return (
+    <div>
+      <div className="border-2 border-solid border-accent-cyan w-fit shadow-button-sh-cyan mb-8">
+        <a
+          href={url}
+          aria-label={linkLabel}
+          className=""
+          target={isExternalLink ? '_blank' : ''}
+          rel={isExternalLink ? 'noopener noreferrer' : ''}
+        >
+          <img src={image} alt="" />
+        </a>
+        <div className="flex h-0 justify-between">
+          <div className="relative -top-9 left-3 h-6 w-6 bg-primary-black">
+            <img
+              src={icon}
+              alt=""
+              aria-hidden="true"
+              className="media-icon h-6 w-6 fill-primary-black dark:fill-primary-yellow"
+            />
+          </div>
+          {type !== 'article' ? (
+            <div className="relative -top-9 -left-3 h-7 w-fit bg-primary-black text-accent-cyan px-[6px] py-[4px]">
+              <p className="copy-sm">{time}</p>
+            </div>
+          ) : null}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h2 className="h2 font-medium text-primary-yellow">{title}</h2>
+      </div>
+      <div className="mb-6 tablet:mb-9">
+        <p className="copy text-primary-yellow">{description}</p>
+      </div>
+      <div className="copy-sm text-primary-yellow">
+        <p>{datePosted}</p>
+        <p className="hidden tablet:block">Posted by {author}</p>
+      </div>
+    </div>
+  );
+}
