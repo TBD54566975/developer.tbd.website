51ec2335cf6aac8cf6ca339a1a67cee33f6408e9
Antonio Luis <antonio.silva@arctouch.com>
2022-06-16 20:58:31 -0300

    fix: Avoiding the sidebar flashing on events page.

--- src/theme/DocSidebar/index.js
+++ src/theme/DocSidebar/index.js
@@ -9,24 +9,35 @@ import { useWindowSize } from '@docusaurus/theme-common';
 import DocSidebarDesktop from '@theme/DocSidebar/Desktop';
 import DocSidebarMobile from '@theme/DocSidebar/Mobile';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import { useEffect, useState } from 'react';
+
 export default function DocSidebar(props) {
   const windowSize = useWindowSize(); // Desktop sidebar visible on hydration: need SSR rendering
-
+  const [noRender, setNoRender] = useState(true);
   let shouldRenderSidebarDesktop =
     windowSize === 'desktop' || windowSize === 'ssr'; // Mobile sidebar not visible on hydration: can avoid SSR rendering
 
   let shouldRenderSidebarMobile = windowSize === 'mobile';
-  if (ExecutionEnvironment.canUseDOM) {
-    const splitPathname = window.location.pathname.split('/');
-    if (splitPathname.length > 1 && splitPathname[1] === 'events') {
-      shouldRenderSidebarDesktop = false;
-      shouldRenderSidebarMobile = false;
+
+  useEffect(() => {
+    if (ExecutionEnvironment.canUseDOM) {
+      const splitPathname = window.location.pathname.split('/');
+      if (splitPathname.length > 1 && splitPathname[1] === 'events') {
+        setNoRender(true);
+      } else {
+        setNoRender(false);
+      }
     }
-  }
+  }, [ExecutionEnvironment.canUseDOM]);
+
   return (
     <>
-      {shouldRenderSidebarDesktop && <DocSidebarDesktop {...props} />}
-      {shouldRenderSidebarMobile && <DocSidebarMobile {...props} />}
+      {!noRender && shouldRenderSidebarDesktop && (
+        <DocSidebarDesktop {...props} />
+      )}
+      {!noRender && shouldRenderSidebarMobile && (
+        <DocSidebarMobile {...props} />
+      )}
     </>
   );
 }
