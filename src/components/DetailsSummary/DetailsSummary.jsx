5732c271a4bbc275ec1520fe3b6bc33748441641
Antonio Luis <antonio.silva@arctouch.com>
2022-06-23 17:38:36 -0300

    fix: Change DetailsSummary to display many paragraphs

--- src/components/DetailsSummary/DetailsSummary.jsx
+++ src/components/DetailsSummary/DetailsSummary.jsx
@@ -2,39 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 const DetailsSummary = ({ details, summary }) => {
+  console.log('details?', details);
   const [isOpen, setIsOpen] = useState(false);
   return (
     <React.Fragment>
-      <details className="copy" open={isOpen}>
-        <summary className="flex cursor-pointer copy">
-          {!isOpen ? summary : null}
-        </summary>
-        {details}
-      </details>
-      <div
-        className="flex justify-between mt-8"
-        onClick={() => setIsOpen((prevValue) => !prevValue)}
-      >
-        <div>
-          <p>See More</p>
-        </div>
-        <button>
-          <div className="ml-auto float-right">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M7 8H5V10H7V12H9V14H11V16H13V14H15V12H17V10H19V8H17V10H15V12H13V14H11V12H9V10H7V8Z"
-                fill="#FFEC19"
-              />
-            </svg>
+      <p className="copy">{summary}</p>
+      <details className="copy">
+        <summary
+          className="block cursor-pointer copy border-b-primary-yellow border-b-2 pb-[0.6875rem]"
+          onClick={() => {
+            console.log('entered here:', isOpen);
+            setIsOpen((prevValue) => !prevValue);
+          }}
+        >
+          <div className="flex justify-between mt-[2.8125rem]">
+            <div>{isOpen ? <p>View Less</p> : <p>View More</p>}</div>
+            <button>
+              <div className="ml-auto float-right">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7 8H5V10H7V12H9V14H11V16H13V14H15V12H17V10H19V8H17V10H15V12H13V14H11V12H9V10H7V8Z"
+                    fill="#FFEC19"
+                  />
+                </svg>
+              </div>
+            </button>
           </div>
-        </button>
-      </div>
+        </summary>
+        <>
+          {details.map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
+        </>
+      </details>
     </React.Fragment>
   );
 };
--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,17 +1,33 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import ReactPlayer from 'react-player';
 import PropTypes from 'prop-types';
+import { DetailsSummary } from '../DetailsSummary';
 
-const Video = ({ url, description }) => {
+const Video = ({ url, description, transcription }) => {
   return (
-    <div className="not-prose flex flex-col gap-12 tablet:gap-4">
-      <div className="border-2 border-accent-cyan w-fit inline-block">
-        <ReactPlayer url={url} />
+    <React.Fragment>
+      <div className="not-prose flex flex-col gap-12 tablet:gap-4">
+        <div className="border-2 border-accent-cyan w-fit inline-block">
+          <ReactPlayer url={url} />
+        </div>
+        {description ? (
+          <div className="mt-6 text-primary-yellow font-normal font-lg w-auto inline-block">
+            {description}
+          </div>
+        ) : null}
       </div>
-      <div className="mt-6 text-primary-yellow font-normal font-lg w-auto inline-block">
-        {description}
-      </div>
-    </div>
+      {transcription ? (
+        <>
+          <h2 className="h2 mt-[5.125rem]">TRANSCRIPTION</h2>
+          <DetailsSummary
+            details={transcription.details}
+            summary={transcription.summary}
+          />
+          <div className="h-[6rem]"></div>
+        </>
+      ) : null}
+    </React.Fragment>
   );
 };
 
@@ -20,8 +36,7 @@ Video.propTypes = {
    * video source
    */
   url: PropTypes.string.isRequired,
-  description: PropTypes.string.isReqiured,
-  details: PropTypes.string,
-  summary: PropTypes.string,
+  description: PropTypes.string,
+  transcription: PropTypes.array,
 };
 export default Video;
