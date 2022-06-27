f119071fe7cca6fac33fbaa31e97068d04ad8c5d
jsquash8 <105517872+jsquash8@users.noreply.github.com>
2022-06-27 10:17:54 -0400

    feat: adds twitter links to guest names on discussion pages
    Resolves: #430

--- src/components/Biography/Biography.jsx
+++ src/components/Biography/Biography.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Biography = ({ imageUrl, headline, description, growToFit }) => {
+const Biography = ({ imageUrl, headline, description, growToFit, twitterLink }) => {
   return (
     <div className={'not-prose ' + growToFit ? 'max-w-[16rem]' : null}>
       <div className="border-accent-cyan border-2 border-solid mb-6 w-fit min-w-full">
@@ -12,7 +12,7 @@ const Biography = ({ imageUrl, headline, description, growToFit }) => {
         />
       </div>
       <div className="mb-4">
-        <h2 className="h2-caps text-primary-yellow">{headline}</h2>
+        <a href={twitterLink}><h2 className="h2-caps text-primary-yellow">{headline}</h2></a>
       </div>
       <div>
         <h3 className="h3 text-primary-yellow">{description}</h3>
@@ -25,6 +25,7 @@ Biography.PropTypes = {
   imageUrl: PropTypes.string.isRequired,
   headline: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  twitterLink: PropTypes.string,
 };
 
 export default Biography;
