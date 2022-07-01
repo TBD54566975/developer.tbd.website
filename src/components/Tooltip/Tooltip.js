fd50f2c22e354e895417c56d8d02dd63ba21e34a
Antonio Luis <antonio.silva@arctouch.com>
2022-07-01 20:27:26 -0300

    fix: Fix tooltip not respecting boundaries screen.

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,29 +1,37 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useTooltip } from '@react-aria/tooltip';
 import { mergeProps } from '@react-aria/utils';
 
 export default function Tooltip({ state, parentRef, ...props }) {
   let { tooltipProps } = useTooltip(props, state);
   let ref = useRef();
+  const [leftShift, setLeftShift] = useState(0);
   useEffect(() => {
     if (state.isOpen) {
       console.log('window.innerWidth ', window.innerWidth);
       console.log('parentRef', parentRef.current.offsetLeft);
+      const offsetParent = parentRef.current.offsetLeft;
+      const windowWidth = window.innerWidth;
       console.log('ref', ref.current.offsetLeft);
+      let leftShift = 0;
+      console.log('offsetParent+ 383', offsetParent + 383);
+      if (windowWidth < offsetParent + 400) {
+        setLeftShift(windowWidth - (offsetParent + 400));
+        console.log('leftShift', offsetParent + 400 - windowWidth);
+      }
     }
-  }, [state, parentRef]);
+  }, [state, parentRef, leftShift]);
   return (
     <span
       ref={ref}
       style={{
         position: 'absolute',
-        left: '5px',
+        left: leftShift + 'px',
         top: '100%',
         marginTop: '10px',
         backgroundColor: 'var(--color-blue)',
         color: 'black',
         padding: '5px',
-        maxWidth: '375px',
         zIndex: '100',
       }}
       {...mergeProps(props, tooltipProps)}
--- src/components/TooltipWrapper/TooltipWrapper.js
+++ src/components/TooltipWrapper/TooltipWrapper.js
@@ -8,10 +8,10 @@ import { Illustration } from '../Illustration';
 export default function TooltipWrapper(props) {
   let state = useTooltipTriggerState(props);
   let ref = React.useRef();
-
+  let parentRef = React.useRef();
   let { triggerProps, tooltipProps } = useTooltipTrigger(props, state, ref);
   return (
-    <span className="relative not-prose">
+    <span className="relative not-prose" ref={parentRef}>
       <button
         className="bg-accent-cyan text-primary-black"
         ref={ref}
@@ -22,8 +22,8 @@ export default function TooltipWrapper(props) {
       >
         {props.trigger}
       </button>
-      {true && (
-        <Tooltip state={state} parentRef={ref} {...tooltipProps}>
+      {state.isOpen && (
+        <Tooltip state={state} parentRef={parentRef} {...tooltipProps}>
           <div className="pr-5 pl-5 pb-5  tooltipBorder w-96">
             <div className="flex  p-2">
               <div className="-ml-3 pr-4 p-0 h-fit">
