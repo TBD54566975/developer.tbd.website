e7c0d7e72d7d1d187defbcd8715640e4f11681ae
Sergio Baptista <sergio.baptista@akqa.com>
2022-07-12 16:30:14 -0400

    refactor: make all common components avaialbel in all MDX pagesa nd pages that use the Layout component without hte need of an import statement

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from '@theme-original/Layout';
 import { GlitchWrapper } from '@site/src/components';
+import MDXContent from '@theme/MDXContent';
+
 export default function LayoutWrapper(props) {
   return (
     <GlitchWrapper>
-      <Layout {...props} />
+      <MDXContent>
+        <Layout {...props} />
+      </MDXContent>
     </GlitchWrapper>
   );
 }
---
+++ src/theme/MDXComponents/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+import {
+  TooltipWrapper,
+  ButtonGroup,
+  Actors,
+  ProjectList,
+  Divider,
+  PillarList,
+  SwitchIllustration,
+  IconTextButton,
+  TwoColumnContainer,
+} from '@site/src/components';
+
+export default {
+  ...MDXComponents,
+  TooltipWrapper: TooltipWrapper,
+  ButtonGroup: ButtonGroup,
+  Actors: Actors,
+  ProjectList: ProjectList,
+  Divider: Divider,
+  PillarList: PillarList,
+  SwitchIllustration: SwitchIllustration,
+  IconTextButton: IconTextButton,
+  TwoColumnContainer: TwoColumnContainer,
+};
