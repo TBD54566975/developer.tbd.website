53a1182fc1bd2dadd866a981522eb1b7637a1cb9
Sergio Baptista <sergio.baptista@akqa.com>
2022-07-15 14:36:17 -0400

    fix: fis typo in og image url

--- src/content/global-meta.js
+++ src/content/global-meta.js
@@ -1,7 +1,7 @@
 const metacontent = {
   description:
     'We build decentralized platforms, protocols, and tools that empower every individual to own their data and participate in the global economy.',
-  ogimage: 'https://developer.tdb.website/img/tbd_og_meta_image.png',
+  ogimage: 'https://developer.tbd.website/img/tbd_og_meta_image.png',
   site_name: 'TBD',
 };
 
