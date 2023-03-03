805b70b0b35eff4274bce800c2df05a9c9a41402
Nick DeJesus <ndejesus1227@gmail.com>
2023-03-03 15:16:57 -0500

    remove extra "posted by"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,6 +72,8 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          blogSidebarTitle: 'All posts',
+          blogSidebarCount: 'ALL',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
