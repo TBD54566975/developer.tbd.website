e4cec96799a3f0090e4c848b702b6d5cfae802a6
Nick DeJesus <ndejesus1227@gmail.com>
2023-04-05 11:34:32 -0500

    Set up community component

---
+++ src/components/Community.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Community() {
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      <div>
+        <div>Icon</div>
+        <div>Title</div>
+        <p>Call to Action</p>
+      </div>
+    </div>
+  );
+}
+
+export default Community;
