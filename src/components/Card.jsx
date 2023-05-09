08651c9ee2f01409825c409ff1d40f7aec6c58c7
Nick DeJesus <ndejesus1227@gmail.com>
2023-05-09 14:40:53 -0400

    added expandability

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Card({ children, className }) {
   return (
     <div
-      className={`${className} flex flex-col bg-transparent border-[#282828] border-2 shadow overflow-hidden sm:rounded-lg lg:p-10 sm:p-5`}
+      className={`flex flex-col bg-transparent border-[#282828] border-2 shadow overflow-hidden sm:rounded-lg lg:p-10 sm:p-5 ${className}`}
     >
       {children}
     </div>
--- src/components/FeedbackWidget.jsx
+++ src/components/FeedbackWidget.jsx
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Card from './Card';
 
-function FeedbackWidget({ EditThisPage }) {
+function FeedbackWidget() {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const expandableContentRef = useRef(null);
+
+  useEffect(() => {
+    if (isExpanded) {
+      expandableContentRef.current.classList.add('expandable-content-expanded');
+    } else {
+      expandableContentRef.current.classList.remove(
+        'expandable-content-expanded',
+      );
+    }
+  }, [isExpanded]);
+
+  const handleExpandToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  console.log('isExanded', isExpanded);
+
   return (
-    <div className="flex justify-end lg:mb-0">
-      <Card className={'w-2/4'}>
-        <h3>Was this page helpful?</h3>
-        <p>
-          Connect with us in{' '}
-          <a href="https://discord.com/invite/tbd">Discord</a>
-        </p>
-        <p>
-          Submit feedback:{' '}
-          <a href="https://github.com/TBD54566975/developer.tbd.website/issues/new/choose">
-            Open a GitHub issue
-          </a>
-        </p>
+    <div className="flex-col justify-start">
+      <Card className={'w-1/3 border-accent-purple lg:p-4'}>
+        <h3 className="m-0">Was this page helpful?</h3>
+        <button
+          className={`expand-btn pt-4 animate-pulse p-4 ${
+            isExpanded ? 'expandable-content-expanded' : ''
+          }`}
+          onClick={handleExpandToggle}
+          type="button"
+        >
+          <img src="/img/arrow-blue.svg" />
+        </button>
+        <div className={`expandable-content`} ref={expandableContentRef}>
+          <div className="mb-4">
+            <p>Connect with us:</p>
+            <a href="https://discord.com/invite/tbd">Discord</a>
+          </div>
+          <p>
+            <div>
+              <p>Submit feedback:</p>
+              <a href="https://github.com/TBD54566975/developer.tbd.website/issues/new/choose">
+                Open a GitHub issue
+              </a>
+            </div>
+          </p>
+        </div>
       </Card>
     </div>
   );
