d0b3b10f22697194a54f57d5e978bb70322f15f4
Nick DeJesus <ndejesus1227@gmail.com>
2023-05-10 15:48:41 -0400

    remove unneeded code

--- src/components/FeedbackWidget.jsx
+++ src/components/FeedbackWidget.jsx
@@ -2,25 +2,6 @@ import React, { useState, useRef, useEffect } from 'react';
 import Card from './Card';
 
 function FeedbackWidget() {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const expandableContentRef = useRef(null);
-
-  useEffect(() => {
-    if (isExpanded) {
-      expandableContentRef.current.classList.add('expandable-content-expanded');
-    } else {
-      expandableContentRef.current.classList.remove(
-        'expandable-content-expanded',
-      );
-    }
-  }, [isExpanded]);
-
-  const handleExpandToggle = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  console.log('isExanded', isExpanded);
-
   return (
     <div className="flex justify-end lg:mb-0">
       <Card className={'w-full'}>
