6529e7b7381b6e8b3d5759bb6626c67c312c4c34
Kirah Sapong <kirah@tbd.email>
2023-05-16 23:27:46 -0700

    Updates to Getting Started page

--- src/components/ExploreCard.jsx
+++ src/components/ExploreCard.jsx
@@ -4,14 +4,14 @@ function ExploreCard({ text, icon, url, imgClass }) {
   return (
     <a
       href={url}
-      className="no-underline w-70 h-56 border-[#282828] border-2 rounded-lg flex flex-col justify-between items-center"
+      className="explore-card no-underline w-70 h-56 border-[#282828] border-2 rounded-lg flex flex-col justify-between items-center"
       target="_blank"
     >
       {icon && text && (
-        <img className={`h-1/2 p-4 tbd-yellow-illustration ${imgClass}`} alt={text} src={icon} />
+        <img className={`m-auto h-1/2 p-4 tbd-yellow-illustration ${imgClass}`} alt={text} src={icon} />
       )}
-      <div className="flex justify-between p-8 bg-[#282828] w-full">
-        <div>{text}</div>
+      <div className="flex justify-between px-4 py-6 bg-[#282828] w-full">
+        <h3>{text}</h3>
       </div>
     </a>
   );
--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -10,25 +10,29 @@ function HeroCard({
   primary = false,
   themeColor = 'cyan',
   imgSrc,
-  imgClass = 'tbd-blue-illustration w-32',
+  imgClass = 'w-32',
 }) {
   return (
     <div
-      className={`${primary ? `primary-theme-card`: ``} theme-card theme-card-${themeColor} relative p-8 rounded-xl shadow-lg tablet:w-full desktop:w-full only:w-full my-8 min-h-[240px] flex flex-col justify-between`}
+      className={`${primary ? `primary-theme-card `: ``}theme-card theme-card-${themeColor} relative p-8 rounded-xl shadow-lg tablet:w-full desktop:w-full only:w-full min-h-[240px] flex justify-between`}
     >
-      {imgSrc && (
-        <img
-          className={`px-2 absolute bottom-0 right-0 hidden md:block padding-bottom--lg ${imgClass}`}
-          src={imgSrc}
-          />
-      )}
+        {imgSrc && (
+            <img
+            className={`px-2 absolute bottom-8 right-4 hidden md:block ${imgClass}`}
+            src={imgSrc}
+            />
+        )}
 
-      <h2 className="text-2xl font-bold mb-4">{heroText}</h2>
-      <p className="w-2/3">{bodyText}</p>
-      <div className="max-w-[240px] hero-button">
-        <Link href={buttonUrl}>
-          <Button label={buttonText} url={buttonUrl} colorDarkMode={themeColor}/>
-        </Link>
+      <div className={`theme-card-content`}>
+        {primary ? 
+            <h2 className="text-2xl font-bold mb-4">{heroText}</h2> 
+            :
+            <h3 className="text-2xl font-bold mb-4">{heroText}</h3>
+        }
+        <p className="mb-8">{bodyText}</p>
+        <div className="max-w-[240px] hero-button">
+            <Button label={buttonText} url={buttonUrl} colorDarkMode={themeColor}/>
+        </div>
       </div>
     </div>
   );
