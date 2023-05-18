1c950ebdd63cb12461e96b096c4f2931388e725a
Andrew Lee Rubinger <alr@alrubinger.com>
2023-05-18 15:20:44 -0400

    Revert "Set up feedback widget (#449)"
    This reverts commit 413ad8751fdfcd688c8c351ea61fa5ae8ef2b099.

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -2,12 +2,28 @@ import React, { useEffect } from 'react';
 import Layout from '@theme-original/Layout';
 import GlitchWrapper from '@site/src/components/GlitchWrapper';
 import MDXContent from '@theme/MDXContent';
+import { useLocation } from '@docusaurus/router';
+
+export function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.body.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 export default function LayoutWrapper(props) {
+  const { pathname } = useLocation();
+
   return (
     <GlitchWrapper>
       <MDXContent>
-        <Layout {...props}>{props.children}</Layout>
+        <Layout {...props}>
+          <ScrollToTop />
+          {props.children}
+        </Layout>
       </MDXContent>
     </GlitchWrapper>
   );
