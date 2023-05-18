88276525f3ae7fa1d15fa6c6807217da20ae318b
Andrew Lee Rubinger <alr@alrubinger.com>
2023-05-18 15:20:44 -0400

    Revert "Issue 439/design pass (#473)"
    This reverts commit 3243449ac5a4beaf216542889ec60979f041bd61.

--- src/components/ApiCard.jsx
+++ src/components/ApiCard.jsx
@@ -5,7 +5,7 @@ import Link from '@docusaurus/Link';
 function ApiCard({ url, name, description }) {
   let shouldOpenInNewTab = url.includes('http') ? '_blank' : '_self';
   return (
-    <div className="api-card flex flex-col bg-transparent border-2 shadow overflow-hidden sm:rounded-sm px-4 py-5 sm:px-6">
+    <div className="flex flex-col bg-transparent border-gray-400 border-2 shadow overflow-hidden sm:rounded-lg px-4 py-5 sm:px-6">
       <Link href={url} className="no-underline">
         <div className="flex-col flex">
           <div target={shouldOpenInNewTab}>
@@ -13,7 +13,7 @@ function ApiCard({ url, name, description }) {
           </div>
         </div>
       </Link>
-      {description && <p class="copy">{description}</p>}
+      {description && <p className="text-primary-white">{description}</p>}
     </div>
   );
 }
