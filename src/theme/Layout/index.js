e27be055bdf30ef3a18ec70f261393fd9c53a13a
kirahsapong <102400653+kirahsapong@users.noreply.github.com>
2023-05-19 11:36:17 -0700

    Add hook to wait for component to mount before scrolling element to view (#487)
    * Add hook to wait for component to mount before scrolling element to view
    
    * Hide logic behind if statement

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -2,13 +2,24 @@ import React, { useEffect } from 'react';
 import Layout from '@theme-original/Layout';
 import GlitchWrapper from '@site/src/components/GlitchWrapper';
 import MDXContent from '@theme/MDXContent';
+import { useLocation } from '@docusaurus/router';
 
 export default function LayoutWrapper(props) {
-  return (
+    const { hash } = useLocation();
+    useEffect(() => {
+        if (hash) {
+            function scrollToAnchor() {
+                document.getElementById(hash.substring(1))?.scrollIntoView();
+                clearTimeout(timeout);
+            }
+            const timeout = setTimeout(scrollToAnchor, 0);
+        }
+    }, []);
+    return (
     <GlitchWrapper>
-      <MDXContent>
+        <MDXContent>
         <Layout {...props}>{props.children}</Layout>
-      </MDXContent>
+        </MDXContent>
     </GlitchWrapper>
-  );
+    );
 }
