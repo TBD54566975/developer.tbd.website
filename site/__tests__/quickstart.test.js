e2171309cbcdfbfec840f3c6f70f3eb9b01bffec
Nick DeJesus <ndejesus1227@gmail.com>
2023-07-27 12:07:54 -0400

    adding comments to some tests

--- site/__tests__/quickstart.test.js
+++ site/__tests__/quickstart.test.js
@@ -1,20 +1,24 @@
 import { test, beforeAll, expect, describe } from 'vitest';
 import { didCreate, dwnWriteTextRecord } from '../src/util/web5';
 
+// This is the web5 instance that will be referred to for all tests. This comes back as a result from Web5.connect() being used in the didCreate function.
 let web5;
+// This is the decentralized ID that will be referred to for all tests. This comes back as a result from Web5.connect() being used in the didCreate function.
 let aliceDid;
+// This record result is what comes back from the dwnWriteTextRecord function. This is used to test the record's attributes and methods.
 let recordResult;
 
 const textInput = 'YEEERR!';
 const updatedTextInput = 'YEEERR! UPDATED';
 
-beforeAll(async () => {
-  const result = await didCreate();
-  web5 = result.web5;
-  aliceDid = result.aliceDid;
-});
-
 describe('/site/tests/quickstart.test.js', () => {
+  // This is where we create a DID, assign the web5 and aliceDid variables, and then use the aliceDid to write a text record.
+  beforeAll(async () => {
+    const result = await didCreate();
+    web5 = result.web5;
+    aliceDid = result.aliceDid;
+  });
+
   test('didCreate returns a decentralized ID', async () => {
     expect(typeof aliceDid).toBe('string');
     const didRegex = /^did:[a-z0-9]+:.+/i;
@@ -22,6 +26,7 @@ describe('/site/tests/quickstart.test.js', () => {
   });
 
   test('dwnWriteTextRecord returns a record with aliceDid being the same value as the author attribute', async () => {
+    // This is where we write a text record and assign the result to the recordResult variable.
     const record = await dwnWriteTextRecord(web5, textInput);
     recordResult = record;
     expect(record.author).toBe(aliceDid);
