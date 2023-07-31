2d2563134e4315f21e3aac044e9806362991284b
Nick DeJesus <ndejesus1227@gmail.com>
2023-07-31 15:51:20 -0400

    looking into coverage options, will uncomment this later

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    //TODO: Investigate coverage options later for output files
+    // coverage: {
+    //   provider: 'istanbul',
+    //   enabled: true,
+    //   reporter: 'html'
+    // },
     browser: {
       name: 'chrome',
       enabled: true,
