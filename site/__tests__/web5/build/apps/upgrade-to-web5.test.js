33935edd04c6133a1be0ca2460e4c88f3e75759e
Nick DeJesus <ndejesus1227@gmail.com>
2023-08-23 14:13:05 -0400

    add snippet for upgrade-to-web5

---
+++ site/__tests__/web5/build/apps/upgrade-to-web5.test.js
@@ -0,0 +1,8 @@
+import { test, expect } from 'vitest';
+import { createAliceDid } from '../../../../code-snippets/web5/build/apps/upgrade-to-web5';
+
+test('read result comes back from creating alice did', async () => {
+  const readResult = await createAliceDid();
+  console.log(readResult);
+  expect(readResult).toBe('Hello Web5');
+});
--- site/src/components/CodeSnippet.jsx
+++ site/src/components/CodeSnippet.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import codeSnippets from '../../src/util/code-snippets-map.json';
 import CodeBlock from '@theme/CodeBlock';
 
-const CodeSnippet = ({ functionName }) => {
+const CodeSnippet = ({ functionName, importString }) => {
   const snippet = codeSnippets[functionName];
 
   if (!snippet) {
     return <p>Error: Code snippet not found for {functionName}.</p>;
   }
 
-  return <CodeBlock language="js">{snippet}</CodeBlock>;
+  return (
+    <CodeBlock language="js">
+      {importString ? `${importString}\n\n${snippet}` : snippet}
+    </CodeBlock>
+  );
 };
 
 export default CodeSnippet;
