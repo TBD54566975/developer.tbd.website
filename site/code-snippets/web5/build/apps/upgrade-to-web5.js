e58c869a9a2a85c3ab5641525291f849a2654c8c
Nick DeJesus <ndejesus1227@gmail.com>
2023-08-23 14:13:05 -0400

    add snippet for upgrade-to-web5

---
+++ site/code-snippets/web5/build/apps/upgrade-to-web5.js
@@ -0,0 +1,18 @@
+import { Web5 } from '@tbd54566975/web5/browser';
+
+export async function createAliceDid() {
+  const { web5, did: aliceDid } = await Web5.connect();
+
+  // Create the record
+  const { record } = await web5.dwn.records.create({
+    data: 'Hello Web5',
+    message: {
+      dataFormat: 'text/plain',
+    },
+  });
+
+  // Read the record
+  const readResult = await record.data.text();
+
+  return readResult;
+}
