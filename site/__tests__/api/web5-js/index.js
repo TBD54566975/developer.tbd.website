89289cb557e7a86cb7ff0ca9f869e5df84b7a9c6
Nick DeJesus <ndejesus1227@gmail.com>
2023-08-24 15:13:11 -0400

    unfinished test

--- site/__tests__/api/web5-js/index.js
+++ site/__tests__/api/web5-js/index.js
@@ -1,8 +1,11 @@
 import { test, expect } from 'vitest';
 import { createDidWithDWNEndpoint } from '../../../code-snippets/api/web5-js';
 
-test('result comes back with web5 including dwnEndpoint in example', async () => {
-  const result = await createDidWithDWNEndpoint();
+test.todo(
+  'result comes back with web5 including dwnEndpoint in example',
+  async () => {
+    const result = await createDidWithDWNEndpoint();
 
-  console.log(result);
-});
+    console.log(result);
+  },
+);
