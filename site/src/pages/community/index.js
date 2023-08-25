b2c41a0f4f2c7e086219793f81bd797eed47ae6f
Ebony <ebony774@gmail.com>
2023-08-25 18:35:48 -0400

    rearranging the order

--- site/src/pages/community/index.js
+++ site/src/pages/community/index.js
@@ -8,12 +8,98 @@ import contributorsData from '@site/src/contributors.json';
 function CommunityIndex() {
   return (
     <Layout>
-      <div>
-        <h1>Welcome to your TBD community!</h1>
-        <p className="mb-8 justify-center">
-          Join community members from all over the world and lets advance the
-          discovery of decentralized applications together
+      <h1>Welcome to your TBD community!</h1>
+      {/* <p className="mb-8 justify-center">
+        Join community members from all over the world and lets advance the
+        discovery of decentralized applications together
+      </p> */}
+      {/* contributor spotlight section */}
+      <div className="pb-11">
+        <h2 className="pt-6">TBD Spotlight</h2>
+        <p className="pb-8">
+          Shoutout to our superstar contributors! 🌟 Whether you're coding,
+          brainstorming, or cheering us on, every contribution adds magic to our
+          community. Ready to contribute? We're looking forward to seeing your
+          name light up this space soon! 🚀
         </p>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+            gap: '20px',
+            maxHeight: '450px',
+            overflowY: 'auto',
+            padding: '10px 0',
+            width: '100%',
+            marginBottom: '40px',
+          }}
+        >
+          {contributorsData.map((contributor, index) => (
+            <div
+              key={index}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: '10px',
+                boxShadow: '0 4px 8px rgba(33, 241, 255, 0.2)',
+                borderRadius: '8px',
+                padding: '20px',
+                backgroundColor: '#1C1C1C',
+                width: '200px',
+                textAlign: 'center',
+                transition: 'all 0.3s ease',
+              }}
+              onMouseEnter={(e) => {
+                e.currentTarget.style.boxShadow =
+                  '0 8px 16px rgba(33, 241, 255, 0.4)';
+                e.currentTarget.style.transform = 'translateY(-5px)';
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.style.boxShadow =
+                  '0 4px 8px rgba(33, 241, 255, 0.2)';
+                e.currentTarget.style.transform = 'translateY(0)';
+              }}
+            >
+              <img
+                src={contributor.avatar_url}
+                alt={contributor.login}
+                style={{
+                  width: '80px',
+                  borderRadius: '50%',
+                }}
+              />
+              <span style={{ color: '#21F1FF' }}>{contributor.login}</span>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  flexWrap: 'wrap',
+                  gap: '5px',
+                }}
+              >
+                {contributor.contributions.map((contribution, cIndex) => (
+                  <span
+                    key={cIndex}
+                    style={{
+                      padding: '2px 5px',
+                      borderRadius: '4px',
+                      backgroundColor: '#FFEC18',
+                      fontSize: '0.8em',
+                      color: 'black',
+                    }}
+                  >
+                    {contribution}
+                  </span>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
         <div className="grid grid-cols-1 desktop:grid-cols-2 gap-4 pt-7 pb-20">
           <HeroCard
             heroText="Contributor Guide"
@@ -61,93 +147,6 @@ function CommunityIndex() {
               channelName="WEB5"
               style={{ flex: 1, marginLeft: '5px' }}
             />
-          </div>
-        </div>
-
-        {/* contributor spotlight section */}
-        <div className="pb-11">
-          <h2 className="pt-6">TBD Spotlight</h2>
-          <p className="pb-8">
-            Shoutout to our superstar contributors! 🌟 Whether you're coding,
-            brainstorming, or cheering us on, every contribution adds magic to
-            our community. Ready to contribute? We're looking forward to seeing
-            your name light up this space soon! 🚀
-          </p>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              justifyContent: 'center',
-              gap: '20px',
-              maxHeight: '350px',
-              overflowY: 'auto',
-              padding: '10px 0',
-              width: '100%',
-              marginBottom: '40px',
-            }}
-          >
-            {contributorsData.map((contributor, index) => (
-              <div
-                key={index}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  gap: '10px',
-                  boxShadow: '0 4px 8px rgba(33, 241, 255, 0.2)',
-                  borderRadius: '8px',
-                  padding: '20px',
-                  backgroundColor: '#1C1C1C',
-                  width: '200px',
-                  textAlign: 'center',
-                  transition: 'all 0.3s ease',
-                }}
-                onMouseEnter={(e) => {
-                  e.currentTarget.style.boxShadow =
-                    '0 8px 16px rgba(33, 241, 255, 0.4)';
-                  e.currentTarget.style.transform = 'translateY(-5px)';
-                }}
-                onMouseLeave={(e) => {
-                  e.currentTarget.style.boxShadow =
-                    '0 4px 8px rgba(33, 241, 255, 0.2)';
-                  e.currentTarget.style.transform = 'translateY(0)';
-                }}
-              >
-                <img
-                  src={contributor.avatar_url}
-                  alt={contributor.login}
-                  style={{
-                    width: '80px',
-                    borderRadius: '50%',
-                  }}
-                />
-                <span style={{ color: '#21F1FF' }}>{contributor.login}</span>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    flexWrap: 'wrap',
-                    gap: '5px',
-                  }}
-                >
-                  {contributor.contributions.map((contribution, cIndex) => (
-                    <span
-                      key={cIndex}
-                      style={{
-                        padding: '2px 5px',
-                        borderRadius: '4px',
-                        backgroundColor: '#FFEC18',
-                        fontSize: '0.8em',
-                        color: 'black',
-                      }}
-                    >
-                      {contribution}
-                    </span>
-                  ))}
-                </div>
-              </div>
-            ))}
           </div>
         </div>
         <Community />
