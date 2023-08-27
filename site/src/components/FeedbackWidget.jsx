1b69dffb8e014084c94125d7d7b3b0eccd9f1dc0
Shodipo Ayomide <shodipovi@gmail.com>
2023-08-27 16:26:01 +0100

    [feat] add `helpful/not Helpful` to the feedback widget

--- site/src/components/FeedbackWidget.jsx
+++ site/src/components/FeedbackWidget.jsx
@@ -1,12 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
 import Link from '@docusaurus/Link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+
+import '../css/feedback.css';
+
+function RatingButton({ label, icon, onClick }) {
+  return (
+    <div className="rating-box" onClick={onClick}>
+      {label}
+      <FontAwesomeIcon icon={icon} className="icon" />
+    </div>
+  );
+}
 
 function FeedbackWidget() {
+  const [userFeedback, setUserFeedback] = useState(null);
+  const [csrfToken, setCsrfToken] = useState('');
+
+  useEffect(() => {
+    fetch('https://tbd-feedback-spreadsheet-server.vercel.app/api/csrf-token', { 
+      credentials: 'include'
+    })
+      .then(res => res.json())
+      .then(data => setCsrfToken(data.csrfToken));
+  }, []);
+
+  const handleUserRatingClick = (rating) => {
+    setUserFeedback(rating);
+    const requestBody = {
+      rating: rating === 'like' ? 'helpful' : 'notHelpful',
+      pageLink: window.location.href,
+    };
+    console.log('Sending request:', requestBody);
+    fetch('https://tbd-feedback-spreadsheet-server.vercel.app/api/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'csrf-token': csrfToken
+      },
+      credentials: 'include',
+      body: JSON.stringify(requestBody),
+    });
+  };
+
   return (
     <div className="flex justify-end lg:mt-8">
-      <Card className={'w-full'}>
-        <h3 className="text-3xl">Was this page helpful?</h3>
+      <Card className="w-full">
+        <h3 className="text-3xl feedback-header">Was this page helpful?</h3>
+        <div className="rating">
+          {userFeedback ? (
+            <>
+              <span>Thank you for your feedback!</span>
+              <FontAwesomeIcon
+                icon={userFeedback === 'like' ? faThumbsUp : faThumbsDown}
+                className={userFeedback ? 'active-icon' : 'icon'}
+              />
+            </>
+          ) : (
+            <>
+              <RatingButton
+                label="Helpful"
+                icon={faThumbsUp}
+                onClick={() => handleUserRatingClick('like')}
+              />
+              <RatingButton
+                label="Not Helpful"
+                icon={faThumbsDown}
+                onClick={() => handleUserRatingClick('dislike')}
+              />
+            </>
+          )}
+        </div>
+
         <p>
           Connect with us on{' '}
           <a href="https://discord.com/invite/tbd">Discord</a>
