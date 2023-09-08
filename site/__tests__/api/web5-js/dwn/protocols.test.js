3f42b2e9bdff99563f0035caf8a24b5f92a5ccb6
Nick DeJesus <ndejesus1227@gmail.com>
2023-09-08 17:09:19 -0400

    set up snippets and tests for record in API docs. Get rid of protocol files

--- site/__tests__/api/web5-js/dwn/protocols.test.js
+++
@@ -1,5 +0,0 @@
-import { test, beforeAll, expect, describe } from 'vitest';
-
-let web5;
-let aliceDid;
-let recordResult;
--- site/__tests__/api/web5-js/dwn/record.test.js
+++ site/__tests__/api/web5-js/dwn/record.test.js
@@ -1,5 +1,38 @@
 import { test, beforeAll, expect, describe } from 'vitest';
+import {
+  createRecord,
+  deleteRecord,
+  updateRecord,
+} from '../../../../code-snippets/api/web5-js/dwn/record';
+import { Web5 } from '@web5/api/browser';
 
 let web5;
-let aliceDid;
-let recordResult;
+let record;
+let myDid;
+
+beforeAll(async () => {
+  const result = await Web5.connect();
+  web5 = result.web5;
+  myDid = result.did;
+});
+
+describe('tests for /api/web5-js/dwn/record', async () => {
+  test('createRecord creates a record', async () => {
+    const result = await createRecord(web5, myDid);
+    record = result;
+
+    expect(result).toBeDefined();
+  });
+
+  test('updateRecord updates the created record', async () => {
+    const result = await updateRecord(record);
+
+    expect.soft(result.status.code).toBe(202);
+  });
+
+  test('deleteRecord deletes the updated record', async () => {
+    const result = await deleteRecord(record);
+
+    expect.soft(result.status.code).toBe(202);
+  });
+});
