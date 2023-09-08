4a87c3664750d8b72055d8a68b635d59761a0f1a
Nick DeJesus <ndejesus1227@gmail.com>
2023-09-08 10:56:45 -0400

    add longer tests

---
+++ site/__tests__/api/web5-js/dwn/record.test.js
@@ -0,0 +1,5 @@
+import { test, beforeAll, expect, describe } from 'vitest';
+
+let web5;
+let aliceDid;
+let recordResult;
--- site/code-snippets/api/web5-js/did.js
+++ site/code-snippets/api/web5-js/did.js
@@ -1,5 +1,6 @@
 export async function createMyDid(web5) {
-  const myDid = await web5.did.create('ion');
+  console.log('web5', web5);
+  // const myDid = await web5.did.create('ion');
 
   return myDid;
 }
