ad41591493b635f4cb69b6c667caf4e3074b8211
Sumit Upadhyay <skumarupadhyay1@gmail.com>
2023-09-28 23:45:12 +0530

    same commit

--- site/src/components/Card.jsx
+++ site/src/components/Card.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Card({ children, className }) {
   return (
     <div
-      className={`flex flex-col bg-transparent border-[#282828] border-2 shadow overflow-hidden sm:rounded-lg p-8 lg:p-10  sm:p-5 ${className}`}
+      className={`flex flex-col bg-transparent border-[#282828] border-2 shadow overflow-hidden sm:rounded-lg p-8 lg:p-10 sm:p-5 ${className}`}
     >
    
       {children}
