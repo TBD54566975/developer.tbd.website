906f0e33e3a628cced1e6eed1878c82c9f0237da
Leo Ribeiro <leonardo@squareup.com>
2023-09-30 01:22:21 -0400

    fix csrf and simplify db

--- apps/feedback-server/server.js
+++ apps/feedback-server/server.js
@@ -2,22 +2,17 @@ require('dotenv').config();
 
 const express = require('express');
 
+const { doubleCsrf } = require('csrf-csrf');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const { Pool: DbPool } = require('pg');
+
+const { db } = require('./db');
 
 const config = {
+  devMode: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
   port: process.env.PORT || 3001,
   serverSecret: process.env.SERVER_SECRET,
   serverAllowedOrigins: (process.env.SERVER_ALLOWED_ORIGINS || '').split(','),
-  db: {
-    address: process.env.DB_CONNECTION,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-  },
 };
 
 console.info('Server secret:', {
@@ -29,49 +24,8 @@ console.info('Server secret:', {
   },
 });
 
-const dbPool = new DbPool({
-  host: config.db.address,
-  user: config.db.user,
-  password: config.db.password,
-  port: config.db.port,
-  database: config.db.database,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
-
-const initDb = async () => {
-  await dbPool.query(`
-    CREATE TABLE IF NOT EXISTS feedback_votes (
-      id SERIAL PRIMARY KEY,
-      url VARCHAR(255) NOT NULL,
-      vote CHAR NOT NULL,
-      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
-  `);
-};
-
-const storeVote = async (url, vote) => {
-  // insert vote into feedback_votes table
-  const res = await dbPool.query(
-    'INSERT INTO feedback_votes (url, vote) VALUES ($1, $2);',
-    [url, vote],
-  );
-  console.info('>>> Inserted vote:', res.rows);
-};
-
 // Init express app
 const app = express();
-
-// Init session
-app.use(
-  session({
-    secret: config.serverSecret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true, sameSite: 'Strict' },
-  }),
-);
 
 // Init CORS with options
 const corsOptions = {
@@ -80,7 +34,7 @@ const corsOptions = {
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Oops! Not allowed by CORS, I am so sorry :( hehehe'));
+      callback(new Error('Oops! Not allowed by CORS'));
     }
   },
   methods: 'GET,POST',
@@ -89,13 +43,28 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json());
+
+// Setup CSRF Config
+const doubleCsrfOptions = {
+  getSecret: () => config.serverSecret,
+  cookieName: `${config.devMode ? 'dev' : '__Host'}-psifi.x-csrf-token`,
+  cookieOptions: {
+    sameSite: 'strict',
+    path: '/',
+    secure: !config.devMode,
+  },
+  getTokenFromRequest: (req) => req.headers['x-csrf-token'],
+};
+const { generateToken, doubleCsrfProtection } = doubleCsrf(doubleCsrfOptions);
 
 app.get('/api/csrf-token', (req, res) => {
-  res.json({ csrfToken: req.csrfToken });
+  const csrfToken = generateToken(req, res);
+  res.json({ csrfToken });
 });
+
+app.use(doubleCsrfProtection);
 
 app.post('/api/feedback', async (req, res) => {
   console.log('Received request:', req.body);
@@ -110,9 +79,9 @@ app.post('/api/feedback', async (req, res) => {
   }
 
   try {
-    // helpful = H, notHelpful = N, noVote = space
-    const voteValue = rating === 'helpful' ? 'H' : 'notHelpful' ? 'N' : ' ';
-    storeVote(pageLink, voteValue);
+    // helpful = Y, notHelpful = N
+    const voteValue = rating === 'helpful' ? 'Y' : 'N';
+    db.storeVote(pageLink, voteValue);
     console.log('Sending response...');
     res.json({ message: 'Feedback collected successfully!' });
   } catch (error) {
@@ -123,7 +92,7 @@ app.post('/api/feedback', async (req, res) => {
   }
 });
 
-initDb().then(() => {
+db.initDb().then(() => {
   app.listen(config.port, () => {
     console.log(`Server running at http://localhost:${config.port}`);
   });
