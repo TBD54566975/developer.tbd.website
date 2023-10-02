ccf7fa5171918d48c683e11011e2b05bb187a7b0
captain-Akshay <akshay.sharma0503@gmail.com>
2023-10-02 12:31:27 +0530

    explore card hover effect added
    Signed-off-by: captain-Akshay <akshay.sharma0503@gmail.com>

--- site/src/components/ExploreCard.jsx
+++ site/src/components/ExploreCard.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ExploreCard({ text, icon, url, imgClass }) {
+  // Create a state variable to control the box shadow
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <a
       href={url}
-      className="explore-card no-underline w-70 h-56 border-[#282828] border-2 rounded-sm flex flex-col justify-between items-center"
+      className={`explore-card no-underline w-70 h-56 border-[#282828] border-2 rounded-sm flex flex-col justify-between items-center transition-transform transform ${isHovered ? 'hover:-translate-y-1' : ''}`}
+      style={{ boxShadow: isHovered ? '0 4px 8px rgba(33, 241, 255, 0.7)' : 'none' }}
       target="_blank"
+      onMouseEnter={() => setIsHovered(true)} // Set isHovered to true on hover
+      onMouseLeave={() => setIsHovered(false)} // Set isHovered to false on mouse leave
     >
       {icon && text && (
         <img className={`m-auto h-1/2 p-4 tbd-yellow-illustration ${imgClass}`} alt={text} src={icon} />
