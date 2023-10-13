06d7592e6324929ccdd9c04812ff6cab3485bac0
shavon harris <shavon.harris@trellix.com>
2023-10-13 13:26:25 -0500

    created a ssiservice component

---
+++ site/src/components/SSIComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SSIService = () => {
+    return (
+        <div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+            </div>
+            <h2><a>Prerequisites</a></h2>
+            <p>Follow guide to <a href="run-ssi-service">Clone & Run SSI Service.</a></p>
+        </div>
+    );
+};
+
+export default SSIService;
