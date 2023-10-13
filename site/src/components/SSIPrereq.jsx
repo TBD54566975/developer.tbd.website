92e301bf728238fe58730f86649f548d8db7b6e0
shavon harris <shavon.harris@trellix.com>
2023-10-13 16:02:31 -0500

    change component name

--- site/src/components/SSIPrereq.jsx
+++ site/src/components/SSIPrereq.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const SSIService = () => {
+const SSIPrereq = () => {
     return (
         <div>
             <div style={{ display: 'flex', alignItems: 'center' }}>
             </div>
-            <h2><a>Prerequisites</a></h2>
+            <h2>Prerequisites<a href="create-did#prerequisites" class="hash-link"></a></h2>
             <p>Follow guide to <a href="run-ssi-service">Clone & Run SSI Service.</a></p>
         </div>
     );
 };
 
-export default SSIService;
+export default SSIPrereq;
