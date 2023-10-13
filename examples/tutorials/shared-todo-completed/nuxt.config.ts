fd1645d1e84af30e36beb3569dfecaa2e2838d66
Adewale Abati <acekyd01@gmail.com>
2023-10-13 19:58:52 +0200

    Turn off telemetry

--- examples/tutorials/shared-todo-completed/nuxt.config.ts
+++ examples/tutorials/shared-todo-completed/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  telemetry: false,
   modules: ["@nuxtjs/tailwindcss"],
   tailwindcss: {
     cssPath: '~/assets/css/main.css',
