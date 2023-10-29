0a90bb82a94afba5c49e561c99dbbabba84a2430
Christine Belzie <105683440+CBID2@users.noreply.github.com>
2023-10-29 19:21:58 -0400

    fix: changed function
    Co-authored-by: Angie Jones <jones.angie@gmail.com>

--- site/code-snippets/web5/build/decentralized-web-nodes/ update-dwn.js
+++ site/code-snippets/web5/build/decentralized-web-nodes/ update-dwn.js
@@ -1,4 +1,4 @@
-export async function updateDWN
+export async function updateDwnRecord(web5, createdRecord) {
 const { record } = await web5.dwn.records.read({
   message: {
     recordId: 'bafyreiaz5oycqbrnmmpvffxqyoqxvx6bcnqueprmt2qnvzcurpc52r5uyy',
