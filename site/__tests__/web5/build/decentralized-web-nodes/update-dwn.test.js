021b5e1132c381e1e9551ea383593c35c9f8efb8
Christine <shecoder30@gmail.com>
2023-10-30 01:08:09 +0000

    fix: revise test code

--- site/__tests__/web5/build/decentralized-web-nodes/update-dwn.test.js
+++ site/__tests__/web5/build/decentralized-web-nodes/update-dwn.test.js
@@ -1,16 +1,19 @@
-import {test, beforeAll, expect, describe} from 'vitest';
+import { test, beforeAll, expect } from 'vitest';
 import {
-    updateDwnRecord,
-} from `../../../../code-snippets/web5/build/decentralized-web-nodes/ update-dwn`;
-import { Web5 } from '@web5/api browser';
+  updateDwnRecord,
+} from '../../../../code-snippets/web5/build/decentralized-web-nodes/update-dwn';
+import { Web5 } from '@web5/api/browser';
 
 let web5;
 
 // connect to web5 beforeAll tests and assign it to web5 variable
 beforeAll(async () => {
   ({web5} = await Web5.connect());
-//Create record to update
-const {record: createdRecord} = await web5.dwn.records.create({
+});
+
+test('updateDwnRecord updates an existing record', async () => {
+  //Create record to update
+  const {record: createdRecord} = await web5.dwn.records.create({
     data: "test record",
     message: {
       dataFormat: "text/plain"
@@ -18,7 +21,8 @@ const {record: createdRecord} = await web5.dwn.records.create({
   });
 
   //Call code snippet to update record
-
+  const updateStatus = await updateDwnRecord(web5, createdRecord);
 
   //Assert that status code is 202
+  expect(updateStatus.code).toBe(202);
 });
