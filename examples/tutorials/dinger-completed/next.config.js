4bf9472063e200681047ffc86ec19a65ca2d671f
Rizel Scarlett <rizel@BLKVKH3C6VTXY.local>
2023-11-09 08:35:21 -0800

    updating next config to handle node crypto errors

--- examples/tutorials/dinger-completed/next.config.js
+++ examples/tutorials/dinger-completed/next.config.js
@@ -1,6 +1,31 @@
+const webpack = require('webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  webpack: (config, { isServer, buildId, dev, webpack }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: require.resolve('stream-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+      };
+
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+        }),
+        new webpack.NormalModuleReplacementPlugin(
+          /node:crypto/,
+          (resource) => {
+            resource.request = resource.request.replace(/^node:/, '');
+          }
+        )
+      );
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
