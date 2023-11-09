c7dae8cb5cb71b40838740e2dfbc3d00a3078b5d
Nick DeJesus <ndejesus1227@gmail.com>
2023-11-09 13:53:59 -0500

    apply feedback

--- examples/tutorials/todo-completed/src/App.vue
+++ examples/tutorials/todo-completed/src/App.vue
@@ -17,10 +17,10 @@ onBeforeMount(async () => {
   const { records } = await web5.dwn.records.query({
     message: {
       filter: {
-        schema: 'http://some-schema-registry.org/todo'
+        schema: 'http://some-schema-registry.org/todo',
       },
-      dateSort: 'createdAscending'
-    }
+      dateSort: 'createdAscending',
+    },
   });
 
   // Add entry to Todo array
@@ -36,8 +36,8 @@ const newTodoDescription = ref('');
 
 async function addTodo() {
   const todoData = {
-    completed   : false,
-    description : newTodoDescription.value
+    completed: false,
+    description: newTodoDescription.value,
   };
 
   newTodoDescription.value = '';
@@ -45,11 +45,11 @@ async function addTodo() {
 
   // Create the record.
   const { record } = await web5.dwn.records.create({
-    data    : todoData,
-    message : {
-      schema     : 'http://some-schema-registry.org/todo',
-      dataFormat : 'application/json'
-    }
+    data: todoData,
+    message: {
+      schema: 'http://some-schema-registry.org/todo',
+      dataFormat: 'application/json',
+    },
   });
 
   // add DWeb message recordId as a way to reference the message for further operations
@@ -68,7 +68,7 @@ async function deleteTodo(todoItem) {
       deletedTodo = todo;
       break;
     }
-    index ++;
+    index++;
   }
 
   todos.value.splice(index, 1);
@@ -76,8 +76,8 @@ async function deleteTodo(todoItem) {
   // Delete the record.
   await web5.dwn.records.delete({
     message: {
-      recordId: deletedTodo.id
-    }
+      recordId: deletedTodo.id,
+    },
   });
 }
 
@@ -99,48 +99,70 @@ async function toggleTodoComplete(todoItem) {
   const { record } = await web5.dwn.records.read({
     message: {
       filter: {
-        recordId: toggledTodo.id
-      }
-    }
+        recordId: toggledTodo.id,
+      },
+    },
   });
 
   // Update the record
   await record.update({ data: updatedTodoData });
 }
-
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center min-h-full px-8 py-12 sm:px-6">
+  <div
+    class="flex flex-col items-center justify-center min-h-full px-8 py-12 sm:px-6"
+  >
     <!-- Title -->
     <div class="sm:max-w-md sm:w-full">
-      <h2 class="font-bold text-3xl text-center tracking-tight">
-        Todo List
-      </h2>
+      <h2 class="font-bold text-3xl text-center tracking-tight">Todo List</h2>
+      <div class="flex items-center justify-center">
+        <div v-if="!myDid" id="mydid-container">Connecting to web5 . . .</div>
+        <div v-else id="mydid-container">Web5 Connected</div>
+      </div>
     </div>
-    <div id="mydid-container" v-if="myDid">Web5 Connected</div>
 
     <!-- Add Todo Form -->
     <div class="mt-16">
-      <form class="flex space-x-4" @submit.prevent="addTodo">
+      <!-- The form will only be displayed if myDid is truthy -->
+      <form v-if="myDid" class="flex space-x-4" @submit.prevent="addTodo">
         <div class="border-b border-gray-200 sm:w-full">
           <label for="add-todo" class="sr-only">Add a todo</label>
           <textarea
-            rows="1" name="add-todo" id="add-todo" v-model="newTodoDescription"
+            rows="1"
+            name="add-todo"
+            id="add-todo"
+            v-model="newTodoDescription"
             @keydown.enter.exact.prevent="addTodo"
             class="block border-0 border-transparent focus:ring-0 p-0 pb-2 resize-none sm:text-sm w-96"
-            placeholder="Add a Todo" />
+            placeholder="Add a Todo"
+          />
         </div>
-        <button type="submit" class="bg-indigo-600 border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 hover:bg-indigo-700 inline-flex items-center p-1 rounded-full shadow-sm text-white">
+        <button
+          type="submit"
+          class="bg-indigo-600 border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 hover:bg-indigo-700 inline-flex items-center p-1 rounded-full shadow-sm text-white"
+        >
+          <!-- Make sure PlusIconMini is a registered component or a known element -->
           <PlusIconMini class="h-5 w-5" aria-hidden="true" />
         </button>
       </form>
     </div>
+
     <!-- Todos -->
-    <div v-if="(todos.length > 0)" class="border-gray-200 border-t border-x mt-16 rounded-lg shadow-md sm:max-w-xl sm:mx-auto sm:w-full">
-      <div v-for="todo in todos" :key="todo" class="border-b border-gray-200 flex items-center p-4">
+    <div
+      v-if="todos.length > 0"
+      class="border-gray-200 border-t border-x mt-16 rounded-lg shadow-md sm:max-w-xl sm:mx-auto sm:w-full"
+    >
+      <div
+        v-for="todo in todos"
+        :key="todo"
+        class="border-b border-gray-200 flex items-center p-4"
+      >
         <div @click="toggleTodoComplete(todo)" class="cursor-pointer">
-          <CheckCircleIcon class="h-8 text-gray-200 w-8" :class="{ 'text-green-500': todo.data.completed }" />
+          <CheckCircleIcon
+            class="h-8 text-gray-200 w-8"
+            :class="{ 'text-green-500': todo.data.completed }"
+          />
         </div>
         <div class="font-light ml-3 text-gray-500 text-xl">
           {{ todo.data.description }}
