00ffc30ae49cb9bd9045104a7f4fce85b0ac0faa
Nick DeJesus <ndejesus1227@gmail.com>
2023-11-22 14:26:52 -0500

    Test

--- check-versions.js
+++ check-versions.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+
+function checkVersionRanges(packageJsonPath) {
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+  const packageName = packageJson.name || 'Unknown Package';
+
+  console.log(`Checking ${packageName} at ${packageJsonPath}`);
+
+  ['dependencies', 'devDependencies', 'peerDependencies'].forEach((depType) => {
+    if (packageJson[depType]) {
+      Object.entries(packageJson[depType]).forEach(([dep, version]) => {
+        if (version.includes('^') || version.includes('~')) {
+          throw new Error(
+            `Version range found in ${depType} for ${dep} (${version}) in package: ${packageName}`
+          );
+        }
+      });
+    }
+  });
+}
+
 function findPackageJsonFiles() {
   const directories = ['site', 'examples', 'apps'];
 
@@ -21,4 +44,12 @@ function findPackageJsonFiles() {
   });
 
   checkVersionRanges(path.join(__dirname, 'package.json'));
+}
+
+try {
+  findPackageJsonFiles();
+  console.log('All dependencies are pinned to specific versions.');
+} catch (error) {
+  console.error(error.message);
+  process.exit(1);
 }
