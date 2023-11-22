16dfbbd23d4ae72542dcbe2f6de0ee983b6ecb22
Nick DeJesus <ndejesus1227@gmail.com>
2023-11-22 14:19:48 -0500

    Update to ignore node_modules

--- check-versions.js
+++ check-versions.js
@@ -1,31 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const glob = require('glob');
-
-function checkVersionRanges(packageJsonPath) {
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-
-  console.log(`Checking ${packageJsonPath}`);
-
-  ['dependencies', 'devDependencies', 'peerDependencies'].forEach((depType) => {
-    if (packageJson[depType]) {
-      Object.entries(packageJson[depType]).forEach(([dep, version]) => {
-        if (version.includes('^') || version.includes('~')) {
-          throw new Error(
-            `Version range found in ${depType} for ${dep}: ${version}`
-          );
-        }
-      });
-    }
-  });
-}
-
 function findPackageJsonFiles() {
   const directories = ['site', 'examples', 'apps'];
 
   directories.forEach((directory) => {
-    const directoryPath = path.join(__dirname, directory, '**', 'package.json');
-    const packageJsonFiles = glob.sync(directoryPath);
+    // Exclude node_modules directories in the glob pattern
+    const directoryPath = path.join(
+      __dirname,
+      directory,
+      '**',
+      '!node_modules',
+      '**',
+      'package.json'
+    );
+    const packageJsonFiles = glob.sync(directoryPath, {
+      ignore: '**/node_modules/**',
+    });
 
     packageJsonFiles.forEach((packageJsonPath) => {
       checkVersionRanges(packageJsonPath);
@@ -34,7 +22,3 @@ function findPackageJsonFiles() {
 
   checkVersionRanges(path.join(__dirname, 'package.json'));
 }
-
-findPackageJsonFiles();
-
-console.log('All dependencies are pinned to specific versions.');
