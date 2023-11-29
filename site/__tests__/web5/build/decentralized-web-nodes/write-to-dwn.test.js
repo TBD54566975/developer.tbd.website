5f4eeecb6a75c47d074bc69443385a9b49ba7868
Rizel Scarlett <rizel@tbd.email>
2023-11-29 11:10:21 -0500

    code snippet and test for uploadFile (#1046)
    * code snippet and test for uploadFile
    
    * adding mixed record

--- site/__tests__/web5/build/decentralized-web-nodes/write-to-dwn.test.js
+++ site/__tests__/web5/build/decentralized-web-nodes/write-to-dwn.test.js
@@ -4,6 +4,8 @@ import {
   createTextRecord,
   createJsonRecord,
   uploadImage,
+  uploadFile,
+  createMixedRecord,
 } from '../../../../code-snippets/web5/build/decentralized-web-nodes/write-to-dwn';
 
 let web5;
@@ -27,13 +29,32 @@ describe('write-to-dwn', () => {
   });
 
   test('uploadImage uploads an image', async () => {
-    const fakeEvent = {
+    const mockEvent = {
       currentTarget: {
         files: [new Blob(['fake image data'], { type: 'image/png' })],
       },
     };
 
-    const record = await uploadImage(fakeEvent);
+    const record = await uploadImage(mockEvent);
+    expect(record).toBeDefined();
+  });
+
+  test('uploadFile uploads a file', async () => {
+    const mockEvent = {
+      currentTarget: {
+        files: [new Blob(['fake file data'], { type: 'application/octet-stream' })],
+      },
+    };
+
+    const record = await uploadFile(mockEvent);
+    expect(record).toBeDefined();
+  });
+  test('createMixedRecord creates a message with an image and file', async () => {
+    const username = 'testUser';
+    const messageText = 'testMessage';
+    const imageFile = new Blob(['fake image data'], { type: 'image/png' });
+
+    const record = await createMixedRecord(username, messageText, imageFile);
     expect(record).toBeDefined();
   });
 });
