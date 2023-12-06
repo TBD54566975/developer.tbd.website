9fac04f88894abf704b7f4c11ae423034adc72c0
Nick DeJesus <ndejesus1227@gmail.com>
2023-12-06 13:09:47 -0500

    remove record.delete() from code snippets

--- site/code-snippets/web5/build/decentralized-web-nodes/delete-from-dwn.js
+++ site/code-snippets/web5/build/decentralized-web-nodes/delete-from-dwn.js
@@ -1,17 +1,20 @@
 export async function deleteFromLocalDWN(web5, recordId) {
-//Query records with plain text data format
-const response = await web5.dwn.records.query({
-  message: {
-    filter: {
-      recordId: recordId,
+  //Query records with plain text data format
+  const response = await web5.dwn.records.query({
+    message: {
+      filter: {
+        recordId: recordId,
+      },
     },
-  },
-});
+  });
 
-// Grab the first indexed record
-const record = response.records[0];
+  // Grab the first indexed record
+  const record = response.records[0];
 
-// Delete the record
-const deleteResult = await record.delete();
-return deleteResult;
+  // Delete the record
+  const deleteResult = await web5.dwn.records.delete({
+    from: web5.did,
+    message: { recordId },
+  });
+  return deleteResult;
 }
