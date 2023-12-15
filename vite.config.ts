2890a0469b03cd5868d8b930204c30f63d075b17
Leo Ribeiro <leordev@gmail.com>
2023-12-15 15:09:31 -0500

    checking vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig, configDefaults } from "vitest/config";
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     testTimeout: 40000,
     hookTimeout: 40000,
     teardownTimeout: 40000,
-    exclude: [...configDefaults.exclude, "apps/**", "**/*.spec.{js,ts,jsx,tsx}"],
+    exclude: [
+      ...configDefaults.exclude,
+      'apps/**',
+      '**/*.spec.{js,ts,jsx,tsx}',
+    ],
     //TODO: Investigate coverage options later for output files
     // coverage: {
     //   provider: 'istanbul',
     //   enabled: true,
     //   reporter: 'html'
     // },
-    browser: {
-      name: "chrome",
-      enabled: true,
-      headless: true,
-    },
-    setupFiles: ["./site/__tests__/setup-web5.js"],
+    // browser: {
+    //   name: 'chrome',
+    //   enabled: true,
+    //   headless: true,
+    // },
+    setupFiles: ['./site/__tests__/setup-web5.js'],
   },
 });
