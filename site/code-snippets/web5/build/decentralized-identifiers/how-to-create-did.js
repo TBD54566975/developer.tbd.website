ee2ac354277258f2d0123c3c3fd5bb510b3b7e68
Leo Ribeiro <leordev@gmail.com>
2023-12-19 13:45:17 -0500

    Introduce @web5/dids

--- site/code-snippets/web5/build/decentralized-identifiers/how-to-create-did.js
+++ site/code-snippets/web5/build/decentralized-identifiers/how-to-create-did.js
@@ -1,7 +1,73 @@
 import { Web5 } from '@web5/api';
+import { DidDhtMethod, DidKeyMethod, DidIonMethod } from '@web5/dids';
 
 export async function createDidAutomatically() {
-  const { web5, did: myDid } = await Web5.connect();
+  const { web5, did: userDid } = await Web5.connect();
+  return userDid;
+}
 
-  return myDid;
+export async function createDidDht() {
+  //Creates a DID using the DHT method and publishes the DID Document to the DHT
+  const didDht = await DidDhtMethod.create({ publish: true });
+
+  //DID and its associated data which can be exported and used in different contexts/apps
+  const portableDID = JSON.stringify(didDht);
+
+  //DID string
+  const did = didDht.did;
+
+  //DID Document
+  const didDocument = JSON.stringify(didDht.document);
+
+  //Cryptographic keys associated with DID
+  const keys = JSON.stringify(didDht.keySet);
+
+  //Primary form of a DID. more info: https://www.w3.org/TR/did-core/#dfn-canonicalid
+  const canonicalId = didDht.canonicalId;
+
+  return didDht.did;
+}
+
+export async function createDidKey() {
+  //Creates a DID using the did:key method
+  const didKey = await DidKeyMethod.create();
+
+  //DID and its associated data which can be exported and used in different contexts/apps
+  const portableDID = JSON.stringify(didKey);
+
+  //DID string
+  const did = didKey.did;
+
+  //DID Document
+  const didDocument = JSON.stringify(didKey.document);
+
+  //Cryptographic keys associated with DID
+  const keys = JSON.stringify(didKey.keySet);
+
+  //Primary form of a DID. more info: https://www.w3.org/TR/did-core/#dfn-canonicalid
+  const canonicalId = didKey.canonicalId;
+
+  return didKey.did;
+}
+
+export async function createDidIon() {
+  //Creates a DID using the did:ion method
+  const didIon = await DidIonMethod.create();
+
+  //DID and its associated data which can be exported and used in different contexts/apps
+  const portableDID = JSON.stringify(didIon);
+
+  //DID string
+  const did = didIon.did;
+
+  //DID Document
+  const didDocument = JSON.stringify(didIon.document);
+
+  //Cryptographic keys associated with DID
+  const keys = JSON.stringify(didIon.keySet);
+
+  //Primary form of a DID. more info: https://www.w3.org/TR/did-core/#dfn-canonicalid
+  const canonicalId = didIon.canonicalId;
+
+  return didIon.did;
 }
