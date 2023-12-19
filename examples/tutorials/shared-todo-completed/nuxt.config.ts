f5d0e5df1a28d6dcb2e99a25b94e08c1947b83c0
Leo Ribeiro <leordev@gmail.com>
2023-12-19 13:28:54 -0500

    Removes @web5/api/browser usage (#1091)
    * Removes @web5/api/browser usage
    
    * fix tests
    
    * add polyfills for shared todo example

--- examples/tutorials/shared-todo-completed/nuxt.config.ts
+++ examples/tutorials/shared-todo-completed/nuxt.config.ts
@@ -1,11 +1,16 @@
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   telemetry: false,
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     cssPath: '~/assets/css/main.css',
   },
   devServer: {
-    port: 5175
-  }
-})
+    port: 5175,
+  },
+  vite: {
+    plugins: [nodePolyfills()],
+  },
+});
--- site/__tests__/web5/build/decentralized-identifiers/how-to-create-did.test.js
+++ site/__tests__/web5/build/decentralized-identifiers/how-to-create-did.test.js
@@ -1,12 +1,31 @@
-import { test, expect } from 'vitest';
-import {
-  createDidAutomatically,
-} from '../../../../code-snippets/web5/build/decentralized-identifiers/how-to-create-did';
+import { test, expect, vi, describe, beforeAll } from 'vitest';
+import { createDidAutomatically } from '../../../../code-snippets/web5/build/decentralized-identifiers/how-to-create-did';
 
+let web5;
 
-test('createDidAutomatically returns a DID', async () => {
-  const did = await createDidAutomatically();
+describe('how-to-create-did', () => {
+  beforeAll(async () => {
+    web5 = globalThis.web5;
+    did = globalThis.did;
 
-  expect(did).toBeDefined();
-  expect(did).toMatch(/^did:/);
+    vi.mock('@web5/api', () => {
+      return {
+        Web5: {
+          connect: vi.fn(() => {
+            return {
+              web5,
+              did,
+            };
+          }),
+        },
+      };
+    });
+  });
+
+  test('createDidAutomatically returns a DID', async () => {
+    const did = await createDidAutomatically();
+
+    expect(did).toBeDefined();
+    expect(did).toMatch(/^did:/);
+  });
 });
