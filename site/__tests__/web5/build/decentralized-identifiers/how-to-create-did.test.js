966d72fdebced01832cba7b7846e6be2b4a5b419
angiejones <jones.angie@gmail.com>
2023-12-20 00:47:57 -0600

    added mock back

--- site/__tests__/web5/build/decentralized-identifiers/how-to-create-did.test.js
+++ site/__tests__/web5/build/decentralized-identifiers/how-to-create-did.test.js
@@ -1,4 +1,5 @@
-import { test, expect, describe } from 'vitest';
+import { test, expect, vi, describe, beforeAll } from 'vitest';
+
 import {
   createDidAutomatically,
   createDidDht,
@@ -6,10 +7,30 @@ import {
   createDidKey
 } from '../../../../code-snippets/web5/build/decentralized-identifiers/how-to-create-did';
 
-describe('create-dids', () => {
+let web5;
+
+describe('how-to-create-did', () => {
+  beforeAll(async () => {
+    web5 = globalThis.web5;
+    did = globalThis.did;
+
+    vi.mock('@web5/api', () => {
+      return {
+        Web5: {
+          connect: vi.fn(() => {
+            return {
+              web5,
+              did,
+            };
+          }),
+        },
+      };
+    });
+  });
+
 
   //timing out
-  test.skip('createDidAutomatically returns a DID', async () => {
+  test('createDidAutomatically returns a DID', async () => {
     const did = await createDidAutomatically();
     expect(did).toBeDefined();
     expect(did).toMatch(/^did:/);
