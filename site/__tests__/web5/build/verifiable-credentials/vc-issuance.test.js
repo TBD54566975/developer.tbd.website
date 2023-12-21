c3fae13feb3420f13dc1279b72cb9196b9402cff
Angie Jones <jones.angie@gmail.com>
2023-12-21 10:43:48 -0600

    vc issuance guide (#1101)
    * vc issuance guide
    
    * code snippets and tests

---
+++ site/__tests__/web5/build/verifiable-credentials/vc-issuance.test.js
@@ -0,0 +1,34 @@
+import { test, beforeAll, expect, describe } from 'vitest';
+import { DidDhtMethod } from '@web5/dids'
+import {
+    createEducationCredential,
+    signCredential
+} from '../../../../code-snippets/web5/build/verifiable-credentials/vc-issuance';
+
+const issuerDid = await DidDhtMethod.create();
+const subjectDid = await DidDhtMethod.create();
+
+describe('issue a credential', () => {
+
+    test('VerifiableCredential.create() creates a VC', async () => {
+      const vc = await createEducationCredential(issuerDid.did, subjectDid.did);
+      expect(vc).toBeDefined();
+      expect.soft(vc).toHaveProperty('type', 'EmploymentCredential');
+      expect.soft(vc).toHaveProperty('issuer', issuerDid.did);
+      expect.soft(vc).toHaveProperty('subject', subjectDid.did);
+      expect.soft(vc.vcDataModel).toHaveProperty('id');
+      expect.soft(vc.vcDataModel).toHaveProperty('expirationDate', '2023-09-30T12:34:56Z');
+      expect.soft(vc.vcDataModel.credentialSubject).toHaveProperty('id');
+      expect.soft(vc.vcDataModel.credentialSubject).toHaveProperty('position', 'Software Developer');
+      expect.soft(vc.vcDataModel.credentialSubject).toHaveProperty('startDate', '2023-04-01T12:34:56Z');
+      expect.soft(vc.vcDataModel.credentialSubject).toHaveProperty('employmentStatus', 'Contractor');
+    });
+
+    test('VerifiableCredential.sign() signs a VC', async () => {
+        const vc = await createEducationCredential(issuerDid.did, subjectDid.did);
+        const vc_jwt = await signCredential(vc, issuerDid);
+        expect(vc_jwt).toBeDefined();
+        expect(vc_jwt).toMatch(/^[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+$/);
+    });
+});
+  
---
+++ site/code-snippets/web5/build/verifiable-credentials/vc-issuance.js
@@ -0,0 +1,26 @@
+import { VerifiableCredential } from '@web5/credentials';
+
+/*
+The output of this credential is hard-coded in the guide.
+If you change any of the data below, please regenerate the 
+output and paste it in the guide.
+*/
+export async function createEducationCredential(employerDid, employeeDid) {
+const vc = await VerifiableCredential.create({
+    type: 'EmploymentCredential',
+    issuer: employerDid,
+    subject: employeeDid,
+    expirationDate: '2023-09-30T12:34:56Z',
+    data: {
+        "position": "Software Developer",
+        "startDate": "2023-04-01T12:34:56Z",
+        "employmentStatus": "Contractor"
+    }
+});
+return vc;
+}
+
+export async function signCredential(vc, employerDid) {
+const vc_jwt_employment = await vc.sign({ did: employerDid });
+return vc_jwt_employment;
+}
