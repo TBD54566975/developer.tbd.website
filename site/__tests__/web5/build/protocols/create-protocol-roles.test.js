70119c05d084e71e1c94b58c3921c81366a6b6bb
angiejones <jones.angie@gmail.com>
2024-01-05 19:27:29 -0600

    fixing code snippets and tests

--- site/__tests__/web5/build/protocols/create-protocol-roles.test.js
+++ site/__tests__/web5/build/protocols/create-protocol-roles.test.js
@@ -1,51 +1,54 @@
 import { test, beforeAll, expect, describe } from 'vitest';
 import {
     eventBookingProtocolDefinitionWithRoles,
-    assignProtocolRole,
+    assignContextRole,
     assignGlobalRole,   
 } from '../../../../code-snippets/web5/build/protocols/create-protocol-roles';
-import { createLocalRecord } from '../../../../code-snippets/web5/build/decentralized-web-nodes/send';
 
 let web5;
 let did;
+let protocolDefinition;
 
 describe('create-protocol-roles', () => {
-    // connect to web5 beforeAll tests and assign it to web5 variable
     beforeAll(async () => {
         web5 = globalThis.web5;
         did = globalThis.did;
+        protocolDefinition = await eventBookingProtocolDefinitionWithRoles(web5);
     });
+
     test('eventBookingProtocolDefinitionWithRoles can be configured', async () => {
-        const protocolDefinition = await eventBookingProtocolDefinitionWithRoles(web5);
         const response = await web5.dwn.protocols.configure({
             message: {
                 definition: protocolDefinition,
             },
         });
-
         expect(response.status.code).toBe(202);
     });
 
     test('assignGlobalRole assigns a $globalRole to user in protocol', async () => {
-        const guestData = {
-            name: 'Alice',
-            
+        const guestData = { name: 'Alice' }
+        const result = await assignGlobalRole(guestData, did, protocolDefinition);
+        
+        if(result.status.code == 400){
+            //preventing test from failing if the user has already been assigned a global role
+            expect(result.status.detail).toContain('ProtocolAuthorizationDuplicateGlobalRoleRecipient');
         }
-        const record = await createLocalRecord(web5);
-         await record.send(did)
-        const result = await assignGlobalRole(guestData, did);
-        console.log(result.status)
-        expect(result.status.code).toBe(202);
+        else expect(result.status.code).toBe(202);
     });
-    test('assignContextRole assigns contextRole in a protocol', async () => {
-        const guestData = {
-            name: 'Alice',
 
-        }
-        const record = await createLocalRecord(web5);
-        await record.send(did)
-        const result = await assignProtocolRole(guestData, record.id, did);
-        console.log(result.status)
+    test('assignContextRole assigns contextRole in a protocol', async () => {       
+        const {record: eventRecord} = await web5.dwn.records.create({
+            data: { name: 'an event' },
+            message: {
+                protocol: protocolDefinition.protocol,
+                protocolPath: "event",
+                schema: protocolDefinition.types.event.schema
+            }
+        });
+        eventRecord.send(did);
+        
+        const guestData = { name: 'Alice' }
+        const result = await assignContextRole(guestData, eventRecord.id, did, protocolDefinition);
         expect(result.status.code).toBe(202);
     });
 });
--- site/code-snippets/web5/build/protocols/create-protocol-roles.js
+++ site/code-snippets/web5/build/protocols/create-protocol-roles.js
@@ -1,124 +1,88 @@
-export const eventBookingProtocolDefinitionWithRoles = (web5) => {
-    const eventBookingProtocolDefinition = {
-        protocol: "https://example.com/protocols/event",
-        published: true,
-        types: {
-            event: {
-                schema: "https://example.com/schema/event",
-                dataFormats: ["application/json"]
+export async function eventBookingProtocolDefinitionWithRoles() {
+const eventBookingProtocolDefinition = {
+    protocol: "https://example.com/protocols/event",
+    published: true,
+    types: {
+        event: {
+            schema: "https://example.com/schema/event",
+            dataFormats: ["application/json"]
+        },
+        rsvp: {
+            schema: "https://example.com/schema/rsvp",
+            dataFormats: ["application/json"]
+        },
+        guest: {
+            schema: "https://example.com/schema/guest",
+            dataFormats: ["application/json"]
+        },
+        admin: {
+            schema: "https://example.com/schema/admin",
+            dataFormats: ["application/json"]
+        }
+    },
+    structure: {
+        admin: { $globalRole: true },
+        event: {
+            $actions: [
+                { who: "anyone", can: "write" },
+                { who: "anyone", can: "read" }
+            ],
+            guest: {
+                $contextRole: true,
+                $actions: [
+                    { who: "author", of: "event", can: "write" },
+                    { who: "anyone", can: "read"},
+                    { role: "admin", can: "delete" }
+                ]
             },
             rsvp: {
-                schema: "https://example.com/schema/rsvp",
-                dataFormats: ["application/json"]
-            },
-            guest: {
-                schema: "https://example.com/schema/guest",
-                dataFormats: ["application/json"]
-            },
-            admin: {
-                schema: "https://example.com/schema/admin",
-                dataFormats: ["application/json"]
-            }
-        },
-        structure: {
-            admin: {
-                $globalRole: true
-            },
-            event: {
                 $actions: [
-                    {
-                        who: "anyone",
-                        can: "write"
-                    },
-                    {
-                        who: "anyone",
-                        can: "read"
-                    }
-                ],
-                guest: {
-                    $contextRole: true,
-                    $actions: [
-                        {
-                            who: "author",
-                            of: "event",
-                            can: "write"
-                        },
-                        {
-                            who: "anyone",
-                            can: "read"
-                        },
-                        {
-                            role: "admin",
-                            can: "delete"
-                        }
-                    ]
-                },
-                rsvp: {
-                    $actions: [
-                        {
-                            role: "event/guest",
-                            can: "write"
-                        },
-                        {
-                            role: "event/guest",
-                            can: "query"
-                        },
-                        {
-                            role: "event/guest",
-                            can: "read"
-                        },
-                        {
-                            role: "admin",
-                            can: "query"
-                        }
-                    ]
-                }
+                    { role: "event/guest", can: "write" },
+                    { role: "event/guest", can: "query" },
+                    { role: "event/guest", can: "read" },
+                    { role: "admin", can: "query" }
+                ]
             }
         }
-    };
-    return eventBookingProtocolDefinition;
+    }
+};
+return eventBookingProtocolDefinition;
 };
 
-export const assignProtocolRole = async ( guestData, eventId, guestDid) => {
-    const protocolDefinition = await eventBookingProtocolDefinitionWithRoles(web5);
-    try {
-        const response = await web5.dwn.records.create({
-            data: guestData,
-            message: {
-                contextId: eventId,
-                parentId: eventId,
-                protocol: protocolDefinition.protocol,
-                protocolPath: "event/guest",
-                schema: protocolDefinition.types.guest.schema,
-                recipient: guestDid,
-            },
-        });
-        return response
-
-    } catch (error) {
-        console.error("Error creating and sending participant:", error);
-        return { record: null, status: null };
+export async function assignContextRole(guestData, eventRecordId, guestDid, protocolDefinition) {
+const response = await web5.dwn.records.create({
+    data: guestData,
+    message: {
+        contextId: eventRecordId,
+        parentId: eventRecordId,
+        protocol: protocolDefinition.protocol,
+        //highlight-next-line
+        protocolPath: "event/guest",
+        schema: protocolDefinition.types.guest.schema,
+        recipient: guestDid
     }
+});
+
+if(response.status.code == 202){
+    await response.record.send(guestDid);
+}
+return response;
 }
 
-export const assignGlobalRole = async (guestData, guestDid) => {
-    const protocolDefinition = await eventBookingProtocolDefinitionWithRoles(web5);
-    try {
-        const response = await web5.dwn.records.create({
-            data: guestData,
-            message: {
-                protocol: protocolDefinition.protocol,
-                protocolPath: "admin",
-                schema: protocolDefinition.types.admin.schema,
-                recipient: guestDid,
-            },
-        });
-        return response
-
-    } catch (error) {
-        console.error("Error creating and sending participant:", error);
-        return { record: null, status: null };
+export async function assignGlobalRole(adminData, adminDid, protocolDefinition){
+const response = await web5.dwn.records.create({
+    data: adminData,
+    message: {
+        protocol: protocolDefinition.protocol,
+        //highlight-next-line
+        protocolPath: "admin",
+        schema: protocolDefinition.types.admin.schema,
+        recipient: adminDid
     }
+});
+
+if(response.status.code == 202){
+    await response.record.send(adminDid);
 }
-
-
+return response;
+}
