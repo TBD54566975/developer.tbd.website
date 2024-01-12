2313e56b65c10fc1641c26d6a4f82a3da15a604b
Nick DeJesus <ndejesus1227@gmail.com>
2024-01-11 22:15:21 -0500

    add more examples

--- site/src/components/CodeSnippet.jsx
+++ site/src/components/CodeSnippet.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import codeSnippets from '../../src/util/code-snippets-map.json';
 import CodeBlock from '@theme/CodeBlock';
 
-const CodeSnippet = ({ functionName, importString }) => {
-  const snippet = codeSnippets[functionName];
-
+const CodeSnippet = ({ snippet, language = 'javascript' }) => {
+  console.log("snippet", snippet)
   if (!snippet) {
-    return <p>Error: Code snippet not found for {functionName}.</p>;
+    return <p>Error: Code snippet not found.</p>;
   }
 
   return (
-    <CodeBlock language="js">
-      {importString ? `${importString}\n\n${snippet}` : snippet}
+    <CodeBlock language={language}>
+      {snippet}
     </CodeBlock>
   );
 };
