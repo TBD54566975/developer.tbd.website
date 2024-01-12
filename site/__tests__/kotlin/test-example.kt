d2c4ad147c4feed43a69c0192008ca3a6b27bce1
Nick DeJesus <ndejesus1227@gmail.com>
2024-01-12 07:41:51 -0500

    test Kotlin files

---
+++ site/__tests__/kotlin/test-example.kt
@@ -0,0 +1,28 @@
+
+fun someTestSuite() {
+// :snippet-start: kotlin-test
+    fun initKeyManagement(env: string, didUri: string) {
+    var keyManager: KeyManager;
+
+    // Determine which key manager you'd like to use based on
+    // the environment
+    when(env) {
+        "development" -> keyManager = InMemoryKeyManager();
+        "production" -> keyManager = AwsKeyManager()
+    }
+
+    // Initialize or load a DID
+    var did: DidDht;
+    if (didUri == null) {
+        // Create a new DID
+        val did = DidDht.create(keyManager)
+    } else {
+        // Load existing DID
+        val did = DidDht(uri = "some_uri", keyManager)
+    }
+}
+ // :snippet-end:
+expect(someStuff).toBeFacts()
+
+}
+
--- site/__tests__/web5/build/decentralized-identifiers/how-to-create-did.test.js
+++ site/__tests__/web5/build/decentralized-identifiers/how-to-create-did.test.js
@@ -60,9 +60,7 @@ describe('how-to-create-did', () => {
   });
 
   test('createDidKey creates a DID with did:key method', async () => {
-    // :snippet-start: createDidKey
     const createdDid = await createDidKey();
-    //:snippet-end:
     expect(createdDid).toMatch(/^did:key:/);
   });
 
