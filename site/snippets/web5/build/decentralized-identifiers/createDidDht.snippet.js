a7eb07f34ff3c3c653b339e1f9d45dab708afed3
Nick DeJesus <ndejesus1227@gmail.com>
2024-01-11 21:25:07 -0500

    update snippet script

---
+++ site/snippets/web5/build/decentralized-identifiers/createDidDht.snippet.js
@@ -0,0 +1,16 @@
+const didDht = await DidDhtMethod.create({ publish: true });
+
+//DID and its associated data which can be exported and used in different contexts/apps
+const portableDID = JSON.stringify(didDht);
+
+//DID string
+const did = didDht.did;
+
+//DID Document
+const didDocument = JSON.stringify(didDht.document);
+
+//Cryptographic keys associated with DID
+const keys = JSON.stringify(didDht.keySet);
+
+//Primary form of a DID. more info: https://www.w3.org/TR/did-core/#dfn-canonicalid
+const canonicalId = didDht.canonicalId;
