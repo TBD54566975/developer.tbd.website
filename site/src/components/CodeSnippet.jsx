c3bf501e0497a852a7f869c85a480d67eb071f3e
Nick DeJesus <ndejesus1227@gmail.com>
2024-01-19 13:53:16 -0500

    adjust code snippet to support old system as well as new

--- site/src/components/CodeSnippet.jsx
+++ site/src/components/CodeSnippet.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import codeSnippets from '../../src/util/code-snippets-map.json'; // Import for old system
 import CodeBlock from '@theme/CodeBlock';
 
-const CodeSnippet = ({ snippet, language = 'javascript' }) => {
-  console.log("snippet", snippet)
-  if (!snippet) {
-    return <p>Error: Code snippet not found.</p>;
+const CodeSnippet = ({
+  functionName,
+  importString,
+  snippet,
+  language = 'javascript',
+}) => {
+  let finalSnippet;
+
+  // Check if using old system
+  if (functionName) {
+    const oldSnippet = codeSnippets[functionName];
+    if (!oldSnippet) {
+      return <p>Error: Code snippet not found for {functionName}.</p>;
+    }
+    finalSnippet = importString
+      ? `${importString}\n\n${oldSnippet}`
+      : oldSnippet;
+  } else {
+    // Using new system
+    if (!snippet) {
+      return <p>Error: Code snippet not found.</p>;
+    }
+    finalSnippet = snippet;
   }
 
-  return (
-    <CodeBlock language={language}>
-      {snippet}
-    </CodeBlock>
-  );
+  return <CodeBlock language={language}>{finalSnippet}</CodeBlock>;
 };
 
 export default CodeSnippet;
