61456140c481b7004c1f4f8a075e48813df2a724
samejima-san <samejimasama@gmail.com>
2024-01-30 12:21:27 -0500

    replaced workaround with CodeBlock import

--- site/src/components/ReusableTabs.jsx
+++ site/src/components/ReusableTabs.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
+import CodeBlock from '@theme/CodeBlock'
 
 const ReusableTabs = ({MacCommand, WinCommand}) => {
     return (
         <Tabs >
             <TabItem value="mac" label="Mac" default>
-                <pre class="prism-code language-bash codeBlock_node_modules-@docusaurus-theme-classic-lib-theme-CodeBlock-Content-styles-module thin-scrollbar">
-                    <code class="codeBlockLines_node_modules-@docusaurus-theme-classic-lib-theme-CodeBlock-Content-styles-module">{MacCommand}</code>
-                </pre>
+                <CodeBlock language="bash">
+                    {MacCommand}
+                </CodeBlock>
+                
             </TabItem>
             <TabItem value="windows" label="Windows">
-                <pre class="prism-code language-json codeBlock_node_modules-@docusaurus-theme-classic-lib-theme-CodeBlock-Content-styles-module thin-scrollbar">
-                    <code class="codeBlockLines_node_modules-@docusaurus-theme-classic-lib-theme-CodeBlock-Content-styles-module">{WinCommand}</code>
-                </pre>
+                <CodeBlock language="bash">
+                    {WinCommand}
+                </CodeBlock>
             </TabItem>
         </Tabs>
 )};
