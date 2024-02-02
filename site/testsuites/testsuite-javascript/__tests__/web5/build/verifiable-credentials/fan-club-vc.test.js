e1a9863cba07885f6c0a95aa514e6d7e88812ab2
Rizel Scarlett <rizel@tbd.email>
2024-02-02 10:48:05 -0500

    all js shnips are tested

--- site/testsuites/testsuite-javascript/__tests__/web5/build/verifiable-credentials/fan-club-vc.test.js
+++ site/testsuites/testsuite-javascript/__tests__/web5/build/verifiable-credentials/fan-club-vc.test.js
@@ -1,123 +1,10 @@
-// import { test, expect, vi, describe, beforeAll } from 'vitest';
-// import { DidKeyMethod } from '@web5/dids';
-// import { VerifiableCredential } from '@web5/credentials';
-// import { setUpWeb5 } from '../../../setup-web5';
-
-// let web5;
-
-// describe('fan-club-vc', () => {
-//     beforeAll(async () => {
-//         await setUpWeb5();
-//         web5 = globalThis.web5;
-//         did = globalThis.did;
-
-//         vi.mock('@web5/api', () => {
-//             return {
-//                 Web5: {
-//                     connect: vi.fn(() => {
-//                         return {
-//                             web5,
-//                             did,
-//                         };
-//                     }),
-//                 },
-//             };
-//         });
-//     });
-
-//     test('createDidKeys creates an issuer DID and alice DID with did:key method', async () => {
-//         // :snippet-start: createDidKeys
-//         const fanClubIssuerDid = await DidKeyMethod.create();
-//         const aliceDid = await DidKeyMethod.create();
-//         // :snippet-end:
-
-//         expect(aliceDid.did).toMatch(/^did:key:/);
-//         expect(fanClubIssuerDid.did).toMatch(/^did:key:/);
-//     });
-
-//     test('createSwiftieFanClubClass creates a class for a vc', async () => {
-//         // :snippet-start: createSwiftieFanClubClass
-//         class SwiftiesFanClub {
-//             constructor(level, legit) {
-//                 // indicates the fan's dedication level
-//                 this.level = level;
-
-//                 // indicates if the fan is a genuine Swiftie
-//                 this.legit = legit;
-//             }
-//         }
-//         // :snippet-end:
-//         const fan = new SwiftiesFanClub('High', true);
-//         expect(fan.legit).toBe(true);
-//         expect(fan.level).toBe('High');
-//     });
-
-//     test('createVerifiableCredential creates a vc', async () => {
-//         const fanClubIssuerDid = await DidKeyMethod.create();
-//         const aliceDid = await DidKeyMethod.create();
-
-//         class SwiftiesFanClub {
-//             constructor(level, legit) {
-//                 // indicates the fan's dedication level
-//                 this.level = level;
-
-//                 // indicates if the fan is a genuine Swiftie
-//                 this.legit = legit;
-//             }
-//         }
-//         // :snippet-start: createVerifiableCredential
-//         const vc = await VerifiableCredential.create({
-//             type: 'SwiftiesFanClub',
-//             issuer: fanClubIssuerDid.did,
-//             subject: aliceDid.did,
-//             data: new SwiftiesFanClub('Stan', true)
-//         });
-//         // :snippet-end:
-//         expect(vc).toHaveProperty('vcDataModel');
-//         expect(vc.vcDataModel).toHaveProperty('@context');
-//         expect(vc.vcDataModel).toHaveProperty('type');
-//         expect(vc.vcDataModel).toHaveProperty('id');
-//         expect(vc.vcDataModel).toHaveProperty('issuer');
-//         expect(vc.vcDataModel).toHaveProperty('issuanceDate');
-//         expect(vc.vcDataModel).toHaveProperty('credentialSubject');
-//         expect(vc.vcDataModel.credentialSubject).toHaveProperty('id');
-//         expect(vc.vcDataModel.credentialSubject).toHaveProperty('level');
-//         expect(vc.vcDataModel.credentialSubject).toHaveProperty('legit');
-//     });
-
-//     test('signVC creates a vc', async () => {
-//         const fanClubIssuerDid = await DidKeyMethod.create();
-//         const aliceDid = await DidKeyMethod.create();
-
-//         class SwiftiesFanClub {
-//             constructor(level, legit) {
-//                 // indicates the fan's dedication level
-//                 this.level = level;
-
-//                 // indicates if the fan is a genuine Swiftie
-//                 this.legit = legit;
-//             }
-//         }
-//         const vc = await VerifiableCredential.create({
-//             type: 'SwiftiesFanClub',
-//             issuer: fanClubIssuerDid.did,
-//             subject: aliceDid.did,
-//             data: new SwiftiesFanClub('Stan', true)
-//         });
-//         // :snippet-start: signVC
-//         const signedVcJwt = await vc.sign({ did: fanClubIssuerDid });
-//         // :snippet-end:
-//         expect(typeof signedVcJwt).toBe('string');
-//         expect(signedVcJwt).not.toBe('');
-//     });
-// }); 
 import { test, expect, vi, describe, beforeAll } from 'vitest';
 import { DidKeyMethod } from '@web5/dids';
-import { VerifiableCredential } from '@web5/credentials';
+import { VerifiableCredential, PresentationExchange } from '@web5/credentials';
 import { setUpWeb5 } from '../../../setup-web5';
 
 describe('fan-club-vc', () => {
-    let web5, did, fanClubIssuerDid, aliceDid, SwiftiesFanClub, vc;
+    let web5, did, fanClubIssuerDid, aliceDid, SwiftiesFanClub, vc, signedVcJwt, presentationDefinition;
 
     beforeAll(async () => {
         await setUpWeb5();
@@ -133,12 +20,36 @@ describe('fan-club-vc', () => {
                 this.legit = legit;
             }
         };
+
         vc = await VerifiableCredential.create({
             type: 'SwiftiesFanClub',
             issuer: fanClubIssuerDid.did,
             subject: aliceDid.did,
             data: new SwiftiesFanClub('Stan', true)
         });
+
+        signedVcJwt = await vc.sign({ did: fanClubIssuerDid });
+
+        presentationDefinition = {
+            'id': 'presDefId123',
+            'name': 'Swifties Fan Club Presentation Definition',
+            'purpose': 'for proving membership in the fan club',
+            'input_descriptors': [
+                {
+                    'id': 'legitness',
+                    'purpose': 'are you legit or not?',
+                    'constraints': {
+                        'fields': [
+                            {
+                                'path': [
+                                    '$.credentialSubject.legit',
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        };
 
         vi.mock('@web5/api', () => ({
             Web5: {
@@ -165,9 +76,9 @@ describe('fan-club-vc', () => {
             }
         }
         // :snippet-end:
-        const fan = new SwiftiesFanClub('High', true);
+        const fan = new SwiftiesFanClub('Stan', true);
         expect(fan.legit).toBe(true);
-        expect(fan.level).toBe('High');
+        expect(fan.level).toBe('Stan');
     });
 
     test('createVerifiableCredential creates a vc', async () => {
@@ -180,15 +91,9 @@ describe('fan-club-vc', () => {
         });
         // :snippet-end:
         expect(vc).toHaveProperty('vcDataModel');
-        expect(vc.vcDataModel).toHaveProperty('@context');
-        expect(vc.vcDataModel).toHaveProperty('type');
-        expect(vc.vcDataModel).toHaveProperty('id');
-        expect(vc.vcDataModel).toHaveProperty('issuer');
-        expect(vc.vcDataModel).toHaveProperty('issuanceDate');
         expect(vc.vcDataModel).toHaveProperty('credentialSubject');
-        expect(vc.vcDataModel.credentialSubject).toHaveProperty('id');
-        expect(vc.vcDataModel.credentialSubject).toHaveProperty('level');
-        expect(vc.vcDataModel.credentialSubject).toHaveProperty('legit');
+        expect(vc.vcDataModel.credentialSubject.level).toBe('Stan');
+        expect(vc.vcDataModel.credentialSubject.legit).toBe(true);
     });
 
     test('signVC creates a vc', async () => {
@@ -197,5 +102,91 @@ describe('fan-club-vc', () => {
         // :snippet-end:
         expect(typeof signedVcJwt).toBe('string');
         expect(signedVcJwt).not.toBe('');
+    });
+
+    test('createPresentationDefinition creates a presentation definition', async () => {
+        // :snippet-start: createPresentationDefinition
+        const presentationDefinition = {
+            'id': 'presDefId123',
+            'name': 'Swifties Fan Club Presentation Definition',
+            'purpose': 'for proving membership in the fan club',
+            'input_descriptors': [
+                {
+                    'id': 'legitness',
+                    'purpose': 'are you legit or not?',
+                    'constraints': {
+                        'fields': [
+                            {
+                                'path': [
+                                    '$.credentialSubject.legit',
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        };
+
+        const definitionValidation = PresentationExchange.validateDefinition({ presentationDefinition });
+        // :snippet-end:
+        expect(definitionValidation).toHaveLength(1);
+        expect(definitionValidation[0]).toHaveProperty('message', 'ok');
+        expect(definitionValidation[0]).toHaveProperty('status', 'info');
+        expect(definitionValidation[0]).toHaveProperty('tag', 'root');
+    })
+
+    test('satisfiesDefinition checks if VC satisfies the presentation definition', async () => {
+        const logSpy = vi.spyOn(console, 'log');
+        // :snippet-start: satisfiesDefinition
+        // Does VC Satisfy the Presentation Definition
+        try {
+            PresentationExchange.satisfiesPresentationDefinition({ vcJwts: [signedVcJwt], presentationDefinition: presentationDefinition });
+            console.log('VC Satisfies Presentation Definition!');
+        } catch (err) {
+            console.log('VC does not satisfy Presentation Definition: ' + err.message);
+        }
+        // :snippet-end:
+        expect(logSpy).toHaveBeenCalledWith('VC Satisfies Presentation Definition!');
+        logSpy.mockRestore();
+    });
+
+    test('createPresentationFromCredentials creates presentation from credentials and checks the presentation result', async () => {
+        // :snippet-start: createPresentationFromCredentials
+        // Create Presentation Result that contains a Verifiable Presentation and Presentation Submission
+        const presentationResult = PresentationExchange.createPresentationFromCredentials({ vcJwts: [signedVcJwt], presentationDefinition: presentationDefinition });
+        console.log('\nPresentation Result: ' + JSON.stringify(presentationResult));
+        // :snippet-end:
+        
+        expect(presentationResult.presentation).toHaveProperty('@context');
+        expect(presentationResult.presentation).toHaveProperty('type');
+        expect(presentationResult.presentation).toHaveProperty('presentation_submission');
+        expect(presentationResult).toHaveProperty('presentationSubmissionLocation');
+        expect(presentationResult).toHaveProperty('presentationSubmission');
+    });
+    test('verify VC checks if VC verification is successful', async () => {
+        // :snippet-start: verifyVC
+        let result
+        
+        try {
+            result = await VerifiableCredential.verify({ vcJwt: signedVcJwt });
+            console.log('VC Verification Successful!');
+        } catch (err) {
+            console.log('VC Verification failed:' + err.message);
+        }
+        // :snippet-end:
+        expect(result).toHaveProperty('vc');
+        expect(result.vc).toHaveProperty('credentialSubject');
+        expect(result.vc.credentialSubject.level).toBe('Stan');
+        expect(result.vc.credentialSubject.legit).toBe(true);
+    });
+
+    test('parseSignedVcJwt parses the signed VC JWT', async () => {
+        // :snippet-start: parseSignedVcJwt
+        const vc = VerifiableCredential.parseJwt({ vcJwt: signedVcJwt });
+        // :snippet-end:
+        expect(vc).toHaveProperty('vcDataModel');
+        expect(vc.vcDataModel).toHaveProperty('credentialSubject');
+        expect(vc.vcDataModel.credentialSubject.level).toBe('Stan');
+        expect(vc.vcDataModel.credentialSubject.legit).toBe(true);
     });
 });
