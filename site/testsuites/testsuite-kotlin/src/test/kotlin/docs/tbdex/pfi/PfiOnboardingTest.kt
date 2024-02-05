f9001104d6f88d05423c924fa64857c61dc51c51
chris-tbd <129119076+chris-tbd@users.noreply.github.com>
2024-02-05 09:54:20 -0600

    Add Kotlin Snippets to PFI/Required SDKs, PFI/Onboarding,  Wallet/Required SDKs pages (#1167)
    * push updates for the Required SDKs and Onboarding pages for PFIs to include Kotlin Snippets
    
    * address comments
    
    * missing fixes
    
    * fix spacing
    
    * fixing code to work
    
    * Adding the proper import statements
    
    * address comments
    
    * add snippets for testing, address comments
    
    * add breadcrumbs
    
    * fixing snippet names
    
    * fixing snippet names
    
    * fix breadcrumbs to add syntax highlighting
    
    * Update site/docs/tbdex/pfi/required-sdks.mdx
    
    * Update site/docs/tbdex/pfi/required-sdks.mdx
    
    * fix tests
    
    * fix breadcrumb syntax highlighting
    
    * fix wallet xml
    
    * update wallet page
    
    * adding notes, fixing links
    
    * fixing snippet files
    
    * Fix test naming convention so Surefire picks it up
    
    * adding pfi sdk install snippet
    
    ---------
    
    Co-authored-by: Chris Giglio <chrisgiglio@chrisgiglio-macbookpro.local>
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    Co-authored-by: Andrew Lee Rubinger <alr@alrubinger.com>

---
+++ site/testsuites/testsuite-kotlin/src/test/kotlin/docs/tbdex/pfi/PfiOnboardingTest.kt
@@ -0,0 +1,39 @@
+package website.tbd.developer.site.docs.tbdex.pfi
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*
+
+// :snippet-start: pfiOnboardingImportsKt
+import web5.sdk.dids.methods.dht.CreateDidDhtOptions
+import web5.sdk.dids.methods.dht.DidDht
+import web5.sdk.crypto.InMemoryKeyManager
+import foundation.identity.did.Service
+import java.net.URI
+// :snippet-end:
+
+/**
+ * Tests for PFI Onboarding guide
+ */
+class PfiOnboardingTest {
+
+  @Test
+  fun `create PFI DID`() {
+
+    // :snippet-start: pfiOnboardingCreateDidKt
+    val serviceToAdd = Service.builder()
+        .id(URI("pfi"))
+        .type("PFI")
+        .serviceEndpoint("tbdex-pfi.tbddev.org")
+        .build()
+
+    val options = CreateDidDhtOptions(
+        publish = true,
+        services = listOf(serviceToAdd),
+    )
+
+    val did = DidDht.create(InMemoryKeyManager(), options)
+    // :snippet-end:
+
+    assertEquals("PFI", did.didDocument?.services?.get(0)?.type, "DID should start with 'did:key'")
+  }
+}
