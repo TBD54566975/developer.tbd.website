b681bf4e9ae946f4ac91ba36c8b5d48fe8828eb9
Angie Jones <jones.angie@gmail.com>
2024-02-06 14:03:08 -0600

    Kotlin: PFI Allowlist (#1208)
    * Kotlin: PFI Allowlist
    
    * dont publish test dids
    
    * ocd

---
+++ site/testsuites/testsuite-javascript/__tests__/tbdex/wallet/allowListPfi.test.js
@@ -0,0 +1,25 @@
+import { test, expect, describe, beforeAll } from 'vitest';
+import { DidDhtMethod } from '@web5/dids';
+
+let pfiDid;
+
+describe('allowlist PFIs', () => {
+  beforeAll(async () => {
+    pfiDid = await DidDhtMethod.create({
+        publish: false,
+        services: [{
+            id: 'pfi',
+            type: 'PFI',
+            serviceEndpoint: 'tbdex-pfi.tbddev.org'
+        }]
+    })
+  });
+  
+  test('PFI DID has PFI service', async () => {
+    // :snippet-start: isPFIJs
+    const isPFI = pfiDid.document.service.some(service => service.type === 'PFI');
+    // :snippet-end:
+    expect(isPFI).toBe(true)
+  });
+
+});
