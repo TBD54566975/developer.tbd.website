99df5a1924adea00dca67f96d039623f0c38aa6e
angiejones <jones.angie@gmail.com>
2024-02-21 00:38:11 -0600

    removing embedded snippets

--- site/testsuites/testsuite-javascript/__tests__/tbdex/issuer/credentialIssuance.test.js
+++ site/testsuites/testsuite-javascript/__tests__/tbdex/issuer/credentialIssuance.test.js
@@ -3,9 +3,6 @@ import express from 'express';
 import { Jwt, VerifiableCredential } from '@web5/credentials';
 import { DidDht } from '@web5/dids';
 
-const app = express();
-app.use(express.json());
-
 const issuerDid = await DidDht.create({ 
   options: { publish: true } 
 });
@@ -36,10 +33,19 @@ function checkSanctionsList(payload) {
 }
 // :snippet-end:
 
+
+const app = express();
+app.use(express.json());
+
+
 // :snippet-start: checkSanctionsEndpoint
 app.get('/check-sanctions', async (req, res) => {
+
+  /***********************************************
+  * Accepts a JWT in the Authorization header 
+  * and parse to get the signer DID
+  ************************************************/
   try {
-    //  Intake user data or send back any responses/redirects requesting more info
     const authHeader = req.headers['authorization'];
 
     if (!authHeader) {
@@ -49,54 +55,55 @@ app.get('/check-sanctions', async (req, res) => {
     }
 
     const tokenParts = authHeader.split('Bearer ');
+
     if (tokenParts.length !== 2) {
       return res.status(401).json({
         errors: ['Authorization header format is Bearer <token>'],
       });
     }
+
     const compactJwt = tokenParts[1];
 
-    let signer, payload;
+    let subject, payload;
+
     try {
       const verificationResult = await Jwt.verify({ jwt: compactJwt });
-
-      // Assuming the user signed the JWT, this is their DID
-      signer = verificationResult.payload.sub;
+      subject = verificationResult.payload.sub;
       payload = verificationResult.payload;
     } catch (error) {
       return res.status(401).json({
-        errors: ['Invalid token'],
+        errors: ['Invalid token']
       });
     }
 
-    // Perform the sanctions check and get the result
-    const sanctionsListResult = checkSanctionsList(payload);
-    //:snippet-end:
 
-    //:snippet-start: createVerifiableCredentialWithSanctionsListResult
-    // Check if the sanctions list result is valid for credential creation
+    /***********************************************
+    * Perform the sanctions check and get the result
+    ************************************************/
+    const sanctionsListResult = checkSanctionsList(payload);
+
     if (!sanctionsListResult || sanctionsListResult.isSanctioned) {
       return res.status(403).json({
         errors: ['User is not eligible for a credential due to sanctions'],
       });
     }
-    //highlight-start
+
+    /***********************************************
+    * Create credential
+    ************************************************/    
     const sanctions_credential = await VerifiableCredential.create({
       type: 'SanctionsCredential',
       issuer: issuerDid.uri,
-      subject: signer,
+      subject: subject,
       data: new SanctionsCredential(sanctionsListResult),
     });
-    //highlight-end
-    //:snippet-end:
 
-    // :snippet-start: signCreatedSanctionsCredential
-    //highlight-start
-    // Sign the credential
+    /***********************************************
+    * To secure the VC, you must sign it
+    ************************************************/     
     const credential_token = await sanctions_credential.sign({
       did: issuerDid,
     });
-    //highlight-end
 
     return res.status(200).json({ credential: credential_token });
   } catch (error) {
@@ -108,11 +115,21 @@ app.get('/check-sanctions', async (req, res) => {
 });
 // :snippet-end:
 
-describe('Direct method functionality tests', () => {
+
+/* 
+Have to add tests separately because the big snippet is
+contained within the express app and the objects within
+are outside of the function's scope to be tested 
+*/
+
+describe('Sanctions Credential Issuance', () => {
+
+  const sanctionsListResult = {
+    listsCleared: ["FBI's Most Wanted, USA Watchlist", 'EU Watchlist'],
+  };
   
-  test('Jwt.verify works with a valid JWT', async () => {
-    const validJwt =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSIsImtpZCI6ImRpZDprZXk6ejZNa3djSmkzeVVONDJFZ1N2cmNHRlFyQzRKY1pkenlWWkhQOVdmMXFRZWRuVlRQI3o2TWt3Y0ppM3lVTjQyRWdTdnJjR0ZRckM0SmNaZHp5VlpIUDlXZjFxUWVkblZUUCJ9.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIiwiSWRlbnRpZmljYXRpb25DcmVkZW50aWFsIl0sImlkIjoidXJuOnV1aWQ6MDQ3ZTg0ZWItY2NhMS00NjFlLWFjZjAtMGMyZGE5ZDczOTNhIiwiaXNzdWVyIjoiZGlkOmtleTp6Nk1rd2NKaTN5VU40MkVnU3ZyY0dGUXJDNEpjWmR6eVZaSFA5V2YxcVFlZG5WVFAiLCJpc3N1YW5jZURhdGUiOiIyMDI0LTAyLTE1VDE5OjMyOjE2WiIsImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImlkIjoiZGlkOmtleTp6Nk1rd2NKaTN5VU40MkVnU3ZyY0dGUXJDNEpjWmR6eVZaSFA5V2YxcVFlZG5WVFAiLCJuYW1lIjoiam9obiJ9fSwiaXNzIjoiZGlkOmtleTp6Nk1rd2NKaTN5VU40MkVnU3ZyY0dGUXJDNEpjWmR6eVZaSFA5V2YxcVFlZG5WVFAiLCJzdWIiOiJkaWQ6a2V5Ono2TWt3Y0ppM3lVTjQyRWdTdnJjR0ZRckM0SmNaZHp5VlpIUDlXZjFxUWVkblZUUCJ9.7oFAPckx-vxCbbzKSk9bU7eXlnjBFvMborl9woHCbcvWaLt0LvTRuDfvGDPC24V9D1K5OFpTnnBiN5jtIOmbBg';
+  test('Jwt.verify() works with a valid JWT', async () => {
+    const validJwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFZERTQSIsImtpZCI6ImRpZDprZXk6ejZNa3djSmkzeVVONDJFZ1N2cmNHRlFyQzRKY1pkenlWWkhQOVdmMXFRZWRuVlRQI3o2TWt3Y0ppM3lVTjQyRWdTdnJjR0ZRckM0SmNaZHp5VlpIUDlXZjFxUWVkblZUUCJ9.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIiwiSWRlbnRpZmljYXRpb25DcmVkZW50aWFsIl0sImlkIjoidXJuOnV1aWQ6MDQ3ZTg0ZWItY2NhMS00NjFlLWFjZjAtMGMyZGE5ZDczOTNhIiwiaXNzdWVyIjoiZGlkOmtleTp6Nk1rd2NKaTN5VU40MkVnU3ZyY0dGUXJDNEpjWmR6eVZaSFA5V2YxcVFlZG5WVFAiLCJpc3N1YW5jZURhdGUiOiIyMDI0LTAyLTE1VDE5OjMyOjE2WiIsImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImlkIjoiZGlkOmtleTp6Nk1rd2NKaTN5VU40MkVnU3ZyY0dGUXJDNEpjWmR6eVZaSFA5V2YxcVFlZG5WVFAiLCJuYW1lIjoiam9obiJ9fSwiaXNzIjoiZGlkOmtleTp6Nk1rd2NKaTN5VU40MkVnU3ZyY0dGUXJDNEpjWmR6eVZaSFA5V2YxcVFlZG5WVFAiLCJzdWIiOiJkaWQ6a2V5Ono2TWt3Y0ppM3lVTjQyRWdTdnJjR0ZRckM0SmNaZHp5VlpIUDlXZjFxUWVkblZUUCJ9.7oFAPckx-vxCbbzKSk9bU7eXlnjBFvMborl9woHCbcvWaLt0LvTRuDfvGDPC24V9D1K5OFpTnnBiN5jtIOmbBg';
     const verificationResult = await Jwt.verify({ jwt: validJwt });
 
     expect(verificationResult).toHaveProperty('header');
@@ -122,10 +139,6 @@ describe('Direct method functionality tests', () => {
   });
 
   test('.create() creates a credential with expected fields', async () => {
-    const sanctionsListResult = {
-      listsCleared: ["FBI's Most Wanted, USA Watchlist", 'EU Watchlist'],
-    };
-
     const createdCredential = await VerifiableCredential.create({
       type: 'SanctionsCredential',
       issuer: issuerDid.uri,
@@ -138,10 +151,6 @@ describe('Direct method functionality tests', () => {
   });
 
   test('.sign() method signs the credential and includes expected fields', async () => {
-    const sanctionsListResult = {
-      listsCleared: ["FBI's Most Wanted, USA Watchlist", 'EU Watchlist'],
-    };
-
     const sanctionsCredential = await VerifiableCredential.create({
       type: 'SanctionsCredential',
       issuer: issuerDid.uri,
@@ -149,7 +158,9 @@ describe('Direct method functionality tests', () => {
       data: new SanctionsCredential(sanctionsListResult),
     });
 
-    const signedCredential = await sanctionsCredential.sign({ did: issuerDid });
+    const signedCredential = await sanctionsCredential.sign({ 
+      did: issuerDid 
+    });
 
     expect(typeof signedCredential).toBe('string');
     expect(signedCredential).not.toBe('');
