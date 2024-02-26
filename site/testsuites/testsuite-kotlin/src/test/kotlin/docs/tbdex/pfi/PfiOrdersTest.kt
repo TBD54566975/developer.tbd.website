55845dbba2bf3e0258689c9fa28fd6a933afaeb0
Chris Giglio <chrisgiglio@tbd.email>
2024-02-26 14:10:23 -0600

    Add Kotlin tests

--- site/testsuites/testsuite-kotlin/src/test/kotlin/docs/tbdex/pfi/PfiOrdersTest.kt
+++ site/testsuites/testsuite-kotlin/src/test/kotlin/docs/tbdex/pfi/PfiOrdersTest.kt
@@ -3,19 +3,32 @@ package website.tbd.developer.site.docs.tbdex.pfi
 import de.fxlae.typeid.TypeId
 import foundation.identity.did.Service
 import tbdex.sdk.httpserver.models.*
-import tbdex.sdk.protocol.models.*
+import tbdex.sdk.protocol.models.Order
+import tbdex.sdk.protocol.models.OrderStatus
+import tbdex.sdk.protocol.models.OrderStatusData
+import tbdex.sdk.protocol.models.Close
+import tbdex.sdk.protocol.models.CloseData
 import web5.sdk.dids.methods.dht.DidDht
 import web5.sdk.crypto.InMemoryKeyManager
 import web5.sdk.dids.methods.dht.CreateDidDhtOptions
 import website.tbd.developer.site.docs.tbdex.pfi.*
 import website.tbd.developer.site.docs.utils.*
 import java.net.URI
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.*
 
 class PfiOrdersTest {
-    fun main() {
 
+    private lateinit var dataProvider: MockDataProvider
+    private lateinit var pfiDid: DidDht
+    private lateinit var senderDid: DidDht
+    private lateinit var exchangesApiProvider: ExchangesApiProvider
+    private lateinit var orderMessage: Order
+
+    @BeforeEach
+    fun setup() {
         // Set up DIDs
-        val dataProvider = MockDataProvider()
+        dataProvider = MockDataProvider()
         val serviceToAdd = Service.builder()
             .id(URI("pfi"))
             .type("PFI")
@@ -28,30 +41,37 @@ class PfiOrdersTest {
         )
         val senderOptions = CreateDidDhtOptions(publish = true)
 
-        val pfiDid = DidDht.create(InMemoryKeyManager(), pfiOptions)
-        val senderDid = DidDht.create(InMemoryKeyManager(), senderOptions)
-        val exchangesApiProvider = ExchangesApiProvider()
+        pfiDid = DidDht.create(InMemoryKeyManager(), pfiOptions)
+        senderDid = DidDht.create(InMemoryKeyManager(), senderOptions)
+        exchangesApiProvider = ExchangesApiProvider()
 
-        val orderMessage = TestData.getOrder(
+        orderMessage = TestData.getOrder(
             to = pfiDid.uri,
             from = senderDid.uri)
+    }
 
+    @Test
+    fun `PFI creates orderStatus and verifies it`() {
         exchangesApiProvider.setWrite()
 
         // :snippet-start: pfiOrderStatusKt
-        if (orderMessage.metadata.kind == MessageKind.order ) {
             val orderStatus = OrderStatus.create(
-                to = pfiDid.uri,
-                from = orderMessage.metadata.from,
+                from = pfiDid.uri,
+                to = orderMessage.metadata.from,
                 exchangeId = orderMessage.metadata.exchangeId,
                 orderStatusData = OrderStatusData("PROCESSING")
             )
 
             orderStatus.sign(pfiDid)
             exchangesApiProvider.write(orderStatus)
-        }
         // :snippet-end:
 
+        assertNotNull(orderStatus.verify(), "OrderStatus signature is invalid")
+    }
+
+    
+    @Test
+    fun `PFI creates Close and verifies it`() {
         exchangesApiProvider.setWrite()
 
         // :snippet-start: pfiCloseOrderKt
@@ -64,6 +84,8 @@ class PfiOrdersTest {
         closeMessage.sign(pfiDid)
         exchangesApiProvider.write(closeMessage)
         // :snippet-end:
+
+        assertNotNull(closeMessage.verify(), "Close signature is invalid")
     }
 }
 
