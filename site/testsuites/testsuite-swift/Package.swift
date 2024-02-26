1df3903054f88480c1f360f31ab217adfcb3e137
Adewale Abati <acekyd01@gmail.com>
2024-02-26 14:28:31 +0100

    swift testing branch

--- site/testsuites/testsuite-swift/Package.swift
+++ site/testsuites/testsuite-swift/Package.swift
@@ -11,6 +11,11 @@ let package = Package(
             name: "DevSiteTestSuite",
             targets: ["DevSiteTestSuite"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+         .package(url: "https://github.com/TBD54566975/web5-swift", .upToNextMajor(from: "0.0.3")),
+         .package(url: "https://github.com/WeTransfer/Mocker.git", .upToNextMajor(from: "3.0.2")),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -18,6 +23,6 @@ let package = Package(
             name: "DevSiteTestSuite"),
         .testTarget(
             name: "DevSiteTestSuiteTests",
-            dependencies: ["DevSiteTestSuite"]),
+            dependencies: ["DevSiteTestSuite", "Mocker"]),
     ]
 )
