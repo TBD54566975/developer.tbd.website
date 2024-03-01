7f72c345b97def537fdce938dcef539b6c181acd
Angie Jones <jones.angie@gmail.com>
2024-03-01 10:55:12 -0600

    Update site/testsuites/testsuite-kotlin/src/test/kotlin/docs/tbdex/pfi/PfiStructureTest.kt

--- site/testsuites/testsuite-kotlin/src/test/kotlin/docs/tbdex/pfi/PfiStructureTest.kt
+++ site/testsuites/testsuite-kotlin/src/test/kotlin/docs/tbdex/pfi/PfiStructureTest.kt
@@ -77,7 +77,7 @@ class PfiStructureTest {
         Thread.sleep(1000)
 
         // Test calls against the server
-        val url = URI.create( "http://localhost:8080/").toURL()
+        val url = URI.create("http://localhost:8080/").toURL()
         val connection = url.openConnection() as HttpURLConnection
         connection.requestMethod = "GET"
         connection.disconnect()
