3513a74e7baf8a92341c536d4d8ba471e2c8a3a4
Angie Jones <jones.angie@gmail.com>
2024-03-04 16:42:24 -0600

    setup and teardown examples for Swift (#1304)

--- site/testsuites/testsuite-swift/Tests/DevSiteTestSuiteTests/DevSiteTestSuiteTests.swift
+++ site/testsuites/testsuite-swift/Tests/DevSiteTestSuiteTests/DevSiteTestSuiteTests.swift
@@ -2,6 +2,27 @@ import XCTest
 @testable import DevSiteTestSuite
 
 final class DevSiteTestSuiteTests: XCTestCase {
+
+    override class func setUp() {
+        super.setUp()
+        // Your setup code here, executed before any tests are run in this class
+    }
+
+    override func setUp() {
+        super.setUp()
+        // This is run before each test method in the class
+    }
+
+    override func tearDown() {
+        // This is run after each test method in the class
+        super.tearDown()
+    }
+
+    override class func tearDown() {
+        // Your teardown code here, executed after all tests in this class are done
+        super.tearDown()
+    }
+
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
