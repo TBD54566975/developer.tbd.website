acc4dd9fd2f419294f6068d27c26abac3db4be05
Adewale Abati <acekyd01@gmail.com>
2024-03-04 15:49:06 +0100

    Add Web5 and tbDEX Swift dependencies (#1299)
    * swift testing branch
    
    * Add web5 and tbdex dependencies with example DID test

--- site/testsuites/testsuite-swift/Package.swift
+++ site/testsuites/testsuite-swift/Package.swift
@@ -5,19 +5,38 @@ import PackageDescription
 
 let package = Package(
     name: "DevSiteTestSuite",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15), // Web5 and tbDEX require minimum 'v10_15'
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DevSiteTestSuite",
             targets: ["DevSiteTestSuite"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+         .package(url: "https://github.com/TBD54566975/web5-swift", exact: "0.0.4"),
+         .package(url: "https://github.com/TBD54566975/tbdex-swift.git", branch: "main"),
+         .package(url: "https://github.com/WeTransfer/Mocker.git", .upToNextMajor(from: "3.0.2")),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DevSiteTestSuite"),
+            name: "DevSiteTestSuite",
+            dependencies: [
+                .product(name: "Web5", package: "web5-swift"),
+                .product(name: "tbDEX", package: "tbdex-swift"),
+            ]),
         .testTarget(
             name: "DevSiteTestSuiteTests",
-            dependencies: ["DevSiteTestSuite"]),
+            dependencies: [
+                "DevSiteTestSuite",
+                "Mocker",
+                .product(name: "Web5", package: "web5-swift"),
+                .product(name: "tbDEX", package: "tbdex-swift"),
+                ]),
     ]
 )
