37696711313dd1ee94a9c8f5aa9f96fea81893e9
angiejones <jones.angie@gmail.com>
2024-03-06 19:18:42 -0600

    deleted commented out lines

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -51,15 +51,6 @@ let config = {
     // [
     //   '@docusaurus/plugin-content-docs',
     //   {
-    //     id: 'api',
-    //     path: 'api',
-    //     routeBasePath: 'api',
-    //     // sidebarPath: require.resolve('./api-sidebars.js'),
-    //   },
-    // ],
-    // [
-    //   '@docusaurus/plugin-content-docs',
-    //   {
     //     id: 'events',
     //     path: 'events',
     //     breadcrumbs: false,
