c95cbf1b6937f1b9e2d2930709417ef19dc68467
angiejones <jones.angie@gmail.com>
2024-03-10 12:01:54 -0500

    Fixing Swift Testsuite

--- site/testsuites/testsuite-swift/Tests/DevSiteTestSuiteTests/tbdex/wallet/RequiedSdks.swift
+++
@@ -1,6 +0,0 @@
-// :snippet-start: walletSdkInstallSwift
-dependencies: [
-        .package(url: "https://github.com/TBD54566975/web5-swift", exact: "0.0.4"),
-        .package(url: "https://github.com/TBD54566975/tbdex-swift.git", branch: "main"),
-]
-// :snippet-end:
