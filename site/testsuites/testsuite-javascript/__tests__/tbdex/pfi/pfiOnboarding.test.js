3fd37bb09b880a0c38553993a8b09eb91484707e
Adewale Abati <acekyd01@gmail.com>
2024-03-13 10:55:19 +0100

    js create pfi did

--- site/testsuites/testsuite-javascript/__tests__/tbdex/pfi/pfiOnboarding.test.js
+++ site/testsuites/testsuite-javascript/__tests__/tbdex/pfi/pfiOnboarding.test.js
@@ -1,19 +1,25 @@
+// :snippet-start: pfiOnboardingImportsJs
 import { DidDht } from '@web5/dids';
+import { LocalKeyManager } from '@web5/crypto'; // optional
+// :snippet-end:
 import { test, expect, describe, beforeAll } from 'vitest';
 
 describe('PFI: Onboarding', () => {
     test('Create DID for a PFI', async () => {
         // :snippet-start: pfiOnboardingCreateDidJs
-        const pfiDid = await DidDht.create({
-            options:{
-                publish: true,
-                services: [{
-                    id: 'pfi',
-                    type: 'PFI',
-                    serviceEndpoint: 'https://example.com/'
-                }]
-            }
-        })
+        const keyManager = new LocalKeyManager();
+    const pfiDid = await DidDht.create({
+        options:{
+            publish: true,
+            services: [{
+                id: 'pfi',
+                type: 'PFI',
+                serviceEndpoint: 'https://example.com/'
+            }]
+        },
+        keyManager
+    })
+
         // :snippet-end:
         expect(pfiDid.document).toHaveProperty('service');
     });
