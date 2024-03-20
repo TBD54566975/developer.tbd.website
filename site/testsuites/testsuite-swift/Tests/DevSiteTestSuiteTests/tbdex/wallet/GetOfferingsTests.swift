4b1df67a3efb18629732b70111b8bf9d8b07b8e4
angiejones <jones.angie@gmail.com>
2024-03-20 11:44:22 -0700

    replaced mock offering

--- site/testsuites/testsuite-swift/Tests/DevSiteTestSuiteTests/tbdex/wallet/GetOfferingsTests.swift
+++ site/testsuites/testsuite-swift/Tests/DevSiteTestSuiteTests/tbdex/wallet/GetOfferingsTests.swift
@@ -24,26 +24,27 @@ final class GetOfferingsTests: XCTestCase {
             // :snippet-start: walletGetOfferingsSwift
             let offerings = try await tbDEXHttpClient.getOfferings(pfiDIDURI: pfiDid)
             // :snippet-end:
-            XCTAssertNotNil(offerings, "Offerings should not be nil")
-        } catch { 
+            XCTAssertEqual(offerings.count, 1, "Exactly 1 offering should be returned")
+        } catch {
             XCTFail("Failed to fetch offerings: \(error)")
         }
     }
 
     func testGetAllOfferings() async throws {
-        var allOfferings = [Offering]()
-         // :snippet-start: walletFindMatchingOfferingsSwift
+        let pfiDids = [pfiDid]
+        
+        // :snippet-start: walletFindMatchingOfferingsSwift
         let payinCurrencyCode = "USD" // Desired payin currency code
-        let payoutCurrencyCode = "KES" // Desired payout currency code
+        let payoutCurrencyCode = "BTC" // Desired payout currency code
 
         var matchedOfferings = [Offering]() // Array to store the matched offerings
 
-        let pfiDids = [pfiDid]
         // Loop through the all PFIs in your network
         for pfiDid in pfiDids { 
+            
             //Makes a request to the PFI to get their offerings
             let offerings = try await tbDEXHttpClient.getOfferings(pfiDIDURI: pfiDid)
-            allOfferings.append(contentsOf: offerings)
+            
             // Filter offerings based on the currency pair
             let filteredOfferings = offerings.filter { offering in
                 offering.data.payinCurrency.currencyCode == payinCurrencyCode &&
@@ -53,11 +54,7 @@ final class GetOfferingsTests: XCTestCase {
             matchedOfferings.append(contentsOf: filteredOfferings)
         }
         // :snippet-end:
-        let mockOfferingData = MockData.selectedOfferingJson.data(using: .utf8)!
-        let decoder = tbDEXJSONDecoder()
-        let mockOffering = try decoder.decode(Offering.self, from: mockOfferingData)
 
-        XCTAssertEqual(matchedOfferings, [mockOffering], "Matched offerings should be equal to mock offering")
+        XCTAssertEqual(matchedOfferings.count, 1, "Exactly 1 offering should be returned")
     }
-
 }
