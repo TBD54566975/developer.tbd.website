4eb7bde95abbf621b1320621f1b4d78bd10371a4
dayhaysoos <ndejesus1227@gmail.com>
2024-03-22 14:50:30 -0400

    pseudo coding to come back to

--- site/src/components/SdkVersions.jsx
+++ site/src/components/SdkVersions.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import CodeBlock from '@theme/CodeBlock';
 
+// Set up template components
+const PackageJson = (depenencies) => {
+    // code block gets returned here
+}
+
+const Kotlin = (dependencies) => {
+    // code block gets returned here
+}
+
+// gradle
+
 function SdkVersions({language, dependency}) {
   const {
     siteConfig: { customFields },
@@ -10,7 +21,12 @@ function SdkVersions({language, dependency}) {
 
   const { SDK_VERSIONS } = customFields
 
-  console.log(SDK_VERSIONS)
+  // check language logic
+  // switch case to determine javascript, gradle, maven, swift
+  // pass array of dependencies to code block
+  // return respective language string into a variable
+
+  // render result below
 
   return <CodeBlock language={language}>{SDK_VERSIONS[language][dependency]}</CodeBlock>;
 }
