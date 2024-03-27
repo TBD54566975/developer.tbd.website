a071de3fc1ca8de2eea05389604c0d2af9827d27
dayhaysoos <ndejesus1227@gmail.com>
2024-03-27 14:41:58 -0400

    Refactor and adding maven support

--- site/src/components/Dependencies.jsx
+++ site/src/components/Dependencies.jsx
@@ -3,6 +3,62 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import CodeBlock from '@theme/CodeBlock';
 
+
+function GradleDependencies({ dependencies, sdks }) {
+  console.log('dependencies', dependencies)
+  const repositoriesBlock = `repositories {
+    mavenCentral()
+    maven("https://repo.danubetech.com/repository/maven-public/")
+  }`;
+
+  // Generate the implementation lines
+  const dependenciesBlock = dependencies.map(dep => {
+    return `    implementation("xyz.block:${dep}:${sdks[dep]}")`; // Note the added indentation
+  }).join('\n');
+
+  // Wrap the implementation lines in a `dependencies` block
+  const gradleDependenciesBlock = `dependencies {\n${dependenciesBlock}\n}`;
+
+  // Combine repositories and dependencies blocks
+  const gradleString = `${repositoriesBlock}\n${gradleDependenciesBlock}`;
+
+  return <CodeBlock language="gradle">{gradleString}</CodeBlock>;
+}
+
+
+function MavenDependencies({ dependencies, sdks }) {
+  const repositoriesBlock = dependencies.map(dep => {
+    return `
+        <repository>
+            <id>mavenCentral</id>
+            <url>https://repo1.maven.org/maven2/</url>
+        </repository>
+    `;
+  }).join('\n');
+
+  const dependenciesBlock = dependencies.map(dep => {
+    return `
+        <dependency>
+            <groupId>xyz.block</groupId>
+            <artifactId>${dep}</artifactId>
+            <version>${sdks[dep]}</version>
+        </dependency>
+    `;
+  }).join('\n');
+
+  const mavenString = `
+<repositories>
+${repositoriesBlock}
+</repositories>
+
+<dependencies>
+${dependenciesBlock}
+</dependencies>
+  `;
+
+  return <CodeBlock language="xml">{mavenString}</CodeBlock>;
+}
+
 // Function to render npm install commands
 function InstallCommands({ dependencies }) {
   const commandString = dependencies.map(dep => `npm install ${dep}`).join('\r\n');
@@ -21,19 +77,16 @@ function PackageJson({ sdkVersions, dependencies }) {
   return <CodeBlock language="js">{jsonString}</CodeBlock>;
 }
 
-// Function to render Gradle dependencies
-function GradleDependencies({ dependencies, sdks }) {
-  const repositoriesBlock = `repositories {
-    mavenCentral()
-    maven("https://repo.danubetech.com/repository/maven-public/")
-  }`;
+function JvmDependencies({ dependencies, sdks, language }) {
+  if(language === 'gradle') {
 
-  const dependenciesBlock = dependencies.map(dep => {
-    return `implementation("xyz.block:${dep}:${sdks[dep]}")`;
-  }).join('\n');
+   return <GradleDependencies dependencies={dependencies} sdks={sdks} />
+  }
 
-  const gradleString = `${repositoriesBlock}\n${dependenciesBlock}`;
-  return <CodeBlock language="gradle">{gradleString}</CodeBlock>;
+  if (language === 'maven') {
+    return <MavenDependencies dependencies={dependencies} sdks={sdks} />
+  }
+
 }
 
 // Main Dependencies component
@@ -47,8 +100,11 @@ function Dependencies({ language, dependencies, display = 'command' }) {
   if (display === 'command') {
     if (language === 'javascript') {
       return <InstallCommands dependencies={dependencies} />;
-    } else if (language === 'maven') {
-      return <GradleDependencies dependencies={dependencies} sdks={SDK_VERSIONS[language]} />;
+    } if (language === 'gradle') {
+      return <JvmDependencies language='gradle' dependencies={dependencies} sdks={SDK_VERSIONS.jvm} />;
+    }
+    if(language === 'maven') {
+      return <JvmDependencies language='maven' dependencies={dependencies} sdks={SDK_VERSIONS.jvm} />
     }
   }
 
@@ -58,7 +114,7 @@ function Dependencies({ language, dependencies, display = 'command' }) {
     default:
       return null; // Or any other default behavior you prefer
   }
-  
+
 }
 
 export default Dependencies;
