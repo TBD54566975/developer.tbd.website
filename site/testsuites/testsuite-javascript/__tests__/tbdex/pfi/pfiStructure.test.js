feabfafe0b1d3226c9325660a7db8262d9df5fbc
angiejones <jones.angie@gmail.com>
2024-04-02 11:18:40 -0500

    JS: PFI Structure

--- site/testsuites/testsuite-javascript/__tests__/tbdex/pfi/pfiStructure.test.js
+++ site/testsuites/testsuite-javascript/__tests__/tbdex/pfi/pfiStructure.test.js
@@ -52,7 +52,7 @@ describe('PFI: Structure', () => {
     test('PFI initializes routes', async () => {
         try {
             // :snippet-start: pfiOverviewServerRoutesJs
-            tbDexServer.onSubmitRfq(async (ctx, rfq, opts) => {
+            tbDexServer.onCreateExchange(async (ctx, rfq, opts) => {
                 await exchangesApiProvider.write({ message: rfq, replyTo: opts.replyTo })
             })
 
@@ -66,9 +66,9 @@ describe('PFI: Structure', () => {
             // :snippet-end:
 
             expect.soft(Object.keys(tbDexServer.callbacks).length).toBe(3)
-            expect.soft(tbDexServer.callbacks.rfq).toBeDefined()
-            expect.soft(tbDexServer.callbacks.order).toBeDefined()
-            expect.soft(tbDexServer.callbacks.close).toBeDefined()
+            expect.soft(tbDexServer.callbacks.createExchange).toBeDefined()
+            expect.soft(tbDexServer.callbacks.submitOrder).toBeDefined()
+            expect.soft(tbDexServer.callbacks.submitClose).toBeDefined()
         } catch(e) {
             assert.fail(`Failed to set up submit routes: ${e.message}`);
         }
