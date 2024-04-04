ae1db6daa741529faf8a8289d43e10d46a04677b
Rizel Scarlett <rizel@tbd.email>
2024-04-04 10:06:54 -0400

    Adding dynamic and synced deps in docs (#1371)

--- site/src/components/Dependency.jsx
+++ site/src/components/Dependency.jsx
@@ -4,106 +4,131 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import CodeBlock from '@theme/CodeBlock';
 import BreadcrumbTab from '@site/src/components/BreadcrumbTab';
 
-
 function GradleDependencies({ dependencies, sdks }) {
   const repositoriesBlock = `repositories {
     mavenCentral()
-    maven("https://repo.danubetech.com/repository/maven-public/")
+    maven {
+      name = "tbd-oss-thirdparty"
+      url = uri("https://blockxyz.jfrog.io/artifactory/tbd-oss-thirdparty-maven2/")
+      mavenContent {
+        releasesOnly()
+    }
   }`;
 
-  // Generate the implementation lines
-  const dependenciesBlock = dependencies.map(dep => {
-    return `    implementation("xyz.block:${dep}:${sdks[dep]}")`; // Note the added indentation
-  }).join('\n');
+  const dependenciesBlock = dependencies
+    .map((dep) => {
+      const version = sdks[dep];
+      if (version) {
+        return `    implementation("xyz.block:${dep}:${version}")`;
+      } else {
+        return `    implementation("${dep}")`;
+      }
+    })
+    .join('\n');
 
-  // Wrap the implementation lines in a `dependencies` block
   const gradleDependenciesBlock = `dependencies {\n${dependenciesBlock}\n}`;
-
-  // Combine repositories and dependencies blocks
   const gradleString = `${repositoriesBlock}\n${gradleDependenciesBlock}`;
 
   return <CodeBlock language="gradle">{gradleString}</CodeBlock>;
 }
 
-
 function MavenDependencies({ dependencies, sdks }) {
-  const repositoriesBlock = dependencies.map(dep => {
-    return `
-        <repository>
-            <id>mavenCentral</id>
-            <url>https://repo1.maven.org/maven2/</url>
-        </repository>
-    `;
-  }).join('\n');
-
-  const dependenciesBlock = dependencies.map(dep => {
-    return `
-        <dependency>
-            <groupId>xyz.block</groupId>
-            <artifactId>${dep}</artifactId>
-            <version>${sdks[dep]}</version>
-        </dependency>
-    `;
-  }).join('\n');
-
-  const mavenString = `
-<repositories>
-${repositoriesBlock}
-</repositories>
-
-<dependencies>
-${dependenciesBlock}
-</dependencies>
+  const repositoriesBlock = `
+ <repository>
+      <id>tbd-oss-thirdparty</id>
+      <name>tbd-oss-thirdparty</name>
+      <releases>
+        <enabled>true</enabled>
+      </releases>
+      <snapshots>
+        <enabled>false</enabled>
+      </snapshots>
+      <url>https://blockxyz.jfrog.io/artifactory/tbd-oss-thirdparty-maven2/</url>
+</repository>
   `;
+
+  const dependenciesBlock = dependencies
+    .map((dep) => {
+      let [groupId, artifactId, version] = dep.split(':');
+      if (!version) {
+        // Handle the case where the version isn't part of the split
+        version = sdks[dep];
+        groupId = 'xyz.block';
+        artifactId = dep;
+      }
+      return `
+<dependency>
+    <groupId>${groupId}</groupId>
+    <artifactId>${artifactId}</artifactId>
+    <version>${version}</version>
+</dependency>`;
+    })
+    .join('');
+
+  const mavenString = `<repositories>${repositoriesBlock}</repositories>\n<dependencies>${dependenciesBlock}</dependencies>`;
 
   return <CodeBlock language="xml">{mavenString}</CodeBlock>;
 }
 
 function SwiftDependencies({ dependencies, sdkVersions }) {
-  const swiftImports = dependencies.map((dep, index) => {
-    const packageInfo = sdkVersions.swift[dep];
-    if (!packageInfo) {
-      console.error(`Package "${dep}" not found in sdkVersions.swift`);
-      return null;
-    }
-    const { url, branch } = packageInfo;
-    return `    .package(url: "${url}", branch: "${branch}")`;
-  }).filter(Boolean).join(',\n');
+  const swiftImports = dependencies
+    .map((dep, index) => {
+      const packageInfo = sdkVersions.swift[dep];
+      if (!packageInfo) {
+        console.error(`Package "${dep}" not found in sdkVersions.swift`);
+        return null;
+      }
+      const { url, branch } = packageInfo;
+      return `    .package(url: "${url}", branch: "${branch}")`;
+    })
+    .filter(Boolean)
+    .join(',\n');
 
   const swiftDependenciesString = `dependencies: [\n${swiftImports}\n]`;
 
   return <CodeBlock language="swift">{swiftDependenciesString}</CodeBlock>;
 }
 
-
 // Function to render npm install commands
-function InstallCommands({ dependencies }) {
-  const commandString = dependencies.map(dep => `npm install ${dep}`).join('\r\n');
+function InstallCommands({ dependencies, sdkVersions }) {
+  const commandString = dependencies
+    .map((dep) => {
+      const normalizedDep = dep.startsWith('@') ? dep : `@${dep}`;
+      const version = sdkVersions[normalizedDep];
+      if (version) {
+        return `npm install ${normalizedDep}@${version}`;
+      } else {
+        return `npm install ${normalizedDep}`;
+      }
+    })
+    .join('\n');
+
   return <CodeBlock language="bash">{commandString}</CodeBlock>;
 }
 
 // Function to render JavaScript package.json
 function PackageJson({ sdkVersions, dependencies }) {
-  const requiredDependencies = Object.entries(dependencies).reduce((acc, [dep, depName]) => {
-    const depVersion = sdkVersions.js[depName];
-    acc[depName] = depVersion;
-    return acc;
-  }, { type: 'module' });
+  const requiredDependencies = Object.entries(dependencies).reduce(
+    (acc, [dep, depName]) => {
+      const depVersion = sdkVersions.js[depName];
+      acc[depName] = depVersion;
+      return acc;
+    },
+    { type: 'module' },
+  );
 
   const jsonString = JSON.stringify(requiredDependencies, null, 2);
   return <CodeBlock language="js">{jsonString}</CodeBlock>;
 }
 
 function JvmDependencies({ dependencies, sdks, language }) {
-  if(language === 'gradle') {
-
-   return <GradleDependencies dependencies={dependencies} sdks={sdks} />
+  if (language === 'gradle') {
+    return <GradleDependencies dependencies={dependencies} sdks={sdks} />;
   }
 
   if (language === 'maven') {
-    return <MavenDependencies dependencies={dependencies} sdks={sdks} />
+    return <MavenDependencies dependencies={dependencies} sdks={sdks} />;
   }
-
 }
 
 // Main Dependencies component
@@ -111,32 +136,49 @@ function Dependency({ language, dependencies, display = 'install' }) {
   const {
     siteConfig: { customFields },
   } = useDocusaurusContext();
-  
+
   const { SDK_VERSIONS } = customFields;
-  
+
   switch (display) {
     case 'install':
       switch (language) {
         case 'javascript':
-          return <InstallCommands dependencies={dependencies} />;
+          return (
+            <InstallCommands
+              dependencies={dependencies}
+              sdkVersions={SDK_VERSIONS.js}
+            />
+          );
         case 'gradle':
         case 'maven':
-          return <JvmDependencies language={language} dependencies={dependencies} sdks={SDK_VERSIONS.jvm} />;
+          return (
+            <JvmDependencies
+              language={language}
+              dependencies={dependencies}
+              sdks={SDK_VERSIONS.jvm}
+            />
+          );
         case 'swift':
-          return <SwiftDependencies sdkVersions={SDK_VERSIONS} dependencies={dependencies} />;
+          return (
+            <SwiftDependencies
+              sdkVersions={SDK_VERSIONS}
+              dependencies={dependencies}
+            />
+          );
         default:
           return null; // Handle unsupported languages
       }
     default:
   }
-  
+
   switch (language) {
     case 'javascript':
-      return <PackageJson sdkVersions={SDK_VERSIONS} dependencies={dependencies} />;
+      return (
+        <PackageJson sdkVersions={SDK_VERSIONS} dependencies={dependencies} />
+      );
     default:
-      return null; 
+      return null;
   }
-
 }
 
 export default Dependency;
