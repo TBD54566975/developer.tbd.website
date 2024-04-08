99383011533f07e93267d604097490f609e2d4b1
Rizel <rizel@tbd.email>
2024-04-08 17:53:51 -0400

    update fan club app

--- examples/tutorials/fan-club-credential/index.js
+++ examples/tutorials/fan-club-credential/index.js
@@ -1,6 +1,8 @@
+
+
 /** This is an example of how to use the web5/credentials package:  https://www.npmjs.com/package/@web5/credentials */
 import { VerifiableCredential, PresentationExchange } from '@web5/credentials';
-import { DidKeyMethod } from '@web5/dids';
+import { DidDht } from '@web5/dids';
 import { Web5 } from '@web5/api';
 import { Web5UserAgent } from '@web5/user-agent';
 
@@ -12,10 +14,10 @@ if (!globalThis.crypto) globalThis.crypto = webcrypto;
  */
 
 // Prerequisites: Create issuer (fan club issuer)
-const fanClubIssuerDid = await DidKeyMethod.create();
+const fanClubIssuerDid = await DidDht.create();
 
 // Prerequisites: Create subject (alice)
-const aliceDid = await DidKeyMethod.create();
+const aliceDid = await DidDht.create();
 
 // Create new credential
 class SwiftiesFanClub {
@@ -26,11 +28,11 @@ class SwiftiesFanClub {
 }
 
 // Credential needs: type, issuer, subject, data
-const vc = await VerifiableCredential.create({ 
-  type: 'SwiftiesFanClub', 
-  issuer: fanClubIssuerDid.did, 
-  subject: aliceDid.did, 
-  data: new SwiftiesFanClub('Stan', true) 
+const vc = await VerifiableCredential.create({
+  type: 'SwiftiesFanClub',
+  issuer: fanClubIssuerDid.uri,
+  subject: aliceDid.uri,
+  data: new SwiftiesFanClub('Stan', true)
 });
 
 console.log('Unsigned VC: \n ' + vc.toString() + '\n');
@@ -48,7 +50,7 @@ try {
 }
 
 // Parse
-const parsedVc = VerifiableCredential.parseJwt({ vcJwt: signedVcJwt} );
+const parsedVc = VerifiableCredential.parseJwt({ vcJwt: signedVcJwt });
 console.log('\nParsed VC: \n' + parsedVc.toString() + '\n');
 
 /** 
@@ -57,14 +59,14 @@ console.log('\nParsed VC: \n' + parsedVc.toString() + '\n');
 
 // Prerequisites: Create Presentation Definition
 const presentationDefinition = {
-  'id'                : 'presDefId123',
-  'name'              : 'T Swift Fan Club Presentation Definition',
-  'purpose'           : 'for getting into the fan club',
-  'input_descriptors' : [
+  'id': 'presDefId123',
+  'name': 'T Swift Fan Club Presentation Definition',
+  'purpose': 'for getting into the fan club',
+  'input_descriptors': [
     {
-      'id'          : 'legitness',
-      'purpose'     : 'are you legit or not?',
-      'constraints' : {
+      'id': 'legitness',
+      'purpose': 'are you legit or not?',
+      'constraints': {
         'fields': [
           {
             'path': [
@@ -80,7 +82,7 @@ const presentationDefinition = {
 // Satisfies Presentation Definition
 try {
   PresentationExchange.validateDefinition({ presentationDefinition });
-  PresentationExchange.satisfiesPresentationDefinition({vcJwts: [signedVcJwt], presentationDefinition: presentationDefinition});
+  PresentationExchange.satisfiesPresentationDefinition({ vcJwts: [signedVcJwt], presentationDefinition: presentationDefinition });
   console.log('\nVC Satisfies Presentation Definition!\n');
 } catch (err) {
   console.log('VC does not satisfy Presentation Definition: ' + err.message);
@@ -88,67 +90,5 @@ try {
 
 
 // Create Presentation Result that contains a Verifiable Presentation and Presentation Submission
-const presentationResult = PresentationExchange.createPresentationFromCredentials({vcJwts: [signedVcJwt], presentationDefinition: presentationDefinition });
+const presentationResult = PresentationExchange.createPresentationFromCredentials({ vcJwts: [signedVcJwt], presentationDefinition: presentationDefinition });
 console.log('\nPresentation Result: ' + JSON.stringify(presentationResult));
-
-/** 
- * Storing a self signed VC in a DWN
- */
-
-// Web5 Connect
-const myDid = await DidKeyMethod.create();
-const userAgent = await Web5UserAgent.create();
-
-// Start the agent.
-await userAgent.start({ passphrase: 'insecure-static-phrase' });
-
-// Import the did and create an identity
-const identity = await userAgent.identityManager.import({
-  did       : myDid,
-  identity  : { did: myDid.did, name: 'whatever' },
-  kms       : 'local'
-});
-
-/** Import the Identity metadata to the User Agent's tenant so that it can be restored
- * on subsequent launches or page reloads. */
-await userAgent.identityManager.import({ identity, context: userAgent.agentDid });
-
-const web5 = new Web5({ agent: userAgent, connectedDid: myDid.did });
-
-class DateOfBirth {
-  constructor(dob) {
-    this.dob = dob;
-  }
-}
-
-// Create self signed VC
-const dwnVc = await VerifiableCredential.create({ type: 'DateOfBirth', issuer: myDid.did, subject: myDid.did, data: new DateOfBirth('1989-11-11') });
-const signedDwnVc = await dwnVc.sign({ did: myDid });
-
-console.log(signedDwnVc)
-
-// Storing VC in DWN
-const { record } = await web5.dwn.records.create({
-  data    : signedDwnVc,
-  message : {
-    schema     : 'DateOfBirth',
-    dataFormat : 'application/vc+jwt',
-  },
-});
-
-console.log('\nVC Record ID: ' + record.id + '\n');
-
-
-// Reading VC from DWN
-const { record: readRecord } = await web5.dwn.records.read({
-  message: {
-    filter: {
-      recordId: record.id
-    }
-  }
-});
-
-const readVcJwt = await readRecord.data.text();
-console.log('\nVC Record: \n' + readVcJwt + '\n');
-
-console.log('Finished!');
