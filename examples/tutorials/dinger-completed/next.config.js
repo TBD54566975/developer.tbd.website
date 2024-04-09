496319e91711e835cb868be4001f3d6d4bb4256c
Rizel <rizel@tbd.email>
2024-04-09 11:18:40 -0400

    remove buffer

--- examples/tutorials/dinger-completed/next.config.js
+++ examples/tutorials/dinger-completed/next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
         ...config.resolve.fallback,
         stream: require.resolve('stream-browserify'),
         crypto: require.resolve('crypto-browserify'),
-        buffer: require.resolve('buffer/'),
       };
 
       config.plugins.push(
