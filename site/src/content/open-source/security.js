39122b424220363b181c17a0a2726b6b1d925cf8
Andrew Lee Rubinger <alr@alrubinger.com>
2024-04-11 03:09:39 -0700

    Issue #1385: Create Security Policy

---
+++ site/src/content/open-source/security.js
@@ -0,0 +1,13 @@
+export const content = {
+
+  buttons: [
+    {
+      type: 'button',
+      data: {
+        label: 'Report Security Vulnerability to security@tbd.email',
+        url: 'mailto:security@tbd.email',
+        isExternalLink: false
+      },
+    },
+  ]
+};
