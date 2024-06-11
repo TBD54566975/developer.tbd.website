df7ba004b0fcf7a32a43d624a98798962b81a312
Chris Giglio <chrisgiglio@tbd.email>
2024-06-11 13:50:17 -0500

    First commit for wallet quickstart, not complete

---
+++ site/code-snippets/tbdex/quickstart.js
@@ -0,0 +1,202 @@
+import { test, expect, describe, beforeAll, afterAll } from 'vitest';
+import { TbdexHttpClient, Rfq, Quote, Order, Close } from '@tbdex/http-client';
+import { PresentationExchange } from '@web5/credentials';
+import { DidDht } from '@web5/dids';
+
+let context = {};
+
+export async function quickstartGetOfferings() {
+    context.pfiDid = 'did:dht:e4re9qwmtfqbonqcpkrfac3h56417tor7hm7rcsfcgxwj8dwfqqo';
+    context.customerDid = globalThis.did;
+    await getOfferings();
+
+    return context.selectedOffering;
+}
+
+export async function quickstartGetCredentials() {
+    await getCredentials();
+
+    return context.credentials;
+}
+
+export async function quickstartCreateRfq() {
+    await createRfq();
+
+    return context.rfq;
+}
+
+export async function quickstartSendRfq() {
+    await getOfferings();
+    await getCredentials();
+    await createRfq();
+    await sendRfq();
+
+    return context.rfq;
+}
+
+export async function quickstartProcessQuote() {
+    await getOfferings();
+    await getCredentials();
+    await createRfq();
+    await sendRfq();
+    await processQuote();
+
+    return context.quote;
+}
+
+export async function quickstartCreateOrder() {
+    await getOfferings();
+    await getCredentials();
+    await createRfq();
+    await sendRfq();
+    await processQuote();
+    await createOrder();
+
+    return context.order;
+}
+
+export async function quickstartSubmitOrder() {
+    await getOfferings();
+    await getCredentials();
+    await createRfq();
+    await sendRfq();
+    await processQuote();
+    await createOrder();
+    await submitOrder();
+
+    return context.order;
+}   
+
+export async function quickstartProcessClose() {
+    await getOfferings();
+    await getCredentials();
+    await createRfq();
+    await sendRfq();
+    await processQuote();
+    await createOrder();
+    await submitOrder();
+    await processClose();
+
+    return context.reasonForClose;
+}
+
+async function getOfferings() {
+    const offerings  = await TbdexHttpClient.getOfferings({ pfiDid: context.pfiDid });
+
+    // TODO: Get credentials and the presentation definition
+
+    context.selectedOffering = offerings[0];
+}
+
+async function getCredentials() {
+    let myCredentials = [];
+
+    // :snippet-start: walletQuickstartSelectCredentials
+    const selectedCredentials = PresentationExchange.selectCredentials({
+        vcJwts: myCredentials,
+        presentationDefinition: context.selectedOffering.data.requiredClaims,
+    });
+    // :snippet-end:
+    context.credentials = selectedCredentials;
+}
+
+async function createRfq() {
+    const BTC_ADDRESS = 'bc1q52csjdqa6cq5d2ntkkyz8wk7qh2qevy04dyyfd'
+    const selectedCredentials = context.credentials;
+
+    // :snippet-start: walletQuickstartCreateRFQ
+    context.rfq = Rfq.create({
+        metadata: {
+        to: context.selectedOffering.metadata.from, // PFI's DID
+        from: context.customerDid,              // Customer DID
+        protocol: '1.0'                     // Version of tbDEX protocol you're using
+        },
+        data: {
+        offeringId: context.selectedOffering.metadata.id,   // The ID of the selected offering
+        payin: {
+            kind: 'DEBIT_CARD',                       // The method of payment
+            amount: '500.65',                         // The amount of the payin currency 
+            paymentDetails: {
+            cardNumber: '1234567890123456',
+            expiryDate: '05/25',
+            cardHolderName: 'Alice Doe',
+            cvv: '123'
+            }
+        },
+        payout: {
+            kind: 'BTC_ADDRESS',                      // The method for receiving payout                         
+            paymentDetails: {
+            btcAddress: BTC_ADDRESS                 // Recipient's BTC wallet address
+            }
+        },
+        claims: selectedCredentials  // Array of signed VCs required by the PFI
+        }
+    });
+}
+
+async function sendRfq() {
+    try{
+        context.rfq.verifyOfferingRequirements(context.selectedOffering);
+        await context.rfq.sign(context.customerDid);
+        await TbdexHttpClient.createExchange(context.rfq);
+    } catch (e) {
+        // handle failed verification
+    }
+}
+
+async function processQuote() {
+    context.exchangeId = context.rfq.exchangeId;
+    while (!context.quote) {
+        const exchange = await TbdexHttpClient.getExchange({
+            pfiDid: context.rfq.metadata.to,
+            did: context.customerDid,
+            exchangeId: context.exchangeId
+        });
+
+        context.quote = exchange.find(msg => msg instanceof Quote);
+
+        if (!context.quote) {
+            // Make sure the exchange is still open
+            context.close = exchange.find(msg => msg instanceof Close);
+            
+            if(context.close) { break; } 
+            else {
+            // Wait 2 seconds before making another request
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            }
+        }
+    }
+}
+
+async function createOrder() {
+    context.order = Order.create({
+        metadata: {
+        from: context.customerDid,         // Customer's DID
+        to: context.pfiDid,       // PFI's DID
+        exchangeId: context.exchangeId,  // Exchange ID from the Quote
+        protocol: "1.0"                // Version of tbDEX protocol you're using
+        }
+    });
+}
+
+async function submitOrder() {
+    await context.order.sign(context.customerDid);
+    await TbdexHttpClient.submitOrder(context.order);
+}
+
+async function processClose() {
+    while (!close) {
+        const exchange = await TbdexHttpClient.getExchange({
+            pfiDid: context.pfiDid,
+            did: context.customerDid,
+            exchangeId: context.exchangeId
+        })
+
+        for (const message of exchange) {
+            if (message instanceof Close) {
+                close = message
+            }
+        }
+    }
+    context.reasonForClose = close.data.reason;
+}
---
+++ site/testsuites/testsuite-javascript/__tests__/tbdex/wallet/quickstart.test.js
@@ -0,0 +1,177 @@
+import { test, expect, describe, beforeAll, afterAll } from 'vitest';
+// :snippet-start: walletQuickstartImports
+import { TbdexHttpClient, Rfq, Quote, Order, Close } from '@tbdex/http-client';
+import { PresentationExchange } from '@web5/credentials';
+import { DidDht } from '@web5/dids';
+// :snippet-end:
+
+let customerDid = globalThis.did;
+let pfiDid = 'did:dht:e4re9qwmtfqbonqcpkrfac3h56417tor7hm7rcsfcgxwj8dwfqqo';
+let selectedOffering;
+let credentials;
+let exchangeId;
+let rfq;
+let quote;
+let order;
+
+describe('Wallet: Quickstart', () => {
+
+    test('getOfferings HTTP Call', async () => {
+        // :snippet-start: walletQuickstartGetOfferings
+        const offerings  = await TbdexHttpClient.getOfferings({ pfiDid: pfiDid });
+        // :snippet-end:
+
+        // TODO: Get credentials and the presentation definition
+
+        selectedOffering = offerings[0];
+        expect(selectedOffering).toBeDefined();
+    });
+
+    test('Pull Wallet Credentials', async () => {
+        // Get the credential like this:
+        // https://github.com/TBD54566975/tbdex-pfi-exemplar/blob/268bf174362d67b1f0408e3342b0807cc31461af/src/example/issue-credential.ts
+
+        let myCredentials = [];
+
+        // :snippet-start: walletQuickstartSelectCredentials
+        const selectedCredentials = PresentationExchange.selectCredentials({
+            vcJwts: myCredentials,
+            presentationDefinition: selectedOffering.data.requiredClaims,
+        });
+        // :snippet-end:
+        credentials = selectedCredentials;
+        expect(selectedCredentials.length).toBe(1);
+    });
+    
+    test('Create RFQ', async () => {
+        const BTC_ADDRESS = 'bc1q52csjdqa6cq5d2ntkkyz8wk7qh2qevy04dyyfd'
+        const selectedCredentials = credentials;
+
+        // :snippet-start: walletQuickstartCreateRfq
+        rfq = Rfq.create({
+            metadata: {
+            to: selectedOffering.metadata.from, // PFI's DID
+            from: customerDid.uri,              // Customer DID
+            protocol: '1.0'                     // Version of tbDEX protocol you're using
+            },
+            data: {
+            offeringId: selectedOffering.metadata.id,   // The ID of the selected offering
+            payin: {
+                kind: 'DEBIT_CARD',                       // The method of payment
+                amount: '500.65',                         // The amount of the payin currency 
+                paymentDetails: {
+                cardNumber: '1234567890123456',
+                expiryDate: '05/25',
+                cardHolderName: 'Alice Doe',
+                cvv: '123'
+                }
+            },
+            payout: {
+                kind: 'BTC_ADDRESS',                      // The method for receiving payout                         
+                paymentDetails: {
+                btcAddress: BTC_ADDRESS                 // Recipient's BTC wallet address
+                }
+            },
+            claims: selectedCredentials  // Array of signed VCs required by the PFI
+            }
+        });
+        // :snippet-end:
+        
+        expect(() => {
+            try{
+                rfq.verifyOfferingRequirements(selectedOffering);
+            } catch (e) {
+                // handle failed verification
+            }
+        }).not.toThrow();
+    });
+
+    test('Verify, send, and sign RFQ', async () => {
+        expect(async () => {
+            // :snippet-start: walletQuickstartSendRfq
+            try{
+                rfq.verifyOfferingRequirements(selectedOffering);
+                await rfq.sign(customerDid);
+                await TbdexHttpClient.createExchange(rfq);
+            } catch (e) {
+                // handle failed verification
+            }
+            // :snippet-end:
+        }).not.toThrow();
+    });
+
+    test('Process Quote and Create Order', async () => {
+        // :snippet-start: walletQuickstartProcessQuote
+        // Wait for Quote message to appear in the exchange
+        exchangeId = rfq.exchangeId;
+        while (!quote) {
+            const exchange = await TbdexHttpClient.getExchange({
+                pfiDid: rfq.metadata.to,
+                did: customerDid,
+                exchangeId: exchangeId
+            });
+
+            quote = exchange.find(msg => msg instanceof Quote);
+
+            if (!quote) {
+                // Make sure the exchange is still open
+                close = exchange.find(msg => msg instanceof Close);
+                
+                if(close) { break; } 
+                else {
+                // Wait 2 seconds before making another request
+                await new Promise(resolve => setTimeout(resolve, 2000));
+                }
+            }
+        }
+        // :snippet-end:
+
+        expect(quote).toBeDefined();
+    });
+
+    test('Create Order', async () => {
+        // :snippet-start: walletQuickstartCreateOrder
+        const order = Order.create({
+            metadata: {
+            from: customerDid,         // Customer's DID
+            to: pfiDid,       // PFI's DID
+            exchangeId: exchangeId,  // Exchange ID from the Quote
+            protocol: "1.0"                // Version of tbDEX protocol you're using
+            }
+        });
+        // :snippet-end:
+
+        expect(order).toBeDefined();
+    });
+        
+    test('Sign and Submit Order', async () => {
+        expect(async () => {
+            // :snippet-start: walletQuickstartSendOrder
+            await order.sign(customerDid);
+            await TbdexHttpClient.submitOrder(order);
+            // :snippet-end:
+        }).not.toThrow();
+    });
+
+    test('Process Close', async () => {
+        while (!close) {
+            const exchange = await TbdexHttpClient.getExchange({
+                pfiDid: pfiDid,
+                did: customerDid,
+                exchangeId: exchangeId
+            })
+
+            for (const message of exchange) {
+                if (message instanceof Close) {
+                    close = message
+                }
+            }
+        }
+
+        // :snippet-start: walletQuickstartProcessClose
+        const reasonForClose = close.data.reason;
+        // :snippet-end:
+
+        expect(reasonForClose).toBeDefined();
+        });
+});
