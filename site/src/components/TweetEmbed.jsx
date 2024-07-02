397ec194db41da687dedc498e185e3c6432af29e
angiejones <jones.angie@gmail.com>
2024-07-02 02:14:12 -0500

    added photos

---
+++ site/src/components/TweetEmbed.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+
+const TweetEmbed = ({ tweetId }) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <blockquote className="twitter-tweet">
+      <a href={`https://twitter.com/user/status/${tweetId}`}></a>
+    </blockquote>
+  );
+};
+
+export default TweetEmbed;
