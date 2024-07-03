f095e747df2246b23933b31ea2dbb166c47c99f2
Ebony Louis <55366651+EbonyLouis@users.noreply.github.com>
2024-07-02 20:59:55 -0400

    updating credentialSchema & add exemplar (#1453)
    * updating credentialSchema & add exemplar
    
    * adding examplar to wallet guide
    
    * updating the wording
    
    * update schema link
    
    * updating schema name
    
    * kccSchema change
    
    * 🤞🏾
    
    ---------
    
    Co-authored-by: Angie Jones <jones.angie@gmail.com>

--- site/testsuites/testsuite-javascript/__tests__/tbdex/issuer/kcc/knownCustomerCredentialIssuer.test.js
+++ site/testsuites/testsuite-javascript/__tests__/tbdex/issuer/kcc/knownCustomerCredentialIssuer.test.js
@@ -431,7 +431,7 @@ app.post('/credentials', async (req, res) => {
      * Create and sign the credential
      ************************************************/
     const kccCredentialInstance = new KccCredential('US', 'Gold', {
-        id: "https://schema.org/PFI",
+        id: "https://developer.tbd.website/schemas/kccSchema.json",
         type: "JsonSchema"
       },
       [
