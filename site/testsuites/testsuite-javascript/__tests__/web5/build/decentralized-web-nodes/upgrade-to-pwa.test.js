5896f8c736a0615f657311e44a74456ae98f99fd
Rizel Scarlett <rizel@blackgirlbytes.local>
2024-07-18 07:44:36 -0400

    fix tests

--- site/testsuites/testsuite-javascript/__tests__/web5/build/decentralized-web-nodes/upgrade-to-pwa.test.js
+++ site/testsuites/testsuite-javascript/__tests__/web5/build/decentralized-web-nodes/upgrade-to-pwa.test.js
@@ -1,23 +1,25 @@
-import { beforeAll, describe, test, expect, vi } from 'vitest';
+import { beforeAll, beforeEach, afterEach, describe, test, expect, vi } from 'vitest';
 import { setUpWeb5 } from '../../../setup-web5';
 
 describe('Testing upgrade to PWA', () => {
   let web5, did;
   const recordId = "bafyreifsfh74ghrkmok7rw5ci6ayhz2bdoeaen4udygmq52twi5lu2jsju";  
+  let originalFetch;
+
   beforeAll(async () => {
-    // Set up Web5 with whatever global setup you need
     await setUpWeb5();
     web5 = globalThis.web5;
     did = globalThis.did;
+  });
 
+  beforeEach(() => {
+    originalFetch = global.fetch; // Save the original fetch function
     // Mock @web5/api
-    vi.mock('@web5/api', () => {
-      return {
-        Web5: {
-          connect: vi.fn(() => Promise.resolve({ web5, did }))
-        }
-      };
-    });
+    vi.mock('@web5/api', () => ({
+      Web5: {
+        connect: vi.fn(() => Promise.resolve({ web5, did }))
+      }
+    }));
 
     // Mock fetch globally in your test environment
     global.fetch = vi.fn((url) => {
@@ -32,12 +34,15 @@ describe('Testing upgrade to PWA', () => {
     });
   });
 
+  afterEach(() => {
+    vi.resetAllMocks(); // Reset mocks to their original state
+    global.fetch = originalFetch; // Restore the original fetch function
+  });
+
   test('drl fetches a read record', async () => {
-     // :snippet-start: drlFetchReadRecord
     const dwebUrl = `https://dweb/${did}/read/records/${recordId}`;
     const response = await fetch(dwebUrl);
     const data = await response.json();
-    // :snippet-end:
 
     expect(response.ok).toBeTruthy();
     expect(response.status).toBe(200);
