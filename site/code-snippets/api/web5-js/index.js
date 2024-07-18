25e84a84f59614d8ceb4937b54d228043aa4c912
Rizel Scarlett <rizel@blackgirlbytes.local>
2024-07-18 01:27:46 -0400

    for quick review

--- site/code-snippets/api/web5-js/index.js
+++ site/code-snippets/api/web5-js/index.js
@@ -11,6 +11,24 @@ export async function connectWithDWNEndpoint() {
   return did;
 }
 
+export async function connectToCommunityNode(){
+  const {did, web5} = await Web5.connect({
+    didCreateOptions: {
+      dwnEndpoints: ['https://dwn.gcda.xyz'], // User provides google's community DWN instance
+    },
+    registration: {
+      onSuccess: () => {
+        // Registration succeeded, set a local storage value to indicate the user is registered and registration does not need to occur again.
+      },
+      onFailure: (error) => {
+        // Registration failed, display an error message to the user, and pass in the registration object again to retry next time the user connects.
+      },
+    },
+  })
+  return did
+}
+
+
 export async function connectWithAgentAndConnectedDid(existingDid) {
   const { web5, did } = await Web5.connect({
     agent: identityAgent,
