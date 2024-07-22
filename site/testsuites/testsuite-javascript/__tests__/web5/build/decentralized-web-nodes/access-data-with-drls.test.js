b8f7ccddf412a35f7866e7415dd538f0fa3b2ccb
Rizel Scarlett <rizel@tbd.email>
2024-07-21 20:37:45 -0400

    activate-polyfills guide (#1476)
    * first steps of activate-polyfills
    
    * test added
    
    * fix import
    
    * fix tests
    
    * remove comments
    
    * missing code snippet
    
    * change title to say upgrade to a DWA instead
    
    * highlights DWAs better
    
    * clarifying update
    
    * address comments
    
    * better wording
    
    * update typo
    
    * maybe this is a better explanation
    
    * typo
    
    * Update community node note
    
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * Update package import note
    
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * update prefix
    
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * update drl wording
    
    * fix merge conflict
    
    * add image tag code snippet
    
    * fixing snippet
    
    * explaining img src url
    
    * Update site/docs/web5/build/decentralized-web-nodes/upgrade-to-dwa.mdx
    
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * update introduction
    
    * update titles
    
    * change file name
    
    * maybe bold
    
    * acronym only because explained in beginning
    
    * maybe this will show the whole variable
    
    * Update access-data-with-drls.mdx
    
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * Update access-data-with-drls.mdx
    
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * Update access-data-with-drls.mdx
    
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * update dweburl to drl
    
    * shnippet update dweburl to drl
    
    ---------
    
    Co-authored-by: Rizel Scarlett <rizel@blackgirlbytes.local>
    Co-authored-by: Angie Jones <jones.angie@gmail.com>

---
+++ site/testsuites/testsuite-javascript/__tests__/web5/build/decentralized-web-nodes/access-data-with-drls.test.js
@@ -0,0 +1,82 @@
+import { beforeAll, beforeEach, afterEach, describe, test, expect, vi } from 'vitest';
+import { setUpWeb5 } from '../../../setup-web5';
+import {
+  uploadImage,
+} from '../../../../../../code-snippets/web5/build/decentralized-web-nodes/write-to-dwn';
+
+describe('Testing upgrade to PWA', () => {
+  let web5, did;
+  let originalFetch;
+  let recordId;
+
+  beforeAll(async () => {
+    await setUpWeb5();
+    web5 = globalThis.web5;
+    did = globalThis.did;
+  });
+
+  beforeEach(() => {
+    originalFetch = global.fetch; 
+
+    vi.mock('@web5/api', () => ({
+      Web5: {
+        connect: vi.fn(() => Promise.resolve({ web5, did }))
+      }
+    }));
+
+    global.fetch = vi.fn((url) => {
+      // Step 3: Use the variable in your mocked fetch function
+      if (url === `https://dweb/${did}/read/records/${recordId}`) {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          blob: () => Promise.resolve(new Blob(['fake image data'], { type: 'image/png' }))
+        });
+      }
+      return Promise.reject(new Error('URL not found'));
+    });
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks(); 
+    global.fetch = originalFetch;
+  });
+
+  test('drl fetches a read record', async () => {
+    const mockEvent = {
+      currentTarget: {
+        files: [new Blob(['fake image data'], { type: 'image/png' })],
+      },
+    };
+    const record = await uploadImage(mockEvent)
+    recordId = record.id;
+    // :snippet-start: drlFetchReadRecord
+    const drl = `https://dweb/${did}/read/records/${recordId}`;
+    const response = await fetch(drl);
+    const imageUrl = URL.createObjectURL(await response.blob());
+    // :snippet-end:
+    expect(response.ok).toBeTruthy();
+    expect(response.status).toBe(200);
+    expect(imageUrl.startsWith('blob:')).toBeTruthy();
+  });
+
+  test('set image src to url', async () => {
+    const mockEvent = {
+      currentTarget: {
+        files: [new Blob(['fake image data'], { type: 'image/png' })],
+      },
+    };
+    const record = await uploadImage(mockEvent)
+    recordId = record.id;
+    const drl = `https://dweb/${did}/read/records/${recordId}`;
+    const response = await fetch(drl);
+    const imageUrl = URL.createObjectURL(await response.blob());
+      return (
+         `
+          // :snippet-start: renderImageUrlTag
+         <img src="${imageUrl}" alt="uploaded image" />
+         // :snippet-end:
+         `
+      )
+  });
+});
