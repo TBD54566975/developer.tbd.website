72514bdfb0b83c0d51b4cf251b89b0cd0e4c850b
dayhaysoos <ndejesus1227@gmail.com>
2024-07-26 00:07:44 -0400

    lots of changes

--- site-new/src/components/LanguageSwitcher.tsx
+++ site-new/src/components/LanguageSwitcher.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import JsIcon from "@site/static/img/js-icon";
+import Chevron from "@site/static/img/chevron";
 import { useLanguage } from "../context/LanguageSwitcher";
 
 function LanguageSwitcher() {
@@ -24,56 +25,55 @@ function LanguageSwitcher() {
   };
 
   return (
-    <div className="flex-col justify-start items-start pl-2 font-spaceGrotesk">
-      <div className="theme-doc-sidebar-item-link theme-doc-sidebar-item-link-level-1 category-label mb-2">
-        LANGUAGE
-      </div>
+    <div className="flex-col justify-start items-start pl-2 font-spaceGrotesk bg-tbd-gray-shade-1">
+      <div className="eyebrow pl-2 py-2">LANGUAGE</div>
       <section>
         <div className="flex items-center justify-start pb-4">
           <div className="relative inline-block w-full">
             <div
-              className="flex items-center justify-between cursor-pointer p-2 border border-gray-300 rounded-none bg-tbd-yellow"
+              className="flex items-center justify-between cursor-pointer p-2 border border-gray-300 rounded-none bg-tbd-gray-shade-1"
               onClick={toggleDropdown}
             >
               <div className="flex items-center">
-                <img
-                  src={selectedOption.icon}
-                  alt={selectedOption.label}
-                  className="w-5 h-5 mr-2"
+                <JsIcon
+                  className="w-5 h-5 mr-2 fill-current text-tbd-yellow"
+                  fill="yellow"
                 />
-                <span className="mr-2">{selectedOption.label}</span>
+                <span className="mr-2 text-white">{selectedOption.label}</span>
               </div>
-              <img
-                src={useBaseUrl("/img/chevron.svg")}
-                alt="Caret Icon"
-                className={`w-4 h-4 transition-transform ${
-                  isOpen
-                    ? "transform rotate-[90deg]"
-                    : "transform rotate-[-90deg]"
+
+              <Chevron
+                className={`h-6 w-6 transition-all ${
+                  isOpen ? "transform rotate-0" : "transform rotate-180"
                 }`}
+                fill="white"
               />
             </div>
-            {isOpen && (
-              <div className="absolute bottom-full left-0 w-full h-10 mt-1 border border-gray-300 rounded-none bg-dark-grey">
-                {options.map((option) => (
-                  <div
-                    key={option.label}
-                    className="flex items-center cursor-pointer p-2 hover:bg-gray-700"
-                    onClick={() => {
-                      handleChange({ target: { value: option.label } });
-                    }}
-                  >
-                    <JsIcon
-                      className="w-5 h-5 mr-2 fill-current text-tbd-yellow"
-                      fill="yellow"
-                    />
-                    <span className="mr-2 text-white font-bold">
-                      {option.label}
-                    </span>
-                  </div>
-                ))}
-              </div>
-            )}
+            <div
+              className={`absolute bottom-full left-0 w-full border border-gray-300 rounded-none bg-dark-grey transition-transform duration-300 ${
+                isOpen
+                  ? "translate-y-0 opacity-100"
+                  : "translate-y-4 opacity-0 pointer-events-none"
+              }`}
+            >
+              {options.map((option) => (
+                <div
+                  key={option.label}
+                  className="flex items-center cursor-pointer p-2 hover:bg-gray-700"
+                  onClick={() => {
+                    handleChange({ target: { value: option.label } });
+                  }}
+                >
+                  <JsIcon
+                    className="w-5 h-5 mr-2 fill-current text-tbd-yellow"
+                    fill="yellow"
+                  />
+                  <span className="mr-2 text-white font-bold">
+                    {option.label}
+                  </span>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </section>
