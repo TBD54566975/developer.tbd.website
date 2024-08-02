77620a4e3ff7e858dbc79792d994ecebbd610009
Nick DeJesus <ndejesus1227@gmail.com>
2024-08-02 12:26:52 -0400

    responsiveness for the component (#1521)
    * responsiveness for the component
    
    * refactored for variants
    
    * refactored for variants

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,3 +1,5 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 module.exports = {
   mode: 'jit',
   purge: [
@@ -15,18 +17,12 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
+    screens: {
+      ...defaultTheme.screens,
+    },
     extend: {
       screens: {
-        'tablet': '768px',
-        // => @media (min-width: 768px) { ... }
-        'desktop': '1024px',
-        // => @media (min-width: 1024px) { ... }
-        'desktop-lg': '1200px',
-        // => @media (min-width: 1200) { ... }
-        'desktop-xl': '1400px',
-        // => @media (min-width: 1200) { ... }
-        'hd': '1820px',
-        // => @media (min-width: 1820px) { ... }
+        mobile: { raw: '(max-width: 360px)' },
       },
       colors: {
         'primary-yellow': 'var(--color-yellow)',
