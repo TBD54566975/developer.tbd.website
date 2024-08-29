f8c3f046e561ec62421c62774536550d98977625
Adewale Abati <acekyd01@gmail.com>
2024-08-29 16:18:23 +0200

    Updated tbDEX Wallet Quickstart

--- site/code-snippets/tbdex/wallet-quickstart.js
+++ site/code-snippets/tbdex/wallet-quickstart.js
@@ -6,148 +6,89 @@ import { VerifiableCredential } from '@web5/credentials'
 let context = {};
 
 export async function quickstartDidCreate() {
-    let customerDidString = '{"uri":"did:dht:h8e3yqnhgjwhtkjhxwhfy5mmkn4nebqxr8idguwrsxgef6ow8efo","document":{"id":"did:dht:h8e3yqnhgjwhtkjhxwhfy5mmkn4nebqxr8idguwrsxgef6ow8efo","verificationMethod":[{"id":"did:dht:h8e3yqnhgjwhtkjhxwhfy5mmkn4nebqxr8idguwrsxgef6ow8efo#0","type":"JsonWebKey","controller":"did:dht:h8e3yqnhgjwhtkjhxwhfy5mmkn4nebqxr8idguwrsxgef6ow8efo","publicKeyJwk":{"crv":"Ed25519","kty":"OKP","x":"4dGQOFwyacipPH04UG1rULQkBc8h6jNOhLPMgvoUOgs","kid":"XPokllC3LZAVGizIL0naDdByQHeyY12uLJaXO4j46Nw","alg":"EdDSA"}}],"authentication":["did:dht:h8e3yqnhgjwhtkjhxwhfy5mmkn4nebqxr8idguwrsxgef6ow8efo#0"],"assertionMethod":["did:dht:h8e3yqnhgjwhtkjhxwhfy5mmkn4nebqxr8idguwrsxgef6ow8efo#0"],"capabilityDelegation":["did:dht:h8e3yqnhgjwhtkjhxwhfy5mmkn4nebqxr8idguwrsxgef6ow8efo#0"],"capabilityInvocation":["did:dht:h8e3yqnhgjwhtkjhxwhfy5mmkn4nebqxr8idguwrsxgef6ow8efo#0"]},"metadata":{"published":true,"versionId":"1718740086"},"privateKeys":[{"crv":"Ed25519","d":"iTfn3Z8uPp3gTg-9LxQVZVODGqnP3M0UDjZiIwBEctc","kty":"OKP","x":"4dGQOFwyacipPH04UG1rULQkBc8h6jNOhLPMgvoUOgs","kid":"XPokllC3LZAVGizIL0naDdByQHeyY12uLJaXO4j46Nw","alg":"EdDSA"}]}'
+    context.customerDid = await DidDht.create({
+        options: {
+            publish: true,
+        },
+    });
 
-    const portableDid = JSON.parse(customerDidString);
-    context.customerDid = await DidDht.import({ portableDid });
-    context.pfiDid = 'did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo';
+    context.pfiDid = 'did:dht:3fkz5ssfxbriwks3iy5nwys3q5kyx64ettp9wfn1yfekfkiguj1y';
     return context.customerDid.uri;
 }
 
 export async function quickstartGetOfferings() {
-    await getOfferings();
+    try {
+      const offerings  = await TbdexHttpClient.getOfferings({ pfiDid: context.pfiDid });
+
+      if (offerings) {
+          context.selectedOffering = offerings.find(
+              (offering) =>
+                  offering.data.payin.currencyCode === 'USD' &&
+                  offering.data.payout.currencyCode === 'KES'
+          );
+      }
+
+      console.log(context.selectedOffering);
+    } catch (e) {
+        console.log(e.message);
+    }
 
     return context.selectedOffering;
 }
 
 export async function quickstartApplyForCredential() {
-    await applyForCredentials();
+    const vcJwt = await fetch(`https://mock-idv.tbddev.org/kcc?name=angie&country=US&did=${context.customerDid.uri}`).then(res => res.text());
+
+    context.credentials = [vcJwt];
 
     return context.credentials;
 }
 
 export async function quickstartGetCredentials() {
-    await getCredentials();
+    let customerCredentials = context.credentials;
+
+    const selectedCredentials = PresentationExchange.selectCredentials({
+        vcJwts: customerCredentials,
+        presentationDefinition: context.selectedOffering.data.requiredClaims,
+    });
+
+    context.credentials = selectedCredentials;
+
+    console.log("the selection", context.credentials);
 
     return context.credentials;
 }
 
 export async function quickstartCreateRfq() {
-    await createRfq();
-    await sendRfq();
-
+      context.rfq = Rfq.create({
+      metadata: {
+          to: context.pfiDid, // PFI's DID
+          from: context.customerDid.uri, // Customer DID
+          protocol: '1.0', // Version of tbDEX protocol you're using
+      },
+      data: {
+          offeringId: context.selectedOffering.metadata.id, // The ID of the selected offering
+          payin: {
+              kind: 'USD_BANK_TRANSFER', // The method of payment
+              amount: '500.65', // The amount of the payin currency
+              paymentDetails: {
+                  accountNumber: '1234567890',
+                  routingNumber: '123456789',
+              },
+          },
+          payout: {
+              kind: 'KES_BANK_TRANSFER', // The method for receiving payout
+              paymentDetails: {
+                  accountNumber: '3245231234', // Details required to execute payment
+              },
+          },
+          claims: context.credentials, // Array of signed VCs required by the PFI
+      },
+  });
     return context.rfq;
 }
 
 export async function quickstartSendRfq() {
-    return "HTTP 202: Successfully Submitted RFQ";
-}
-
-export async function quickstartProcessQuote() {
-    await processQuote();
-
-    return context.quote;
-}
-
-export async function quickstartCreateOrder() {
-    await createOrder();
-    await submitOrder();
-
-    return context.order;
-}
-
-export async function quickstartSendOrder() {
-    return "HTTP 202: Successfully Submitted Order";
-}
-
-export async function quickstartProcessClose() {
-    await processClose();
-
-    return context.reasonForClose;
-}
-
-async function getOfferings() {
-
-    try {
-        const offerings  = await TbdexHttpClient.getOfferings({ pfiDid: context.pfiDid });
-    
-        context.selectedOffering = offerings[0];
-        console.log(context.selectedOffering);
-    } catch (e) {
-        console.log(e.message);
-    }
-    
-}
-
-async function applyForCredentials() {
-    let issuerDid = '{"uri":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo","document":{"id":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo","verificationMethod":[{"id":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0","type":"JsonWebKey","controller":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo","publicKeyJwk":{"crv":"Ed25519","kty":"OKP","x":"In71mcx1pEBaAjFPnNIbNIccBDwVVsgRl2AmRSNFXW8","kid":"yBmVo8U4VzCFqhj0a88kiZPOU_gpMxEmHUy-tdvKqHM","alg":"EdDSA"}}],"authentication":["did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0"],"assertionMethod":["did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0"],"capabilityDelegation":["did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0"],"capabilityInvocation":["did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0"],"service":[{"id":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#pfi","type":"PFI","serviceEndpoint":"https://pfiexemplar.tbddev.org"}]},"metadata":{"published":true,"versionId":"1721323484"},"privateKeys":[{"crv":"Ed25519","d":"rTGqrDYW2rQUGsnPAtpPO0ZVe585WDbbtkufheCAloU","kty":"OKP","x":"In71mcx1pEBaAjFPnNIbNIccBDwVVsgRl2AmRSNFXW8","kid":"yBmVo8U4VzCFqhj0a88kiZPOU_gpMxEmHUy-tdvKqHM","alg":"EdDSA"}]}'
-    const portableDid = JSON.parse(issuerDid);
-    const issuer = await DidDht.import({ portableDid });
-
-    const vc = await VerifiableCredential.create({
-        type    : 'SanctionCredential',
-        issuer  : issuer.uri,
-        subject : context.customerDid.uri,
-        data    : {
-            'beep': 'boop'
-        }
-        })
-        
-    const vcJwt = await vc.sign({ did: issuer})
-
-    // Hard-coded credential
-    context.credentials = [vcJwt];
-
-    return vcJwt;
-}
-
-async function getCredentials() {
-    let myCredentials = context.credentials;
-
-    // :snippet-start: walletQuickstartSelectCredentials
-    const selectedCredentials = PresentationExchange.selectCredentials({
-        vcJwts: myCredentials,
-        presentationDefinition: context.selectedOffering.data.requiredClaims,
-    });
-    // :snippet-end:
-    context.credentials = selectedCredentials;
-}
-
-async function createRfq() {
-    const selectedCredentials = context.credentials;
-
-    // :snippet-start: walletQuickstartCreateRFQ
-    context.rfq = Rfq.create({
-        metadata: {
-            to: context.pfiDid, // PFI's DID
-            from: context.customerDid.uri,              // Customer DID
-            protocol: '1.0'                     // Version of tbDEX protocol you're using
-            },
-            data: {
-            offeringId: context.selectedOffering.metadata.id,   // The ID of the selected offering
-            payin: {
-                kind: 'USD_LEDGER',                       // The method of payment
-                amount: '500.65',                         // The amount of the payin currency 
-                paymentDetails: {
-                cardNumber: '1234567890123456',
-                expiryDate: '05/25',
-                cardHolderName: 'Alice Doe',
-                cvv: '123'
-                }
-            },
-            payout: {
-                kind: 'MOMO_MPESA',                      // The method for receiving payout                         
-                paymentDetails: {
-                    phoneNumber: '123-456-7890',                 // Details to execute payment
-                    reason: "Payment for services rendered"
-                }
-            },
-            claims: selectedCredentials  // Array of signed VCs required by the PFI
-            }
-    });
-}
-
-async function sendRfq() {
     try{
         context.rfq.verifyOfferingRequirements(context.selectedOffering);
         await context.rfq.sign(context.customerDid);
@@ -155,11 +96,10 @@ async function sendRfq() {
     } catch (e) {
         throw e;
     }
+    return "HTTP 202: Successfully Submitted RFQ";
 }
 
-async function processQuote() {
-    // Wait to ensure exchange is created
-
+export async function quickstartProcessQuote() {
     context.exchangeId = context.rfq.exchangeId;
     while (!context.quote) {
         try {
@@ -168,14 +108,14 @@ async function processQuote() {
                 did: context.customerDid,
                 exchangeId: context.exchangeId
             });
-    
+
             context.quote = exchange.find(msg => msg instanceof Quote);
-    
+
             if (!context.quote) {
                 // Make sure the exchange is still open
                 context.close = exchange.find(msg => msg instanceof Close);
-                
-                if(context.close) { break; } 
+
+                if(context.close) { break; }
                 else {
                 // Wait 2 seconds before making another request
                 await new Promise(resolve => setTimeout(resolve, 2000));
@@ -188,25 +128,30 @@ async function processQuote() {
             else throw e;
         }
     }
+
+    return context.quote;
 }
 
-async function createOrder() {
+export async function quickstartCreateOrder() {
     context.order = Order.create({
-        metadata: {
+      metadata: {
         from: context.customerDid.uri,         // Customer's DID
         to: context.pfiDid,       // PFI's DID
         exchangeId: context.exchangeId,  // Exchange ID from the Quote
         protocol: "1.0"                // Version of tbDEX protocol you're using
-        }
+      }
     });
+
+    return context.order;
 }
 
-async function submitOrder() {
-    await context.order.sign(context.customerDid);
-    await TbdexHttpClient.submitOrder(context.order);
+export async function quickstartSendOrder() {
+  await context.order.sign(context.customerDid);
+  await TbdexHttpClient.submitOrder(context.order);
+  return "HTTP 202: Successfully Submitted Order";
 }
 
-async function processClose() {
+export async function quickstartProcessClose() {
     let close = null;
     while (!close) {
         try {
@@ -230,4 +175,6 @@ async function processClose() {
     }
 
     context.reasonForClose = close.data.reason;
+
+    return context.reasonForClose;
 }
--- site/testsuites/testsuite-javascript/__tests__/tbdex/wallet/walletQuickstart.test.js
+++ site/testsuites/testsuite-javascript/__tests__/tbdex/wallet/walletQuickstart.test.js
@@ -2,105 +2,99 @@ import { it, expect, describe } from 'vitest';
 // :snippet-start: walletQuickstartImports
 import { TbdexHttpClient, Rfq, Quote, Order, Close } from '@tbdex/http-client';
 import { PresentationExchange } from '@web5/credentials';
-import { DidDht,DidJwk } from '@web5/dids';
-import { VerifiableCredential } from '@web5/credentials'
+import { DidDht, DidJwk } from '@web5/dids';
+import { VerifiableCredential } from '@web5/credentials';
 // :snippet-end:
 
+// Implementation in /site/code-snippets/tbdex/wallet-quickstart.js
+
+// using https://aqf-mock-pfis.tbddev.org/ from mock-pfis
 // :snippet-start: walletQuickstartPfiDid
-const pfiDid = 'did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo';
+const pfiDid = 'did:dht:3fkz5ssfxbriwks3iy5nwys3q5kyx64ettp9wfn1yfekfkiguj1y';
 // :snippet-end:
+
+let customerDid, customerCredentials, selectedOffering, selectedCredentials, rfq, exchangeId, quote, close;
 
 describe('Wallet: Quickstart', () => {
 
-    it.skip('Testing Quickstart Workflow', async () => {
-
+    it('should create customer DID', async () => {
         // :snippet-start: walletQuickstartDidCreate
-        var customerDid = await DidJwk.create({
+        customerDid = await DidDht.create({
             options: {
-                publish: true
+                publish: true,
             },
         });
         // :snippet-end:
-      
-        const pfiDidString = '{"uri":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo","document":{"id":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo","verificationMethod":[{"id":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0","type":"JsonWebKey","controller":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo","publicKeyJwk":{"crv":"Ed25519","kty":"OKP","x":"In71mcx1pEBaAjFPnNIbNIccBDwVVsgRl2AmRSNFXW8","kid":"yBmVo8U4VzCFqhj0a88kiZPOU_gpMxEmHUy-tdvKqHM","alg":"EdDSA"}}],"authentication":["did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0"],"assertionMethod":["did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0"],"capabilityDelegation":["did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0"],"capabilityInvocation":["did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#0"],"service":[{"id":"did:dht:rj9xmgqcqs1rysongf833wo5g1dtabbhnimcorczcyurke4fmizo#pfi","type":"PFI","serviceEndpoint":"https://pfiexemplar.tbddev.org"}]},"metadata":{"published":true,"versionId":"1721323484"},"privateKeys":[{"crv":"Ed25519","d":"rTGqrDYW2rQUGsnPAtpPO0ZVe585WDbbtkufheCAloU","kty":"OKP","x":"In71mcx1pEBaAjFPnNIbNIccBDwVVsgRl2AmRSNFXW8","kid":"yBmVo8U4VzCFqhj0a88kiZPOU_gpMxEmHUy-tdvKqHM","alg":"EdDSA"}]}';
+    });
 
-        const pfiPortableDid = JSON.parse(pfiDidString);
-        const pfiBearerDid = await DidDht.import({ portableDid: pfiPortableDid });
+    it('should get offerings and select one', async () => {
+      // :snippet-start: walletQuickstartGetOfferings
+      const offerings = await TbdexHttpClient.getOfferings({ pfiDid: pfiDid });
+      // :snippet-end:
 
-        await DidDht.publish({ did: pfiBearerDid });
+      //:snippet-start: walletQuickstartSelectOffering
+      if (offerings) {
+          selectedOffering = offerings.find(
+              (offering) =>
+                  offering.data.payin.currencyCode === 'USD' &&
+                  offering.data.payout.currencyCode === 'KES'
+          );
+      }
+      //:snippet-end:
 
-        const vc = await VerifiableCredential.create({
-            type    : 'SanctionCredential',
-            issuer  : pfiBearerDid.uri,
-            subject : customerDid.uri,
-            data    : {
-              'beep': 'boop'
-            }
-          })
-          
-        const vcJwt = await vc.sign({ did: pfiBearerDid})
+      expect(selectedOffering).toBeDefined();
+    });
 
-        // Hard-coded credential
-        let myCredentials = [vcJwt];
+    it('should successfully request a verifiable credential', async () => {
+        const vcJwt = await fetch(`https://mock-idv.tbddev.org/kcc?name=angie&country=US&did=${customerDid.uri}`).then(res => res.text());
+        customerCredentials = [vcJwt];
 
-        // :snippet-start: walletQuickstartGetOfferings
-        const offerings  = await TbdexHttpClient.getOfferings({ pfiDid: pfiDid });
-        // :snippet-end:
+        expect(typeof vcJwt).toBe('string');
+        expect(customerCredentials).toBeDefined();
+    });
 
-        //:snippet-start: walletQuickstartSelectOffering
-        var selectedOffering;
-        if(offerings){
-            selectedOffering = offerings.find(offering =>
-            offering.data.payin.currencyCode === 'USD' &&
-            offering.data.payout.currencyCode === 'KES'
-            );
-        }
-        //:snippet-end:
-
-        expect(selectedOffering).toBeDefined();
-
+    it('should select credentials', async () => {
         // :snippet-start: walletQuickstartSelectCredentials
-        const selectedCredentials = PresentationExchange.selectCredentials({
-            vcJwts: myCredentials,
+        selectedCredentials = PresentationExchange.selectCredentials({
+            vcJwts: customerCredentials,
             presentationDefinition: selectedOffering.data.requiredClaims,
         });
         // :snippet-end:
 
         expect(selectedCredentials.length).toBe(1);
+    });
 
+    it('should create and send RFQ', async () => {
         // :snippet-start: walletQuickstartCreateRfq
-        var rfq = Rfq.create({
+        rfq = Rfq.create({
             metadata: {
                 to: pfiDid, // PFI's DID
-                from: customerDid.uri,              // Customer DID
-                protocol: '1.0'                     // Version of tbDEX protocol you're using
+                from: customerDid.uri, // Customer DID
+                protocol: '1.0', // Version of tbDEX protocol you're using
             },
             data: {
-                offeringId: selectedOffering.metadata.id,   // The ID of the selected offering
+                offeringId: selectedOffering.metadata.id, // The ID of the selected offering
                 payin: {
-                    kind: 'USD_LEDGER',                       // The method of payment
-                    amount: '500.65',                         // The amount of the payin currency 
+                    kind: 'USD_BANK_TRANSFER', // The method of payment
+                    amount: '500.65', // The amount of the payin currency
                     paymentDetails: {
-                    cardNumber: '1234567890123456',
-                    expiryDate: '05/25',
-                    cardHolderName: 'Alice Doe',
-                    cvv: '123'
-                    }
+                        accountNumber: '1234567890',
+                        routingNumber: '123456789',
+                    },
                 },
                 payout: {
-                    kind: 'MOMO_MPESA',                      // The method for receiving payout                         
+                    kind: 'KES_BANK_TRANSFER', // The method for receiving payout
                     paymentDetails: {
-                        phoneNumber: '123-456-7890',                 // Details required to execute payment
-                        reason: "Payment for services rendered"
-                    }
+                        accountNumber: '3245231234', // Details required to execute payment
+                    },
                 },
-                claims: selectedCredentials  // Array of signed VCs required by the PFI
-            }
+                claims: selectedCredentials, // Array of signed VCs required by the PFI
+            },
         });
         // :snippet-end:
 
         expect(async () => {
-            try{
+            try {
                 // :snippet-start: walletQuickstartSendRfq
                 await rfq.verifyOfferingRequirements(selectedOffering);
                 await rfq.sign(customerDid);
@@ -110,53 +104,60 @@ describe('Wallet: Quickstart', () => {
                 throw e;
             }
         }).not.toThrow();
+    });
 
+    it('should process the quote', async () => {
         // :snippet-start: walletQuickstartProcessQuote
         // Wait for Quote message to appear in the exchange
-        let exchangeId = rfq.exchangeId;
-        let quote;
-        let close;
-        while (!quote) {
+        exchangeId = rfq.exchangeId;
+        let attempts = 0;
+        const maxAttempts = 30;
+        const delay = 2000; // 2 seconds
+
+        while (!quote && attempts < maxAttempts) {
             try {
                 const exchange = await TbdexHttpClient.getExchange({
                     pfiDid: pfiDid,
                     did: customerDid,
-                    exchangeId: exchangeId
+                    exchangeId: exchangeId,
                 });
 
-                quote = exchange.find(msg => msg instanceof Quote);
+                quote = exchange.find((msg) => msg instanceof Quote);
 
                 if (!quote) {
                     // Make sure the exchange is still open
-                    close = exchange.find(msg => msg instanceof Close);
-                    
-                    if(close) { break; } 
-                    else {
+                    close = exchange.find((msg) => msg instanceof Close);
+
+                    if (close) {
+                        break;
+                    } else {
                         // Wait 2 seconds before making another request
-                        await new Promise(resolve => setTimeout(resolve, 2000));
+                        await new Promise((resolve) => setTimeout(resolve, delay));
                     }
                 }
             } catch (e) {
                 if (e.statusCode === 404 || e.statusCode === 401) {
-                    //waiting on RFQ to be processed
-                }
-                else throw e;
+                    // Waiting on RFQ to be processed
+                } else throw e;
             }
+            attempts++;
         }
         // :snippet-end:
 
         expect(quote).toBeDefined();
+    });
 
-        let order; 
+    it('should create and submit an order', async () => {
+        let order;
         while (!order) {
             // :snippet-start: walletQuickstartCreateOrder
             order = Order.create({
                 metadata: {
-                    from: customerDid.uri,         // Customer's DID
-                    to: pfiDid,                    // PFI's DID
-                    exchangeId: exchangeId,        // Exchange ID from the Quote
-                    protocol: "1.0"                // Version of tbDEX protocol you're using
-                }
+                    from: customerDid.uri, // Customer's DID
+                    to: pfiDid, // PFI's DID
+                    exchangeId: exchangeId, // Exchange ID from the Quote
+                    protocol: '1.0', // Version of tbDEX protocol you're using
+                },
             });
             // :snippet-end:
 
@@ -171,38 +172,44 @@ describe('Wallet: Quickstart', () => {
                 // :snippet-end:
             } catch (e) {
                 if (e.statusCode === 404) {
-                    //waiting on RFQ to be processed
-                }
-                else throw e;
+                    // Waiting on RFQ to be processed
+                } else throw e;
             }
         }).not.toThrow();
+    });
 
+    it('should process close message', async () => {
         // :snippet-start: walletQuickstartProcessClose
-        while (!close) {
+        let attempts = 0;
+        const maxAttempts = 30;
+        const delay = 2000; // 2 seconds
+
+        while (!close && attempts < maxAttempts) {
             try {
                 const exchange = await TbdexHttpClient.getExchange({
                     pfiDid: pfiDid,
                     did: customerDid,
-                    exchangeId: exchangeId
-                })
+                    exchangeId: exchangeId,
+                });
 
                 for (const message of exchange) {
                     if (message instanceof Close) {
-                        close = message
+                        close = message;
                     }
                 }
             } catch (e) {
                 if (e.statusCode === 404 || e.statusCode === 401) {
-                    //waiting on RFQ to be processed
-                }
-                else throw e;
+                    // Waiting on RFQ to be processed
+                } else throw e;
             }
+            attempts++;
+            if (!close) await new Promise((resolve) => setTimeout(resolve, delay));
         }
         const reasonForClose = close.data.reason;
         const closeSuccess = close.data.success;
         // :snippet-end:
 
         expect(closeSuccess).toBe(true);
-        expect(reasonForClose).toBeDefined();   
+        expect(reasonForClose).toBeDefined();
     });
 });
