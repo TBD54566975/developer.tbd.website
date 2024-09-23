f24377d477c32999c66315cadc733d5ae0f9cc43
dayhaysoos <ndejesus1227@gmail.com>
2024-09-23 14:01:49 -0400

    Adding more urls to community navbar drop down

---
+++ site-new/src/pages/community/spotlight.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "@site/src/components/Hero";
+
+function Spotlight() {
+  return (
+    <Hero
+      subject="Community"
+      title="Monthly Spotlight"
+      description="Shoutout to our superstar contributors! Whether you're coding, brainstorming, or cheering us on, every contribution adds magic to our community. Ready to contribute? We're looking forward to seeing your name light up this space soon! 🚀"
+      url="https://discord.gg/tbd"
+    />
+  );
+}
+
+export default Spotlight;
