085b863e8fa53286698789758982e0dc54998dba
dayhaysoos <ndejesus1227@gmail.com>
2024-09-24 22:55:51 -0400

    Governance page set up

--- site-new/src/page-components/community/governance.tsx
+++ site-new/src/page-components/community/governance.tsx
@@ -4,6 +4,7 @@ import Background from "@site/src/components/Background";
 import Heading from "@theme/Heading";
 import { TextIconFeature } from "@site/src/components/TextIconFeature";
 import { PixelBorder } from "@site/src/components/PixelBorder";
+import { SpotLightCard } from "@site/src/components/SpotLightCard";
 
 const contributors = [
   { body: "Demonstrating empathy and kindoess toward other people" },
@@ -24,6 +25,41 @@ const maintainerActivities = [
   { body: "Demonstrating empathy and kindoess toward other people" },
   { body: "Demonstrating empathy and kindoess toward other people" },
   { body: "Demonstrating empathy and kindoess toward other people" },
+];
+
+const governanceCommittee = [
+  { body: "Demonstrating empathy and kindoess toward other people" },
+  { body: "Demonstrating empathy and kindoess toward other people" },
+  { body: "Demonstrating empathy and kindoess toward other people" },
+  { body: "Demonstrating empathy and kindoess toward other people" },
+];
+
+const currentGC = [
+  {
+    handle: "Ben Boeser",
+    title: "Technical Partnerships Lead, TBD",
+    src: "https://avatars.githubusercontent.com/u/78901234?v=4",
+  },
+  {
+    handle: "Ben Boeser",
+    title: "Technical Partnerships Lead, TBD",
+    src: "https://avatars.githubusercontent.com/u/78901234?v=4",
+  },
+  {
+    handle: "Ben Boeser",
+    title: "Technical Partnerships Lead, TBD",
+    src: "https://avatars.githubusercontent.com/u/78901234?v=4",
+  },
+  {
+    handle: "Ben Boeser",
+    title: "Technical Partnerships Lead, TBD",
+    src: "https://avatars.githubusercontent.com/u/78901234?v=4",
+  },
+  {
+    handle: "Ben Boeser",
+    title: "Technical Partnerships Lead, TBD",
+    src: "https://avatars.githubusercontent.com/u/78901234?v=4",
+  },
 ];
 
 function Governance() {
@@ -77,6 +113,47 @@ function Governance() {
           <TextIconFeature key={index} body={feature.body} variant="yellow" />
         ))}
       </div>
+      <PixelBorder tone1="black" tone2="black" className="mt-8" />
+      <PixelBorder tone1="black" tone2="black" className="mt-8" />
+      <div className="w-3/4">
+        <Heading className="text-tbd-teal" as="h3">
+          Governance Committee
+        </Heading>
+        <p>
+          The TBD Open Source Governance Committee (GC) has final authority over
+          this project, including:
+        </p>
+      </div>
+      <div className="mb-8 grid grid-cols-1 gap-8 md:grid-cols-2">
+        {governanceCommittee.map((feature, index) => (
+          <TextIconFeature key={index} body={feature.body} variant="teal" />
+        ))}
+      </div>
+      <PixelBorder tone1="black" tone2="black" className="mt-8" />
+      <PixelBorder tone1="black" tone2="black" className="mt-8" />
+      <div className="w-3/4">
+        <Heading className="text-tbd-teal" as="h3">
+          Current GC members:
+        </Heading>
+      </div>
+      <div className="mb-8 grid grid-cols-1 gap-8 md:grid-cols-2">
+        {currentGC.map(({ title, handle, src }, index) => (
+          <SpotLightCard
+            key={index}
+            title={title}
+            handle={handle}
+            tone="teal"
+            src={src}
+          />
+        ))}
+      </div>
+      <p className="text-white">
+        Members are not to be contacted individually. The GC may be reached
+        through tbd-open-source-governance@squareup.com and is an available
+        resource in mediation or for sensitive cases beyond the scope of project
+        maintainers. It operates as a "Self-appointing council or board" as
+        defined by Red Hat: Open Source Governance Models.
+      </p>
     </Background>
   );
 }
