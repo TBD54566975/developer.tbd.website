29dafe0aa590c34970fec19f742e194e9b9030e2
dayhaysoos <ndejesus1227@gmail.com>
2024-09-26 18:20:19 -0400

    switch to es6

--- propagateSdkVersions.js
+++ propagateSdkVersions.js
@@ -1,9 +1,10 @@
-const fs = require("fs");
-const path = require("path");
-const xml2js = require("xml2js");
-const util = require("util");
+import fs from "fs";
+import path from "path";
+import { promisify } from "util";
+import { fileURLToPath } from "url";
 
 // Setup
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const sdkVersionsPath = path.resolve(__dirname, "sdk-versions.json");
 const directoriesToUpdate = [
   __dirname,
@@ -23,7 +24,6 @@ const getSdkVersions = async () => {
 
 // Update package.json dependencies
 const updatePackageJsonDependencies = async (dirPath, sdkVersions) => {
-  // Check and update the root or any directory level package.json first
   const packageJsonPath = path.join(dirPath, "package.json");
   if (fs.existsSync(packageJsonPath)) {
     try {
@@ -31,7 +31,7 @@ const updatePackageJsonDependencies = async (dirPath, sdkVersions) => {
         await fs.promises.readFile(packageJsonPath, "utf8")
       );
       let updated = false;
-      for (let sdk in sdkVersions.js) {
+      for (const sdk in sdkVersions.js) {
         if (packageJson.dependencies && packageJson.dependencies[sdk]) {
           packageJson.dependencies[sdk] = sdkVersions.js[sdk];
           updated = true;
@@ -53,14 +53,13 @@ const updatePackageJsonDependencies = async (dirPath, sdkVersions) => {
     }
   }
 
-  // Continue with the original directory processing
   if (fs.lstatSync(dirPath).isDirectory()) {
     const entries = await fs.promises.readdir(dirPath, { withFileTypes: true });
-    for (let dirent of entries) {
-      if (dirent.name === "node_modules") continue; // Skip node_modules directory
+    for (const dirent of entries) {
+      if (dirent.name === "node_modules") continue;
       const fullPath = path.join(dirPath, dirent.name);
       if (dirent.isDirectory()) {
-        await updatePackageJsonDependencies(fullPath, sdkVersions); // Recursive call for subdirectories
+        await updatePackageJsonDependencies(fullPath, sdkVersions);
       }
     }
   }
@@ -91,27 +90,25 @@ const updatePomXmlVersion = async (filePath, sdkVersions) => {
 
     if (updatedPomContent !== pomContent) {
       await fs.promises.writeFile(filePath, updatedPomContent);
-    } else {
     }
   } catch (error) {
     console.error(`Failed to update ${filePath}:`, error);
   }
 };
 
-// Function to recursively find and update pom.xml files, excluding node_modules
-async function findAndUpdatePomFiles(dirPath, sdkVersions) {
+const findAndUpdatePomFiles = async (dirPath, sdkVersions) => {
   const entries = await fs.promises.readdir(dirPath, { withFileTypes: true });
-  for (let dirent of entries) {
-    if (dirent.name === "node_modules") continue; // Skip node_modules directory
+  for (const dirent of entries) {
+    if (dirent.name === "node_modules") continue;
 
     const fullPath = path.join(dirPath, dirent.name);
     if (dirent.isDirectory()) {
-      await findAndUpdatePomFiles(fullPath, sdkVersions); // Recursive call for directories
+      await findAndUpdatePomFiles(fullPath, sdkVersions);
     } else if (dirent.name === "pom.xml") {
-      await updatePomXmlVersion(fullPath, sdkVersions); // Directly update pom.xml files
+      await updatePomXmlVersion(fullPath, sdkVersions);
     }
   }
-}
+};
 
 // Function to update Package.swift dependencies
 const updatePackageSwiftDependencies = async (dirPath, sdkVersions) => {
@@ -143,33 +140,33 @@ const updatePackageSwiftDependencies = async (dirPath, sdkVersions) => {
 };
 
 // Function to recursively find and update Package.swift files
-async function findAndUpdatePackageSwift(dirPath, sdkVersions) {
+const findAndUpdatePackageSwift = async (dirPath, sdkVersions) => {
   const entries = await fs.promises.readdir(dirPath, { withFileTypes: true });
-  for (let dirent of entries) {
-    if (dirent.name === "node_modules") continue; // Skip node_modules directory
+  for (const dirent of entries) {
+    if (dirent.name === "node_modules") continue;
 
     const fullPath = path.join(dirPath, dirent.name);
     if (dirent.isDirectory()) {
-      await findAndUpdatePackageSwift(fullPath, sdkVersions); // Recursive call for directories
+      await findAndUpdatePackageSwift(fullPath, sdkVersions);
     } else if (dirent.name === "Package.swift") {
-      await updatePackageSwiftDependencies(dirPath, sdkVersions); // Directly update dependencies in Package.swift files
+      await updatePackageSwiftDependencies(dirPath, sdkVersions);
     }
   }
-}
+};
 
 // Main function to initiate the version propagation process
-async function propagateVersions() {
+const propagateVersions = async () => {
   try {
     const sdkVersions = await getSdkVersions();
 
     for (const dirPath of directoriesToUpdate) {
       await updatePackageJsonDependencies(dirPath, sdkVersions);
       await findAndUpdatePomFiles(dirPath, sdkVersions);
-      await findAndUpdatePackageSwift(dirPath, sdkVersions); // Add this line to update Swift dependencies
+      await findAndUpdatePackageSwift(dirPath, sdkVersions);
     }
   } catch (error) {
     console.error("Failed to propagate versions:", error);
   }
-}
+};
 
 propagateVersions();
