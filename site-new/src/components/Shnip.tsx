6386735e2533485d64585966a6480c97e5d070df
dayhaysoos <ndejesus1227@gmail.com>
2024-10-02 16:32:42 -0400

    New Shnip component

--- site-new/src/components/Shnip.tsx
+++ site-new/src/components/Shnip.tsx
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import CodeBlock from "@theme/CodeBlock";
 import useSnippetLoader, { languageFolders } from "../hooks/useSnippetLoader";
 
+import JsIcon from "@site/static/img/js-icon";
+import KotlinIcon from "@site/static/img/KotlinIcon";
+
 interface ShnipProps {
   snippetName: string;
   languages: string[];
 }
 
+const languageIconMap: Record<string, React.ElementType> = {
+  javascript: JsIcon,
+  kotlin: KotlinIcon,
+};
+
 const Shnip: React.FC<ShnipProps> = ({ snippetName, languages }) => {
   const { snippetMap, error } = useSnippetLoader({ snippetName, languages });
-  const [activeTab, setActiveTab] = useState(languages[0]); // Default to the first language
+  const [activeTab, setActiveTab] = useState(languages[0]);
 
   if (error) {
     return <div>{error}</div>;
@@ -18,19 +26,25 @@ const Shnip: React.FC<ShnipProps> = ({ snippetName, languages }) => {
   return (
     <div>
       <div className="tabs space-x-4">
-        {languages.map((lang) => (
-          <button
-            key={lang}
-            className={`tab-button ${
-              activeTab === lang
-                ? "bg-blue-500 text-white"
-                : "bg-gray-300 text-black"
-            } rounded px-4 py-2`}
-            onClick={() => setActiveTab(lang)}
-          >
-            {lang}
-          </button>
-        ))}
+        {languages.map((lang) => {
+          const Icon = languageIconMap[lang];
+          return (
+            <button
+              key={lang}
+              className={`tab-button text-lg font-medium text-white ${
+                activeTab === lang
+                  ? "rounded-none border-b-2 border-l-0 border-r-0 border-t-0 border-tbd-yellow"
+                  : "border-b-2 border-transparent text-white"
+              } flex items-center rounded bg-transparent px-4 py-2`}
+              onClick={() => setActiveTab(lang)}
+            >
+              {Icon && (
+                <Icon className="mr-2 h-5 w-5 fill-current text-tbd-yellow" />
+              )}
+              {lang.charAt(0).toUpperCase() + lang.slice(1)}
+            </button>
+          );
+        })}
       </div>
 
       <div className="snippet-content mt-4">
