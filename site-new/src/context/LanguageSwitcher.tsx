426bacfc06893ace84dc18b9c58ef874187677f3
Nick DeJesus <ndejesus1227@gmail.com>
2024-10-04 19:58:30 -0400

    New site content (#1705)
    * adding SDK versions
    
    * adding content
    
    * removing content for now to redo shnip/language switching components
    
    * adding SDK versions
    
    * adding content
    
    * removing content for now to redo shnip/language switching components
    
    * Language switcher rework (#1783)
    
    * ✨  Blog filter implementation (#1678)
    
    * add blog filter
    
    * add filter text style
    
    * update pnpm.lock
    
    * add typewriter animation (#1682)
    
    Co-authored-by: Nick DeJesus <ndejesus1227@gmail.com>
    
    * Hacktoberfest 2024 Blog Post (#1681)
    
    * hacktoberfest 2024 blog post
    
    * Hacktoberfest 2024 Blog Post
    
    * Hacktoberfest 2024 Blog
    
    * refactoring
    
    * redo blog image
    
    * typo
    
    * add header emoji
    
    ---------
    
    Co-authored-by: Tania Chakraborty <tchakraborty@BLKG9TC7C9CQ1.local>
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * meta image header (#1684)
    
    * adding SDK versions
    
    * adding content
    
    * removing content for now to redo shnip/language switching components
    
    * Shnip rework
    
    * remove test files
    
    * Get Snippet helper function going
    
    * Set up new Shnip component and new way of gathering snippets
    
    * New Shnip component
    
    ---------
    
    Co-authored-by: B.M. Fahad-ul-Amin <shovonfahad991@gmail.com>
    Co-authored-by: taniashiba <126204004+taniashiba@users.noreply.github.com>
    Co-authored-by: Tania Chakraborty <tchakraborty@BLKG9TC7C9CQ1.local>
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * adding SDK versions
    
    * adding content
    
    * removing content for now to redo shnip/language switching components
    
    * Language switcher rework (#1783)
    
    * ✨  Blog filter implementation (#1678)
    
    * add blog filter
    
    * add filter text style
    
    * update pnpm.lock
    
    * add typewriter animation (#1682)
    
    Co-authored-by: Nick DeJesus <ndejesus1227@gmail.com>
    
    * Hacktoberfest 2024 Blog Post (#1681)
    
    * hacktoberfest 2024 blog post
    
    * Hacktoberfest 2024 Blog Post
    
    * Hacktoberfest 2024 Blog
    
    * refactoring
    
    * redo blog image
    
    * typo
    
    * add header emoji
    
    ---------
    
    Co-authored-by: Tania Chakraborty <tchakraborty@BLKG9TC7C9CQ1.local>
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * meta image header (#1684)
    
    * adding SDK versions
    
    * adding content
    
    * removing content for now to redo shnip/language switching components
    
    * Shnip rework
    
    * remove test files
    
    * Get Snippet helper function going
    
    * Set up new Shnip component and new way of gathering snippets
    
    * New Shnip component
    
    ---------
    
    Co-authored-by: B.M. Fahad-ul-Amin <shovonfahad991@gmail.com>
    Co-authored-by: taniashiba <126204004+taniashiba@users.noreply.github.com>
    Co-authored-by: Tania Chakraborty <tchakraborty@BLKG9TC7C9CQ1.local>
    Co-authored-by: Angie Jones <jones.angie@gmail.com>
    
    * fix typo
    
    * update shnip for global context, added content page for testing
    
    * remove commetn
    
    * browser check for localStorage
    
    * fix shnip and add it to start process
    
    * add shnip command to build
    
    * Update for FOSSA
    
    * Remove rollup override
    
    ---------
    
    Co-authored-by: B.M. Fahad-ul-Amin <shovonfahad991@gmail.com>
    Co-authored-by: taniashiba <126204004+taniashiba@users.noreply.github.com>
    Co-authored-by: Tania Chakraborty <tchakraborty@BLKG9TC7C9CQ1.local>
    Co-authored-by: Angie Jones <jones.angie@gmail.com>

--- site-new/src/context/LanguageSwitcher.tsx
+++ site-new/src/context/LanguageSwitcher.tsx
@@ -1,24 +1,48 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
-const LanguageContext = createContext();
+interface LanguageContextProps {
+  selectedLanguage: string;
+  setLanguage: (language: string) => void;
+}
 
-export const LanguageProvider = ({ children }) => {
-  const [selectedOption, setSelectedOption] = useState({
-    label: "JavaScript",
-    icon: "/img/js-icon.svg",
-  });
+const LanguageContext = createContext<LanguageContextProps | undefined>(
+  undefined,
+);
 
-  const toggleLanguage = (newLanguage) => {
-    setSelectedOption(newLanguage);
+export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [selectedLanguage, setSelectedLanguage] =
+    useState<string>("javascript");
+
+  useEffect(() => {
+    if (ExecutionEnvironment.canUseDOM) {
+      const storedLanguage = localStorage.getItem("selectedLanguage");
+      if (storedLanguage) {
+        setSelectedLanguage(storedLanguage);
+      }
+    }
+  }, []);
+
+  const setLanguage = (language: string) => {
+    setSelectedLanguage(language);
+    if (ExecutionEnvironment.canUseDOM) {
+      localStorage.setItem("selectedLanguage", language);
+    }
   };
 
   return (
-    <LanguageContext.Provider value={{ selectedOption, toggleLanguage }}>
+    <LanguageContext.Provider value={{ selectedLanguage, setLanguage }}>
       {children}
     </LanguageContext.Provider>
   );
 };
 
-export const useLanguage = () => {
-  return useContext(LanguageContext);
+export const useLanguage = (): LanguageContextProps => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error("useLanguage must be used within a LanguageProvider");
+  }
+  return context;
 };
