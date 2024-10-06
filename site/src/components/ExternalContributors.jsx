4c873c8f88a3385c3c154b09f202ec10c45835a5
angiejones <jones.angie@gmail.com>
2024-10-06 11:05:06 -0500

    removing side menu

--- site/src/components/ExternalContributors.jsx
+++ site/src/components/ExternalContributors.jsx
@@ -28,11 +28,8 @@ export default function ContributorShowcase() {
 
   return (
     <div className='pb-20'>
-      <p>
-        Compelling content is how we introduce and teach developers about the decentralized web (dweb). Whether it's a short video or in-depth blog post breaking down a complex topic, this space is dedicated to your top content on our open source projects, the dweb, and more.</p>
-      <p className='pb-20 , pt-10'>Have a contribution you want to share? Head over to GitHub and check out  
-        <a href="https://github.com/TBD54566975/developer.tbd.website/issues/1749" target="_blank" rel="noopener noreferrer"> this issue </a> to learn how!
-      </p>
+      <p>Compelling content is how we introduce and teach developers about the decentralized web (dweb). Whether it's a short video or in-depth blog post breaking down a complex topic, this space is dedicated to your top content on our open source projects, the dweb, and more.</p>
+      <p className='pb-20 , pt-10'>Have a contribution you want to share? Head over to GitHub and check out <a href="https://github.com/TBD54566975/developer.tbd.website/issues/1749" target="_blank" rel="noopener noreferrer">this issue</a> to learn how!</p>
       
       <div style={filterContainerStyle}>
         <label style={labelStyle}>
