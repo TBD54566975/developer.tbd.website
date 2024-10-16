7ce89434b8ee3dcc21eb4d2b569eb00652ec4279
FahadAminShovon <shovonfahad991@gmail.com>
2024-10-17 00:06:40 +0600

    update reading progress bar

--- site-new/src/components/ReadingProgress/ReadingProgress.tsx
+++ site-new/src/components/ReadingProgress/ReadingProgress.tsx
@@ -3,7 +3,6 @@ import RocketProgress from "@site/assets/icons/RocketProgress";
 
 const ReadingProgress = () => {
   const rocketRef = useRef<HTMLSpanElement | null>(null);
-  const rocketBgRef = useRef<HTMLSpanElement | null>(null);
 
   useEffect(() => {
     const updateProgressBar = () => {
@@ -14,25 +13,17 @@ const ReadingProgress = () => {
       if (rocketRef.current) {
         rocketRef.current.style.left = `calc(${Math.max(scrollPercent)}%)`;
       }
-
-      if (rocketBgRef.current) {
-        rocketBgRef.current.style.width = `calc(${Math.max(scrollPercent)}%)`;
-      }
     };
     document.addEventListener("scroll", updateProgressBar);
 
     return () => {
       document.removeEventListener("scroll", updateProgressBar);
     };
-  }, [rocketRef, rocketBgRef]);
+  }, [rocketRef]);
 
   return (
     <div className="fixed top-[--ifm-navbar-height] z-20 h-[36px] w-full bg-black">
       <div className="relative w-full">
-        <span
-          className="absolute inline-block h-[36px] w-0 bg-tbd-yellow"
-          ref={rocketBgRef}
-        />
         <span ref={rocketRef} className="absolute inline-block">
           <RocketProgress />
         </span>
